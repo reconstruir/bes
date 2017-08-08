a3f035fbad6c311cf1c60cbe1cb697b25778b5ba
Ramiro Estrugo <ramiro@fateware.com>
2017-08-08 12:53:29 -0700

    more finding funcs

--- bes/common/node.py
+++ bes/common/node.py
@@ -23,35 +23,34 @@ class node(object):
     return n
 
   def ensure_child(self, data):
-    return self.caca_find_child(data) or self.add_child(data)
+    return self.find_child_by_data(data, recurse = False) or self.add_child(data)
 
   def num_children(self):
     return len(self.children)
-    
-  def caca_find_child(self, data):
-    for child in self.children:
-      if child.data == data:
-        return child
-    return None
 
+  def find_child_by_data(self, data, recurse = True):
+    func = lambda node: node.data == data
+    return self.find_child(func, recurse = recurse)
+  
   find_result = namedtuple('find_result', 'depth,child')
 
-  def find_children(self, func):
-    return self._find_children(func, 0)
+  def find_children(self, func, recurse = True):
+    return self._find_children(func, 0, recurse)
 
-  def find_child(self, func):
-    found = self._find_children(func, 0)
+  def find_child(self, func, recurse = True):
+    found = self._find_children(func, 0, recurse)
     if not found:
       return None
     return found[0].child
 
-  def _find_children(self, func, depth):
+  def _find_children(self, func, depth, recurse):
     result = []
     for child in self.children:
       if func(child):
         result.append(self.find_result(depth, child))
-    for child in self.children:
-      result += child._find_children(func, depth + 1)
+    if recurse:
+      for child in self.children:
+        result += child._find_children(func, depth + 1, recurse)
     return result
 
 #  def has_child(self, data):
--- bes/common/tests/test_node.py
+++ bes/common/tests/test_node.py
@@ -10,28 +10,28 @@ class test_node(unittest.TestCase):
     n = node('root')
     self.assertEqual( 0, n.num_children() )
     self.assertEqual( 'root', n.data )
-    self.assertEqual( None, n.caca_find_child('notthere') )
+    self.assertEqual( None, n.find_child_by_data('notthere') )
 #    self.assertEqual( False, n.has_child('notthere') )
 
   def test_add_child(self):
     n = node('root')
     n.add_child('fruits')
     self.assertEqual( 1, n.num_children() )
-    self.assertEqual( node('fruits'), n.caca_find_child('fruits') )
+    self.assertEqual( node('fruits'), n.find_child_by_data('fruits') )
 #    self.assertTrue( n.has_child('fruits') )
 
   def test_to_string(self):
     n = node('root')
     n.add_child('fruits')
-    fruits = n.caca_find_child('fruits')
+    fruits = n.find_child_by_data('fruits')
     fruits.add_child('kiwi')
     fruits.add_child('strawberry')
     fruits.add_child('melons')
-    melons = fruits.caca_find_child('melons')
+    melons = fruits.find_child_by_data('melons')
     melons.add_child('watermelon')
     melons.add_child('canteloupe')
     n.add_child('cheeses')
-    cheeses = n.caca_find_child('cheeses')
+    cheeses = n.find_child_by_data('cheeses')
     cheeses.add_child('gouda')
     cheeses.add_child('brie')
 
@@ -71,20 +71,10 @@ class test_node(unittest.TestCase):
 '''
     self.assertEqual( expected, str(n) )
     
-  def _ensure_paths(self, n, paths):
-    for p in paths:
-      n.ensure_path(p.split('/'))
-
   def _make_tree(self, paths):
     n = node('root')
-    self._ensure_paths(n, [
-      'fruits/kiwi',
-      'fruits/strawberry',
-      'fruits/melons/watermelon',
-      'fruits/melons/canteloupe',
-      'cheeses/gouda',
-      'cheeses/brie',
-    ])
+    for p in paths:
+      n.ensure_path(p.split('/'))
     return n
       
   def test_find_children(self):
@@ -104,6 +94,30 @@ class test_node(unittest.TestCase):
     self.assertEqual( 'brie', found[1].child.data )
     self.assertEqual( 1, found[1].depth )
 
+  def test_find_children_nested(self):
+    n = self._make_tree([
+      'fruits/kiwi',
+      'fruits/strawberry',
+      'fruits/melons/watermelon',
+      'fruits/melons/canteloupe',
+      'cheeses/gouda',
+      'cheeses/brie',
+      'pies/fruit_pies/kiwi',
+      'pies/fruit_pies/strawberry',
+    ])
+    func = lambda node: node.data.startswith('kiwi') or node.data.startswith('strawberry')
+    found = n.find_children(func)
+    self.assertEqual( 4, len(found) )
+    self.assertEqual( 'kiwi', found[0].child.data )
+    self.assertEqual( 1, found[0].depth )
+    self.assertEqual( 'strawberry', found[1].child.data )
+    self.assertEqual( 1, found[1].depth )
+    self.assertEqual( 'kiwi', found[2].child.data )
+    self.assertEqual( 2, found[2].depth )
+    self.assertEqual( 'strawberry', found[3].child.data )
+    self.assertEqual( 2, found[3].depth )
+
+    
   def test_find_child(self):
     n = self._make_tree([
       'fruits/kiwi',
