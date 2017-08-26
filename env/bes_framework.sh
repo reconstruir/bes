abec2b49b49800004d98f98dab11880ee7bdd1f0
Ramiro Estrugo <ramiro@fateware.com>
2017-08-25 17:52:57 -0700

    foo

--- env/bes_framework.sh
+++ env/bes_framework.sh
@@ -3,8 +3,28 @@ if [ -n "$_BES_TRACE" ]; then echo "bes_framework.sh begin"; fi
 
 source $_BES_DEV_ROOT/env/bes_path.sh
 
+# Return system host name.  linux or macos same is bes/system/host.py
+_bes_host()
+{
+  local _name=$(uname -s)
+  local _host='unknown'
+  case $_name in
+    Linux)
+      _host='linux'
+      ;;
+    Darwin)
+      _host='macos'
+      ;;
+  esac
+  echo $_host
+  if [ $_host = 'unknown' ]; then
+     return 1
+  fi
+  return 0
+}
+
 # Source a shell file if it exists
-bes_source()
+function bes_source()
 {
   if [ $# -lt 1 ]; then
     printf "\nUsage: bes_source filename\n\n"
@@ -33,7 +53,7 @@ bes_invoke()
   return $_rv
 }
 
-bes_setup()
+function bes_setup()
 {
   if [ $# -lt 1 ]; then
     printf "\nUsage: bes_setup root_dir\n\n"
@@ -56,15 +76,76 @@ bes_setup()
 }
 
 # Get a var value
-bes_var_get()
+function bes_var_get()
 {
   eval 'printf "%s\n" "${'"$1"'}"'
 }
 
 # Set a var value
-bes_var_set()
+function bes_var_set()
 {
   eval "$1=\"\$2\""
+}
+
+function bes_PATH_prepend()
+{
+  bes_env_path_prepend PATH "$@"
+}
+
+function bes_PATH_append()
+{
+  bes_env_path_append PATH "$@"
+}
+
+function bes_PATH()
+{
+  bes_path_print $PATH
+}
+
+function bes_PYTHONPATH_prepend()
+{
+  bes_env_path_prepend PYTHONPATH "$@"
+}
+
+function bes_PYTHONPATH_append()
+{
+  bes_env_path_append PYTHONPATH "$@"
+}
+
+function bes_PYTHONPATH()
+{
+  bes_path_print $PYTHONPATH
+}
+
+function LD_LIBRARY_PATH_var_name()
+{
+  local _host=$(_bes_host)
+  local _rv=
+  case "$_host" in
+    macos)
+      _rv=DYLD_LIBRARY_PATH
+      ;;
+    linux:*)
+      _rv=LD_LIBRARY_PATH
+      ;;
+  esac
+  echo $_rv
+  return 0
+}
+
+function bes_LD_LIBRARY_PATH_prepend()
+{
+  bes_env_path_prepend $(LD_LIBRARY_PATH_var_name) "$@"
+}
+
+function bes_LD_LIBRARY_PATH_append()
+{
+  bes_env_path_append $(LD_LIBRARY_PATH_var_name) "$@"
+}
+
+function bes_LD_LIBRARY_PATH()
+{
+  bes_path_print $(bes_var_get $(LD_LIBRARY_PATH_var_name))
 }
 
 if [ -n "$_BES_TRACE" ]; then echo "bes_framework.sh end"; fi
