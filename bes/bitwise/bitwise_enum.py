10db44499800b8c1eb80c9543341a932110d9810
Ramiro Estrugo <ramiro@fateware.com>
2017-08-29 21:33:56 -0700

    more wip

--- bes/bitwise/bitwise_enum.py
+++ bes/bitwise/bitwise_enum.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import inspect
 from bitwise_unpack import bitwise_unpack
 
 class _bitwise_enum_meta(type):
 
   def __new__(meta, name, bases, class_dict):
-    print "__new__: ", name
     clazz = type.__new__(meta, name, bases, class_dict)
-#    if not hasattr(clazz, 'SIZE'):
-#      raise TypeError('Enum subclass does not define SIZE: %s' % (clazz.__name__))
-#    if not hasattr(clazz, 'VALUES'):
-#      raise TypeError('Enum subclass does not define VALUES: %s' % (clazz.__name__))
-    size = getattr(clazz, 'SIZE', 1)
-    if size not in [ 1, 2, 4, 8 ]:
-      raise TypeError('Invalid SIZE.  Should be 1, 2, 4 or 8: %s' % (size))
+    if clazz.__name__ != 'bitwise_enum':
+      size = getattr(clazz, 'SIZE', 1)
+      if size not in [ 1, 2, 4, 8 ]:
+        raise TypeError('Invalid SIZE.  Should be 1, 2, 4 or 8: %s' % (size))
 
-    print "DIR: ", dir(clazz)
-    print "DICT: ", clazz.__dict__
-    #
-    #values = getattr(clazz, 'VALUES')
-#    if not isinstance(values, ( tuple ) size not in [ 1, 2, 4, 8 ]:
-#      raise TypeError('Invalid SIZE.  Should be 1, 2, 4 or 8: %s' % (size))
-    #message_registry.register_class(clazz)
+      names = [ f for f in clazz.__dict__ if not f.startswith('_') ]
+      names = [ f for f in names if f not in [ 'SIZE', 'DEFAULT' ] ]
+      values = [ getattr(clazz, name) for name in names ]
+
+      for value in values:
+        if not isinstance(value, int):
+          raise TypeError('Value should be of type int instead of %s: %s' % (type(value), str(value)))
+      
+      setattr(clazz, '_NAMES', sorted(names))
+      setattr(clazz, '_VALUES', sorted(values))
+      
+      dvalues = {}
+      min_value = None
+      for name in names:
+        value = getattr(clazz, name)
+        dvalues[name] = getattr(clazz, name)
+        if min_value is None:
+          min_value = value
+        else:
+          min_value = min(min_value, value)
+          
+      setattr(clazz, '_DVALUES', dvalues)
+
+      if not hasattr(clazz, 'DEFAULT'):
+        setattr(clazz, 'DEFAULT', min_value)
+      default = getattr(clazz, 'DEFAULT')
+      if not isinstance(default, int):
+        raise TypeError('DEFAULT should be of type int instead of %s: %s' % (type(default), str(default)))
+      if not default in values:
+        raise ValueError('DEFAULT invalid: %d' % (default))
+      
     return clazz
 
 class bitwise_enum(object):
@@ -30,8 +51,34 @@ class bitwise_enum(object):
   __metaclass__ = _bitwise_enum_meta
 
   def __init__(self, value = None):
+    if value is None:
+      value = self.DEFAULT
     self._value = value
 
+  @classmethod
+  def value_is_valid(clazz, value):
+    return value in clazz._VALUES
+
+  @classmethod
+  def name_is_valid(clazz, name):
+    return name in clazz._NAMES
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if not self.value_is_valid(value):
+      raise ValueError('Invalid value \"%s\".  Should be one of %s' % (value, ' '.join(self._VALUES)))
+    self._value = value
+  
+  @classmethod
+  def parse(clazz, s):
+    if not s in clazz._NAMES:
+      return None
+    return self._DVALUES[s]
+  
   def write_to_io(self, io):
     pass
     
--- bes/bitwise/tests/test_bitwise_enum.py
+++ bes/bitwise/tests/test_bitwise_enum.py
@@ -6,8 +6,32 @@ from bes.bitwise import bitwise_enum as E
 
 class test_bitwise_enum(unit_test_helper):
 
-  def test_caca(self):
-    pass
+  class fruit(E):
+    SIZE = 1
+
+    PEAR = 1
+    APPLE = 2
+    KIWI = 3
+
+    DEFAULT = PEAR
   
+  def test_default_value(self):
+    F = self.fruit
+    self.assertEqual( F.PEAR, F().value )
+    
+  def test_init_value(self):
+    F = self.fruit
+    self.assertEqual( F.APPLE, F(F.APPLE).value )
+    self.assertEqual( F.KIWI, F(F.KIWI).value )
+    self.assertEqual( F.KIWI, F(F.KIWI).value )
+    
+  def test_set_value(self):
+    F = self.fruit
+    f = F()
+    f.value = F.APPLE
+    self.assertEqual( F.APPLE, f.value )
+    f.value = F.KIWI
+    self.assertEqual( F.KIWI, f.value )
+    
 if __name__ == "__main__":
   unit_test_helper.main()
