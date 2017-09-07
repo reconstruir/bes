13c9acc2b5fd540a4f3181c4ecb9a63576997367
Ramiro Estrugo <ramiro@fateware.com>
2017-09-06 19:16:33 -0700

    foo

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, find_packages
-import os, subprocess
+
+import fnmatch, os, subprocess
 
 _WANT_TESTS = bool(os.environ.get('BES_EGG_INCLUDE_TESTS', None))
 
 def _find_tests(d):
-  cmd = [ 'find', d, '-name', 'test_*.py' ]
-  result = subprocess.check_output(cmd, shell = False)
-  tests = [ test.strip() for test in result.strip().split('\n') if test.strip() ]
-  tests = [ test[len(d)+1:] for test in tests ]
+  cmd = [ 'find', d, '-type', 'f' ]
+  print "CMD: ", cmd
+  rv = subprocess.check_output(cmd, shell = False)
+  print "RV: ", rv
+  files = [ f.strip() for f in rv.split('\n') if f.strip() ]
+  result = []
+  for f in files:
+    print "FILE: ", f
+  for f in files:
+    if fnmatch.fnmatch(f, '*/tests/test_*.py*') or fnmatch.fnmatch(f, '*/test_data/*'):
+      result.append(f)
+  tests = [ f[len(d)+1:] for f in result ]
   return sorted(tests)
 
 def find_tests(d):
   if not _WANT_TESTS:
     return []
+  for x in _find_tests(d):
+    print "CACA: ", x
   return _find_tests(d)
 
 setup(
