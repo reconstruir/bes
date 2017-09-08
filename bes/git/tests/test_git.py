79c70aef1d87cda9f4065137ea82b67c5ebab5d0
Ramiro Estrugo <ramiro@fateware.com>
2017-09-07 23:48:29 -0700

    foo

--- bes/git/tests/test_git.py
+++ bes/git/tests/test_git.py
@@ -3,13 +3,13 @@
 #
 import os.path as path, unittest
 from bes.fs import file_util, temp_file
-from rebuild.git import Git
+from bes.git import git
 
 class TestGit(unittest.TestCase):
 
   def __create_tmp_repo(self):
     tmp_repo = temp_file.make_temp_dir()
-    Git.init(tmp_repo)
+    git.init(tmp_repo)
     return tmp_repo
 
   def __create_tmp_files(self, tmp_repo):
@@ -22,25 +22,25 @@ class TestGit(unittest.TestCase):
   def test_add(self):
     tmp_repo = self.__create_tmp_repo()
     new_files = self.__create_tmp_files(tmp_repo)
-    Git.add(tmp_repo, new_files)
-    expected_status = [ Git.status_item(Git.ADDED, f) for f in new_files ]
-    actual_status = Git.status(tmp_repo, '.')
+    git.add(tmp_repo, new_files)
+    expected_status = [ git.status_item(git.ADDED, f) for f in new_files ]
+    actual_status = git.status(tmp_repo, '.')
     self.assertEqual( expected_status, actual_status )
 
   def test_commit(self):
     tmp_repo = self.__create_tmp_repo()
     new_files = self.__create_tmp_files(tmp_repo)
-    Git.add(tmp_repo, new_files)
-    Git.commit(tmp_repo, 'nomsg\n', '.')
+    git.add(tmp_repo, new_files)
+    git.commit(tmp_repo, 'nomsg\n', '.')
 
   def test_clone(self):
     tmp_repo = self.__create_tmp_repo()
     new_files = self.__create_tmp_files(tmp_repo)
-    Git.add(tmp_repo, new_files)
-    Git.commit(tmp_repo, 'nomsg\n', '.')
+    git.add(tmp_repo, new_files)
+    git.commit(tmp_repo, 'nomsg\n', '.')
 
     cloned_tmp_repo = temp_file.make_temp_dir()
-    Git.clone(tmp_repo, cloned_tmp_repo)
+    git.clone(tmp_repo, cloned_tmp_repo)
 
     expected_cloned_files = [ path.join(cloned_tmp_repo, path.basename(f)) for f in new_files ]
 
@@ -50,11 +50,11 @@ class TestGit(unittest.TestCase):
   def test_clone_or_update(self):
     tmp_repo = self.__create_tmp_repo()
     new_files = self.__create_tmp_files(tmp_repo)
-    Git.add(tmp_repo, new_files)
-    Git.commit(tmp_repo, 'nomsg\n', '.')
+    git.add(tmp_repo, new_files)
+    git.commit(tmp_repo, 'nomsg\n', '.')
 
     cloned_tmp_repo = temp_file.make_temp_dir()
-    Git.clone(tmp_repo, cloned_tmp_repo)
+    git.clone(tmp_repo, cloned_tmp_repo)
 
     expected_cloned_files = [ path.join(cloned_tmp_repo, path.basename(f)) for f in new_files ]
 
