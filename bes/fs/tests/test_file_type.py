0d2376a638f92278cbb24842d6e2ac042ec8ae94
Ramiro Estrugo <ramiro@fateware.com>
2017-09-13 13:06:02 -0700

    more tests

--- bes/fs/tests/test_file_type.py
+++ bes/fs/tests/test_file_type.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-
 from bes.testing.unit_test import unit_test
 from bes.fs import file_type, temp_file
 
 class test_file_type(unit_test):
   
   def test_file(self):
-    tmp = temp_file.make_temp_file()
-    self.assertTrue( file_type.match(tmp, file_type.FILE) )
+    tmp_file = temp_file.make_temp_file()
+    self.assertTrue( file_type.match(tmp_file, file_type.FILE) )
+
+  def test_dir(self):
+    tmp_dir = temp_file.make_temp_dir()
+    self.assertTrue( file_type.match(tmp_dir, file_type.DIR) )
+
+  def test_file_or_dir(self):
+    tmp_file = temp_file.make_temp_dir()
+    tmp_dir = temp_file.make_temp_dir()
+    self.assertTrue( file_type.match(tmp_file, file_type.DIR | file_type.FILE) )
+    self.assertTrue( file_type.match(tmp_dir, file_type.DIR | file_type.FILE) )
+
+  def test_char(self):
+    self.assertTrue( file_type.match('/dev/null', file_type.CHAR) )
 
 if __name__ == '__main__':
   unit_test.main()
