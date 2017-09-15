f4fb9571ad3b19c827630673387fe46f59febc9c
Ramiro Estrugo <ramiro@fateware.com>
2017-09-14 21:26:32 -0700

    add read and write tests

--- bes/bitwise/tests/test_bitwise_enum.py
+++ bes/bitwise/tests/test_bitwise_enum.py
@@ -2,7 +2,8 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.testing.unit_test import unit_test
-from bes.bitwise import bitwise_enum
+from bes.bitwise import bitwise_enum, bitwise_io
+from cStringIO import StringIO
 
 class fruit(bitwise_enum):
   SIZE = 1
@@ -69,6 +70,21 @@ class test_bitwise_enum(unit_test):
     
   def test_parse(self):
     self.assertEqual( fruit('KIWI'), fruit.parse('KIWI') )
+    
+  def test_read(self):
+    buf = StringIO()
+    io = bitwise_io(buf)
+    io.write_u8(fruit.KIWI)
+    f = fruit()
+    f.read_from_io(bitwise_io(StringIO(buf.getvalue())))
+    self.assertEqual( fruit('KIWI'), f )
+    
+  def test_write(self):
+    buf = StringIO()
+    io = bitwise_io(buf)
+    f = fruit(fruit.KIWI)
+    f.write_to_io(io)
+    self.assertEqual( fruit.KIWI, bitwise_io(StringIO(buf.getvalue())).read_u8() )
     
 if __name__ == "__main__":
   unit_test.main()
