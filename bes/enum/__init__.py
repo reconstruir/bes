281d569c5cabab577e375f6f19358fff82222316
Ramiro Estrugo <ramiro@fateware.com>
2017-09-15 18:52:54 -0700

    foo

---
+++ bes/enum/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from .enum import enum
+from .flag_enum import flag_enum
+from .enum_loader import enum_loader
+from .enum_manager import enum_manager
