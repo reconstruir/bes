e9e27d889d281b61300637a3ff216d8e381fe839
Ramiro Estrugo <ramiro@fateware.com>
2017-09-27 22:41:33 -0700

    2to3

--- lib/bes/archive/__init__.py
+++ lib/bes/archive/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-from archive import archive
-from archive_extension import archive_extension
-from archiver import archiver
-from temp_archive import temp_archive
+from .archive import archive
+from .archive_extension import archive_extension
+from .archiver import archiver
+from .temp_archive import temp_archive
 
 
 
