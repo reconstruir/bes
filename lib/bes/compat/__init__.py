edc64be2b913ff4dd39e5dab871b16c1c1b6e71c
Ramiro Estrugo <ramiro@fateware.com>
2017-10-02 02:10:04 -0700

    2to3

--- lib/bes/compat/__init__.py
+++ lib/bes/compat/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from .Queue import Queue
+from .Queue import Queue, Empty
 from .StringIO import StringIO
 from .cmp import cmp
 from .map import map
