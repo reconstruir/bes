94c3992cd8c6a7375d20b16215ce50e1c562b8c4
Ramiro Estrugo <ramiro@fateware.com>
2017-10-12 13:23:22 -0700

    add unit test

--- lib/bes/fs/tests/test_file_replace.py
+++ lib/bes/fs/tests/test_file_replace.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 #
-import unittest
-from bes.fs import file_replace
 
-class Testfile_replace(unittest.TestCase):
+from bes.testing.unit_test import unit_test
+from bes.fs import file_replace, file_util, temp_file
+import os.path as path
 
-  def test_foo(self):
-    pass
+class test_file_replace(unit_test):
 
-if __name__ == "__main__":
-  unittest.main()
+  __unit_test_data_dir__ = 'test_data/file_replace'
+
+  def test_file_replace(self):
+    src_file = self.data_path('foo.txt')
+    tmp_dir = temp_file.make_temp_dir()
+    tmp_file = path.join(tmp_dir, 'foo.txt')
+    file_util.copy(src_file, tmp_file)
+    replacements = {
+      'This': 'That',
+      'foo': 'bar',
+    }
+    file_replace.replace(tmp_file, replacements, backup = False, word_boundary = True)
+    self.assertEqual('This is foo.\n', file_util.read(src_file, codec = 'utf-8'))
+    self.assertEqual('That is bar.\n', file_util.read(tmp_file, codec = 'utf-8'))
+    
+if __name__ == '__main__':
+  unit_test.main()
