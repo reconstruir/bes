5b134dd3ec6843ef776a5c8eca284dab2ca8e937
Ramiro Estrugo <ramiro@fateware.com>
2017-10-12 13:36:50 -0700

    add unicode test

--- lib/bes/fs/tests/test_file_replace.py
+++ lib/bes/fs/tests/test_file_replace.py
@@ -10,18 +10,29 @@ class test_file_replace(unit_test):
 
   __unit_test_data_dir__ = 'test_data/file_replace'
 
-  def test_file_replace(self):
-    src_file = self.data_path('foo.txt')
-    tmp_dir = temp_file.make_temp_dir()
-    tmp_file = path.join(tmp_dir, 'foo.txt')
-    file_util.copy(src_file, tmp_file)
+  def test_file_replace_ascii(self):
+    tmp_file = self._make_temp_replace_file('foo.txt')
     replacements = {
       'This': 'That',
       'foo': 'bar',
     }
     file_replace.replace(tmp_file, replacements, backup = False, word_boundary = True)
-    self.assertEqual('This is foo.\n', file_util.read(src_file, codec = 'utf-8'))
     self.assertEqual('That is bar.\n', file_util.read(tmp_file, codec = 'utf-8'))
+    
+  def test_file_replace_utf8(self):
+    tmp_file = self._make_temp_replace_file('bar.txt')
+    replacements = {
+      'This': 'That',
+    }
+    file_replace.replace(tmp_file, replacements, backup = False, word_boundary = True)
+    self.assertEqual('That is bér.\n', file_util.read(tmp_file, codec = 'utf-8'))
+    
+  def _make_temp_replace_file(self, filename):
+    src_file = self.data_path(filename)
+    tmp_dir = temp_file.make_temp_dir()
+    tmp_file = path.join(tmp_dir, path.basename(src_file))
+    file_util.copy(src_file, tmp_file)
+    return tmp_file
     
 if __name__ == '__main__':
   unit_test.main()
