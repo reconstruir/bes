823a6aff7a58f67027cc8bd9acee146bfbb9c258
Ramiro Estrugo <ramiro@fateware.com>
2017-11-02 13:16:34 -0700

    preserve executable permissions

--- lib/bes/archive/archive_zip.py
+++ lib/bes/archive/archive_zip.py
@@ -5,7 +5,7 @@ from .archive import archive
 from .archive_extension import archive_extension
 from bes.fs import file_util
 
-import os, os.path as path, zipfile
+import os, os.path as path, stat, zipfile
 
 class archive_zip(archive):
   'A Zip archive class.'
@@ -26,8 +26,12 @@ class archive_zip(archive):
                       include = None, exclude = None):
     with zipfile.ZipFile(file = self.filename, mode = 'r') as archive:
       dest_dir = self._determine_dest_dir(dest_dir, base_dir)
-      for member in self._filter_for_extract(members, include, exclude):
-        archive.extract(member, path = dest_dir)
+      filtered_filenames = self._filter_for_extract(members, include, exclude)
+      zip_infos = self._infos_for_files(archive, filtered_filenames)
+      for zip_info in zip_infos:
+        extracted = archive.extract(zip_info, path = dest_dir)
+        self._fix_permissions(extracted, zip_info)
+        
       self._handle_extract_strip_common_base(members, strip_common_base, strip_head, dest_dir)
 
   def create(self, root_dir, base_dir = None,
@@ -39,3 +43,19 @@ class archive_zip(archive):
     with zipfile.ZipFile(file = self.filename, mode = 'w', compression = zipfile.ZIP_DEFLATED) as archive:
       for item in items:
         archive.write(item.filename, arcname = item.arcname)
+
+  @classmethod
+  def _infos_for_files(clazz, archive, filenames):
+    return [ archive.getinfo(filename) for filename in filenames ]
+
+  _ZIP_UNIX_SYSTEM = 3
+  @classmethod
+  def _fix_permissions(clazz, filename, info):
+    'From https://stackoverflow.com/questions/42326428/zipfile-in-python-file-permission'
+    if not path.isfile(filename):
+      return
+    if info.create_system != clazz._ZIP_UNIX_SYSTEM:
+      return
+    unix_attributes = info.external_attr >> 16
+    if unix_attributes & stat.S_IXUSR:
+      os.chmod(filename, os.stat(filename).st_mode | stat.S_IXUSR)
