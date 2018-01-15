727f18dc603414a1ebc950f37a37854fb0fc9430
Ramiro Estrugo <ramiro@fateware.com>
2018-01-14 16:28:20 -0800

    mv common test_data

--- lib/bes/common/tests/test_shell.py
+++ lib/bes/common/tests/test_shell.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
-#
-import os.path as path, unittest
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+import os.path as path
 from bes.common import Shell
 
-class test_Shell(unittest.TestCase):
+class test_Shell(unit_test):
 
-  TEST_DATA_DIR = path.abspath(path.join(path.dirname(__file__), 'test_data/shell'))
+  __unit_test_data_dir__ = '../../../../test_data/bes.common/shell'
 
   def test_true(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
     rv = Shell.execute(script)
     self.assertEqual( 0, rv.exit_code )
 
   def test_false(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_false.sh')
+    script = self.data_path('shell_script_false.sh')
     rv = Shell.execute(script, raise_error = False)
     self.assertEqual( 1, rv.exit_code )
 
   def test_args(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
 
     cmd = [
       script,
@@ -34,7 +35,7 @@ class test_Shell(unittest.TestCase):
     self.assertEqual( [ 'one', 'two', 'three' ], rv.stdout.strip().split(' ') )
 
   def test_args_flat(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
 
     cmd = '%s one two three' % (script)
     rv = Shell.execute(cmd)
@@ -43,17 +44,17 @@ class test_Shell(unittest.TestCase):
     self.assertEqual( [ 'one', 'two', 'three' ], rv.stdout.strip().split(' ') )
 
   def test_true_with_shell(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
     rv = Shell.execute(script, shell = True)
     self.assertEqual( 0, rv.exit_code )
 
   def test_false_with_shell(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_false.sh')
+    script = self.data_path('shell_script_false.sh')
     rv = Shell.execute(script, raise_error = False, shell = True)
     self.assertEqual( 1, rv.exit_code )
 
   def test_args_with_shell(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
 
     cmd = [
       script,
@@ -68,7 +69,7 @@ class test_Shell(unittest.TestCase):
     self.assertEqual( [ 'one', 'two', 'three' ], rv.stdout.strip().split(' ') )
 
   def test_args_flat_with_shell(self):
-    script = path.join(self.TEST_DATA_DIR, 'shell_script_true.sh')
+    script = self.data_path('shell_script_true.sh')
 
     cmd = '%s one two three' % (script)
     rv = Shell.execute(cmd, shell = True)
@@ -76,5 +77,5 @@ class test_Shell(unittest.TestCase):
     self.assertEqual( 0, rv.exit_code )
     self.assertEqual( [ 'one', 'two', 'three' ], rv.stdout.strip().split(' ') )
 
-if __name__ == '__main__':
-  unittest.main()
+if __name__ == "__main__":
+  unit_test.main()
