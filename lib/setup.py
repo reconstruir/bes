01064fe226629cce804b6dd91066fa281397d0c1
Ramiro Estrugo <ramiro@fateware.com>
2018-02-13 10:18:59 -0800

    simpplify

--- lib/setup.py
+++ lib/setup.py
@@ -1,24 +1,5 @@
-from setuptools import setup, find_packages
-
-import fnmatch, os, subprocess
-
-_WANT_TESTS = bool(os.environ.get('BES_EGG_INCLUDE_TESTS', None))
-
-def _find_tests(d):
-  cmd = [ 'find', d, '-type', 'f' ]
-  rv = subprocess.check_output(cmd, shell = False)
-  files = [ f.strip() for f in rv.split('\n') if f.strip() ]
-  result = []
-  for f in files:
-    if fnmatch.fnmatch(f, '*/tests/*.py*') or fnmatch.fnmatch(f, '*/test_data/*'):
-      result.append(f)
-  tests = [ f[len(d)+1:] for f in result ]
-  return sorted(tests)
-
-def find_tests(d):
-  if not _WANT_TESTS:
-    return []
-  return _find_tests(d)
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 setup(
   name = 'bes',
@@ -27,10 +8,6 @@ setup(
   zip_safe = True,
   author = 'Ramiro Estrugo',
   author_email = 'bes@fateware.com',
-  include_package_data = True,
-  package_data = {
-    'bes': find_tests('bes'),
-  },
   scripts = [
     '../bin/bes_test.py',
     '../bin/bes_path.py',
