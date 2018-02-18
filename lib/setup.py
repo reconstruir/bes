b30cf934f9960cdbc5a08ead7d27647a779d48d5
Ramiro Estrugo <ramiro@fateware.com>
2018-02-18 02:40:26 -0800

    foo

--- lib/setup.py
+++ lib/setup.py
@@ -2,13 +2,11 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from setuptools import setup, find_packages
-
-import os
-print('fuck: pwd=%s' % (os.getcwd()))
+import json
 
 setup(
   name = 'bes',
-  version = '@VERSION@',
+  version = json.loads(open('bes/version.txt', 'r').read())['version'],
   packages = find_packages(include = ['bes*']),
   include_package_data = True,
   zip_safe = True,
