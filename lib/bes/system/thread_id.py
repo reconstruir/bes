bd70214e0aa1eea1b1daef8fa20874444812f770
Ramiro Estrugo <ramiro@fateware.com>
2018-02-19 23:09:23 -0800

    foo

--- lib/bes/system/thread_id.py
+++ lib/bes/system/thread_id.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-from .impl_import import impl_import
+#from .impl_import import impl_import
 
-thread_id = impl_import.load(__name__, 'thread_id', globals())
+#thread_id = impl_import.load(__name__, 'thread_id', globals())
+
+from .thread_id_base import thread_id_base
+import threading
+
+class thread_id(thread_id_base):
+
+  @classmethod
+  def thread_id(clazz):
+    'Return the current thread id.'
+    return threading.current_thread().name
