29153438c1409dbcd9b472be62a23f0395a8eaa9
Ramiro Estrugo <ramiro@fateware.com>
2018-02-20 11:45:23 -0800

    update

--- bin/bes_test.py
+++ bin/bes_test.py
@@ -7,7 +7,7 @@ import argparse, ast, copy, fnmatch, math, os, os.path as path, platform, random
 import exceptions, glob, shutil, time, tempfile
 from collections import namedtuple
 
-from bes.testing.framework import algorithm, object_util, string_util
+from bes.testing.framework import config_file
 from bes.common import algorithm, object_util, string_util
 from bes.git import git
 from bes.text import comments, lines
@@ -180,7 +180,7 @@ def main():
   if any_git_root:
     config_find_root = file_util.parent_dir(any_git_root)
     bescfg = config_file.load_configs(config_find_root)
-#    print('bescfg: %s' % (str(bescfg)))
+    print('bescfg: %s' % (str(bescfg)))
     env_dirs = file_filter.env_dirs(filtered_files)
     names = [ bescfg.env_dirs[env_dir]['name'] for env_dir in env_dirs ]
     resolved_deps = dependency_resolver.resolve_deps(bescfg.dep_map, names)
@@ -865,101 +865,6 @@ class printer(object):
   def flush(clazz):
     clazz.OUTPUT.flush()
 
-class config_file(object):
-
-  bescfg = namedtuple('bescfg', 'root_dir,configs,dep_map,env_dirs')
-
-  @classmethod
-  def load_configs(clazz, d):
-    root_dir = path.abspath(d)
-    configs = {}
-    config_files = config_file.find_config_files(root_dir)
-    env_dirs = {}
-    for f in config_files:
-      config = config_file.read_config_file(f)
-      configs[config['name']] = config
-      env_dirs[path.join(config['root_dir'], 'env')] = config
-    dep_map = clazz._make_dep_map(configs)
-    return clazz.bescfg(root_dir, configs, dep_map, env_dirs)
-    
-  @classmethod
-  def _make_dep_map(clazz, configs):
-    dep_map = {}
-    for name, config in configs.items():
-      dep_map[name] = set(config.get('requires', []))
-    return dep_map
-    
-  @classmethod
-  def find_config_files(clazz, d):
-    return file_find.find(d, '-maxdepth', '4', '-name', '*.bescfg')
-
-  @classmethod
-  def read_config_file(clazz, filename):
-    filename = path.abspath(filename)
-    root = path.normpath(path.join(path.dirname(filename), '..'))
-    content = file_util.read(filename)
-    config = clazz.parse(content)
-    variables = {
-      'root': root,
-      'rebuild_dir': path.expanduser('~/.rebuild'),
-    }
-    config = clazz.substitute_variables(config, variables)
-    config['filename'] = filename
-    config['root_dir'] = root
-    return config
-    
-  @classmethod
-  def parse(clazz, s):
-    result = {}
-    lines = s.split('\n')
-    lines = [ comments.strip_line(line) for line in lines ]
-    lines = [ line.strip() for line in lines ]
-    lines = [ line for line in lines if line ]
-    for line in lines:
-      key, sep, value = line.partition(':')
-      assert sep == ':'
-      key = key.strip()
-      value = value.strip()
-      if key == 'requires':
-        value = tuple(sorted(string_util.split_by_white_space(value)))
-      elif key in [ 'PATH', 'PYTHONPATH' ]:
-        value = value.split(':')
-      assert key not in result
-      result[key] = value
-    return result
-
-  @classmethod
-  def substitute_variables(clazz, config, variables):
-    assert isinstance(config, dict)
-    assert isinstance(variables, dict)
-    result = copy.deepcopy(config)
-    for key in config.keys():
-      clazz._sub_one(result, key, variables)
-    return result
-    
-  @classmethod
-  def _sub_one(clazz, config, key, variables):
-    assert isinstance(config, dict)
-    assert isinstance(key, basestring)
-    assert isinstance(variables, dict)
-    value = config[key]
-    for var_name, var_value in variables.items():
-      sub_key = '${%s}' % (var_name)
-      value = clazz._replace_value(value, sub_key, var_value)
-    config[key] = value
-    
-  @classmethod
-  def _replace_value(clazz, value, sub_key, sub_value):
-    assert isinstance(value, ( basestring, tuple, list ) )
-    assert isinstance(sub_key, basestring)
-    assert isinstance(sub_value, basestring)
-    if isinstance(value, basestring):
-      return value.replace(sub_key, sub_value)
-    elif isinstance(value, tuple):
-      return tuple([ x.replace(sub_key, sub_value) for x in value ])
-    elif isinstance(value, list):
-      return [ x.replace(sub_key, sub_value) for x in value ]
-
 def _python_exe_blurb(python_exe, interpreters):
   if len(interpreters) <= 1:
     return ''
@@ -1082,39 +987,6 @@ class test_apple_fixture(unit_test):
     ],
                       unit_test_inspect.inspect_file(filename) )
     file_util.remove(filename)
-
-class test_config_file(test_case):
-
-  def test_parse(self):
-    text = '''
-# foo
-name: foo
-PATH: ${root}/bin
-PYTHONPATH: ${root}/lib
-requires: bar baz
-'''
-    
-    self.assertEqual( {
-      'name': 'foo',
-      'PATH': [ '${root}/bin' ],
-      'PYTHONPATH': [ '${root}/lib' ],
-      'requires': ( 'bar', 'baz' ),
-    }, config_file.parse(text) )
-
-  def test_substitute_variables(self):
-    config = {
-      'name': 'foo',
-      'PATH': [ '${root}/bin' ],
-      'PYTHONPATH': [ '${root}/lib' ],
-      'requires': ( 'bar', 'baz' ),
-    }
-    variables = { 'root': '/home/pato' }
-    self.assertEqual( {
-      'name': 'foo',
-      'PATH': [ '/home/pato/bin' ],
-      'PYTHONPATH': [ '/home/pato/lib' ],
-      'requires': ( 'bar', 'baz' ),
-    }, config_file.substitute_variables(config, variables) )
 
 if len(sys.argv) >= 2 and sys.argv[1] in [ '--unit' ]:
   sys.argv = sys.argv[0:1]
