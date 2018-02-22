2a852530756d7ba324d09072a046b598d61869ca
Ramiro Estrugo <ramiro@fateware.com>
2018-02-21 20:30:55 -0800

    wip

--- lib/bes/testing/framework/__init__.py
+++ lib/bes/testing/framework/__init__.py
@@ -2,4 +2,5 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from .config_file import config_file_caca
-from .config_file import config_file
+from .config_data import config_data
+
--- tests/lib/bes/testing/framework/test_config_file.py
+++ tests/lib/bes/testing/framework/test_config_file.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-#!/usr/bin/env python
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
 from bes.testing.unit_test import unit_test
 from bes.testing.framework import config_file_caca
-from bes.testing.framework import config_file as CF
-
-class test_config_file(unit_test):
-
-  def test___init__(self):
-    pass
-
-  def test_parse(self):
-    text = '''
-# foo
-name: foo
-unixpath: ${root}/bin
-pythonpath: ${root}/lib
-requires: bar baz
-'''
-    self.assertEqual( ( 'foo', [ '${root}/bin' ], [ '${root}/lib' ], set([ 'bar', 'baz' ]) ), CF.parse(text) )
   
 class test_config_file_caca(unit_test):
 
