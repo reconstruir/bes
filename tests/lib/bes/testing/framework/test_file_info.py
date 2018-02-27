f49ea6bbe01abfbfc66d360e36ded21b71f5fe08
Ramiro Estrugo <ramiro@fateware.com>
2018-02-27 00:39:09 -0800

    update

--- tests/lib/bes/testing/framework/test_file_info.py
+++ tests/lib/bes/testing/framework/test_file_info.py
@@ -10,13 +10,16 @@ from bes.git import git, repo, temp_git_repo
 class test_file_info(unit_test):
 
   __unit_test_data_dir__ = '${BES_TEST_DATA_DIR}/bes.testing/framework'
+
+  def test_config(self):
+    ce = config_env(self.data_dir())
+    a = FI(ce, self.data_path('orange/lib/orange/common/orange_util.py'))
+    self.assertEqual( ce.config_for_name('orange'), a.config )
   
-  def xtest_basic(self):
+  def test_git_root(self):
     ce = config_env(self.data_dir())
     a = FI(ce, self.data_path('orange/lib/orange/common/orange_util.py'))
     self.assertEqual( git.root(self.data_path('orange/lib/orange/common/orange_util.py')), a.git_root )
-    self.assertEqual( True, a.git_tracked )
-    self.assertEqual( ce.config_for_name('orange'), a.config )
 
   def test_git_tracked(self):
     r = temp_git_repo.make_temp_repo(content = [ 'file lib/foo/foo.py "def foo(): return 666\n" 644' ])
