6952ca2ad93db47a68ceceedfe3ea38d5ccf56cf
Ramiro Estrugo <ramiro@fateware.com>
2018-02-28 20:45:30 -0800

    update

--- bin/bes_test.py
+++ bin/bes_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-print('COKKSUKER')
 # A script to run python unit tests.  Depends on bes which as bit of a chicken-and-egg
 # problem when unit testing bes itself.  Use the standalone bes_test version to avoid
 # the issue.
