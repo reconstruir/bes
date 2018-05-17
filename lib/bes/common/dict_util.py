ef73fb8ab52534ffabb41c255d0276d695982315
Ramiro Estrugo <ramiro@fateware.com>
2018-05-17 08:38:28 -0700

    move the notion of strip to be part of the patches file_list

--- lib/bes/common/dict_util.py
+++ lib/bes/common/dict_util.py
@@ -4,6 +4,7 @@
 import copy
 from bes.compat import StringIO
 from .string_util import string_util
+from .variable import variable
 
 class dict_util(object):
   'Dict util'
@@ -78,3 +79,8 @@ class dict_util(object):
     for k, v in d.items():
       if string_util.is_string(v):
         d[k] = string_util.quote(v)
+
+  @staticmethod
+  def substitute_variables(d, substitutions, word_boundary = True):
+    for key in d.iterkeys():
+      d[key] = variable.substitute(d[key], substitutions, word_boundary = word_boundary)
