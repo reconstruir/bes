b97e49dfeb8a52d262cba6e74f2cdacad915b91f
Ramiro Estrugo <ramiro@fateware.com>
2018-05-30 01:43:26 -0700

    restore old inspect

--- lib/bes/testing/framework/unit_test_inspect.py
+++ lib/bes/testing/framework/unit_test_inspect.py
@@ -1,7 +1,9 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import os.path as path
+import ast, os.path as path
 from bes.common import algorithm
+from bes.fs import file_util
+from bes.system import compat
 from .unit_test_description import unit_test_description
 
 import unittest
@@ -11,7 +13,8 @@ class unit_test_inspect(object):
   @classmethod
   def inspect_file(clazz, filename):
     try:
-      return clazz.inspect_file_new(filename)
+      return clazz.inspect_file_old(filename)
+    #return clazz.inspect_file_new(filename)
     except Exception as ex:
       print('WARNING: failed to inspect unit test %s: %s' % (path.relpath(filename), str(ex)))
       return None
@@ -31,3 +34,39 @@ class unit_test_inspect(object):
           for function in test_functions:
             result.append(unit_test_description(filename, fixture, function))
     return sorted(algorithm.unique(result), key = lambda x: x.function)
+
+  @classmethod
+  def inspect_file_old(clazz, filename):
+    code = file_util.read(filename)
+    tree = ast.parse(code, filename = filename)
+    s = ast.dump(tree, annotate_fields = True, include_attributes = True)
+    result = []
+    for node in tree.body:
+      if clazz._node_is_unit_test_class(node):
+        for statement in node.body:
+          if isinstance(statement, ast.FunctionDef):
+            if statement.name.startswith('test_'):
+              result.append(unit_test_description(filename, node.name, statement.name))
+    return sorted(result, key = lambda x: x.function)
+
+  @classmethod
+  def _node_is_unit_test_class(clazz, node):
+    if not isinstance(node, ast.ClassDef):
+      return False
+    for i, base in enumerate(node.bases):
+      base_class_name = clazz._base_class_name(base)
+      if base_class_name in [ 'unittest.TestCase', 'unit_test', 'script_unit_test' ]:
+        return True
+    return False
+    
+  @classmethod
+  def _base_class_name(clazz, base):
+    result = []
+    for field in base._fields:
+      value = getattr(base, field)
+      if isinstance(value, ast.Name):
+        result.append(value.id)
+      elif compat.is_string(value):
+        result.append(value)
+    return '.'.join(result)
+  
