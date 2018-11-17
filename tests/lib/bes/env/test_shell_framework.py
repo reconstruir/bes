fdc8fff1092ba6cd0c6428829c68536aad9fab55
Ramiro Estrugo <ramiro@fateware.com>
2018-11-17 14:10:31 -0800

    add a test that actually uses the shell_framework

--- tests/lib/bes/env/test_shell_framework.py
+++ tests/lib/bes/env/test_shell_framework.py
@@ -3,8 +3,11 @@
 
 import os.path as path
 from bes.testing.unit_test import unit_test
-from bes.fs import temp_file
+from bes.fs import file_util, temp_file
 from bes.env import shell_framework
+from bes.env.env_dir import env_dir
+from bes.system import os_env
+from bes.fs.testing import temp_content
 
 class test_shell_framework(unit_test):
 
@@ -12,7 +15,35 @@ class test_shell_framework(unit_test):
     tmp_dir = temp_file.make_temp_dir(delete = False)
     ef = shell_framework()
     ef.extract(tmp_dir)
-    print('tmp_dir: %s' % (tmp_dir))
+    self.assertTrue( path.exists(path.join(tmp_dir, 'env/bes_framework.sh')) )
+    
+  def test_use_framework(self):
+    tmp_dir = temp_file.make_temp_dir(delete = False)
+    ef = shell_framework()
+    ef.extract(tmp_dir)
+    env_path = path.join(tmp_dir, 'env')
+
+    my_script_content = '''
+_this_file="$( command readlink "$BASH_SOURCE" )" || _this_file="$BASH_SOURCE"
+_root="${_this_file%/*}"
+if [ "$_root" == "$_this_file" ]; then
+  _root=.
+fi
+_FOO="$( command cd -P "$_root" > /dev/null && command pwd -P )"
+unset _this_file
+unset _root
+source ${_FOO}/bes_framework.sh
+bes_env_path_append PATH /foo/bin
+'''
+    my_script_path = path.join(env_path, 'my_script.sh')
+    file_util.save(my_script_path, content = my_script_content)
+    
+    ed = env_dir(env_path, files = [ 'my_script.sh' ])
+    expected = {
+      'PATH': '%s:/foo/bin' % (os_env.DEFAULT_SYSTEM_PATH),
+    }
+    actual = ed.transform_env({ 'PATH': os_env.DEFAULT_SYSTEM_PATH })
+    self.assertEqual( expected, actual )
     
 if __name__ == '__main__':
   unit_test.main()
