87928972d12a68dc6372c0123eebba3b614d43d9
Ramiro Estrugo <ramiro@fateware.com>
2018-11-30 10:00:27 -0800

    add exectext

--- lib/bes/python/code.py
+++ lib/bes/python/code.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 class code(object):
   'Class to deal with python code.'
 
   @classmethod
-  def execfile(clazz, filename, xglobals, xlocals):
+  def execfile(clazz, filename, exec_globals, exec_locals):
     try:      
       with open(filename, 'r') as f:
         content = f.read()
     except Exception as ex:
       raise
-    c = compile(content, filename, 'exec')
-    exec(c, xglobals, xlocals)
+    clazz.exectext(filename, content, exec_globals, exec_locals)
+
+  @classmethod
+  def exectext(clazz, filename, text, exec_globals, exec_locals):
+    c = compile(text, filename, 'exec')
+    exec(c, exec_globals, exec_locals)
