3fb6f9582d8810a997ea90a86e8c2a0b728c7f1f
Ramiro Estrugo <ramiro@fateware.com>
2018-12-07 12:33:21 -0800

    Env var support

--- lib/bes/config/simple_config.py
+++ lib/bes/config/simple_config.py
@@ -6,6 +6,8 @@ from bes.compat import StringIO
 from bes.key_value import key_value, key_value_list
 from bes.fs import file_util
 from bes.text import tree_text_parser
+from bes.common import check, dict_util, variable
+from bes.system import os_env_var
 
 from collections import namedtuple
 
@@ -16,7 +18,9 @@ class error(Exception):
     self.origin = origin
 
   def __str__(self):
-    return '%s:%s: %s' % (self.origin.source, self.origin.line_number, self.message)
+    source = '<unknown>' if not self.origin else self.origin.source
+    line_number = '<unknown>' if not self.origin else self.origin.line_number
+    return '%s:%s: %s' % (source, line_number, self.message)
 
 class origin(namedtuple('origin', 'source, line_number')):
 
@@ -58,28 +62,63 @@ class section(namedtuple('section', 'name, entries, origin')):
       buf.write(str(entry))
     return buf.getvalue()
 
-  def find_by_key(self, key, raise_error = True):
+  def find_by_key(self, key, raise_error = True, resolve_env_vars = False):
     for entry in self.entries:
       if entry.value.key == key:
-        return entry.value.value
+        value = entry.value.value
+        if resolve_env_vars:
+          value = self._resolve_variable(value, entry.origin)
+        return value
     if raise_error:
       raise error('%s not found' % (key), self.origin)
     return None
   
-  def to_key_value_list(self):
-    return key_value_list([ entry.value for entry in self.entries ])
+  def to_key_value_list(self, resolve_env_vars = False):
+    'Return values as a key_value_list optionally resolving environment variables.'
+    result = key_value_list()
+    for entry in self.entries:
+      value = entry.value.value
+      if resolve_env_vars:
+        value = self._resolve_variable(value, entry.origin)
+      result.append(key_value(entry.value.key, value))
+    return result
   
-  def to_dict(self):
-    return self.to_key_value_list().to_dict()
+  def to_dict(self, resolve_env_vars = False):
+    'Return values as a dict optionally resolving environment variables.'
+    return self.to_key_value_list(resolve_env_vars = resolve_env_vars).to_dict()
+
+  @classmethod
+  def _resolve_variable(clazz, value, origin):
+    variables = variable.find_variables(value)
+    if variables:
+      substitutions = clazz._substitutions_for_value(value, origin)
+      return variable.substitute(value, substitutions, word_boundary = True)
+    return value
+  
+  @classmethod
+  def _substitutions_for_value(clazz, v, origin):
+    result = {}
+    variables = variable.find_variables(v)
+    for var in variables:
+      os_var = os_env_var(var)
+      if not os_var.is_set:
+        raise error('Not set in the current environment: %s' % (v), origin)
+      result[var] = os_var.value
+    return result
   
 check.register_class(section)
 
 class simple_config(object):
   'A very simple config file'
 
-  def __init__(self, sections = None):
+  # Convenience reference so users dont need to import error to catch it
+  error = error
+  
+  def __init__(self, sections = None, source = None):
     sections = sections or []
+    source = source or '<unknown>'
     log.add_logging(self, 'simple_config')
+    self.origin = origin(source, 1)
     self.sections = sections[:]
     
   def __str__(self):
@@ -114,7 +153,7 @@ class simple_config(object):
     for child in root.children:
       section = clazz._parse_section(child, source)
       sections.append(section)
-    return clazz(sections = sections)
+    return simple_config(sections = sections, source = source)
 
   @classmethod
   def _parse_section(clazz, node, source):
@@ -132,6 +171,6 @@ class simple_config(object):
     for child in node.children:
       entry_value = key_value.parse(child.data.text, delimiter = ':')
       entry_origin = origin(source, child.data.line_number)
-      entry(entry_value, entry_origin)
-      result.append(entry(entry_value, entry_origin))
+      new_entry = entry(entry_value, entry_origin)
+      result.append(new_entry)
     return result
--- tests/lib/bes/config/test_simple_config.py
+++ tests/lib/bes/config/test_simple_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+import os
 from bes.testing.unit_test import unit_test
 from bes.config import simple_config as SC
 
@@ -25,8 +26,8 @@ credential
     
     s = SC.from_text(text)
 
-    sections = s.find_sections('foo')
-    self.assertEqual( [], sections )
+    with self.assertRaises(SC.error):
+      s.find_sections('foo')
 
     sections = s.find_sections('credential')
     self.assertEqual( 2, len(sections) )
@@ -39,5 +40,65 @@ credential
       'password': 'sekret1',
       }, sections[0].to_dict() )
     
+  def test_env_var(self):
+    text = '''\
+# This is one
+credential
+  provider: pcloud
+  type: download
+  email: email1@bar.com # one
+  password: ${SEKRET1}
+
+# This is two
+credential
+  provider: pcloud
+  type: upload
+  email: email2@bar.com # two
+  password: ${SEKRET2}
+'''
+    try:
+      os.environ['SEKRET1'] = 'sekret1'
+      os.environ['SEKRET2'] = 'sekret2'
+      s = SC.from_text(text)
+
+      sections = s.find_sections('credential')
+      self.assertEqual( 2, len(sections) )
+      self.assertEqual( 'download', sections[0].find_by_key('type') )
+      self.assertEqual( 'upload', sections[1].find_by_key('type') )
+      self.assertEqual( '${SEKRET1}', sections[0].find_by_key('password', resolve_env_vars = False) )
+      self.assertEqual( 'sekret1', sections[0].find_by_key('password', resolve_env_vars = True) )
+
+      self.assertEqual( {
+        'provider': 'pcloud',
+        'type': 'download',
+        'email': 'email1@bar.com',
+        'password': '${SEKRET1}',
+      }, sections[0].to_dict(resolve_env_vars = False) )
+
+      self.assertEqual( {
+        'provider': 'pcloud',
+        'type': 'download',
+        'email': 'email1@bar.com',
+        'password': 'sekret1',
+      }, sections[0].to_dict(resolve_env_vars = True) )
+    finally:
+      del os.environ['SEKRET1']
+      del os.environ['SEKRET2']
+    
+  def test_env_var_missing(self):
+    text = '''\
+# This is one
+credential
+  provider: pcloud
+  type: download
+  email: email1@bar.com # one
+  password: ${SEKRET1}
+'''
+    s = SC.from_text(text)
+    sections = s.find_sections('credential')
+    self.assertEqual( 1, len(sections) )
+    with self.assertRaises(SC.error) as context:
+      sections[0].to_dict(resolve_env_vars = True)
+      
 if __name__ == '__main__':
   unit_test.main()
