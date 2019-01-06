a17b8d1596cd1588d61873b6d8879c0cdbabfe90
Ramiro Estrugo <ramiro@fateware.com>
2019-01-06 03:42:32 -0800

    shell_profile skeleton

---
+++ tests/lib/bes/unix/test_shell_profile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+import os.path as path
+from bes.testing.unit_test import unit_test
+from bes.unix.shell_profile import shell_profile as SP
+#from bes.fs import temp_file
+
+class test_shell_profile(unit_test):
+
+  def test_shell_is_bash(self):
+    print( SP.shell_is_bash() )
+    
+if __name__ == '__main__':
+  unit_test.main()
