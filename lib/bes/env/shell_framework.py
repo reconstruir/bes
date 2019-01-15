07f3b8251091e04b7ffd6689476f54f9c27413de
Ramiro Estrugo <ramiro@fateware.com>
2019-01-15 01:58:44 -0800

    make it a classmethod

--- lib/bes/env/shell_framework.py
+++ lib/bes/env/shell_framework.py
@@ -6,11 +6,12 @@ from bes.fs import file_util
 class shell_framework(object):
 
   _FILENAME = 'env/bes_shell.sh'
-  
-  def extract(self, where):
+
+  @classmethod
+  def extract(clazz, where):
     python_module_name = 'bes'
-    content = pkgutil.get_data('bes', self._FILENAME).decode('utf-8')
-    dst_path = path.join(where, path.basename(self._FILENAME))
+    content = pkgutil.get_data('bes', clazz._FILENAME).decode('utf-8')
+    dst_path = path.join(where, path.basename(clazz._FILENAME))
     file_util.save(dst_path, content = content, mode = 0o755)
     if file_util.read(dst_path, codec = 'utf8') != content:
       raise RuntimeError('Failed to save %s.' % (dst_path))
