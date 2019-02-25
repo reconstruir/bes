57128397463f2ec8d5d06eba61cad03515b484dd
Ramiro Estrugo <ramiro@fateware.com>
2019-02-24 21:57:23 -0800

    add support for dealing with linux systems with selinux that add extra 'selinux' keys to some attr results

--- tests/lib/bes/fs/test_file_attributes.py
+++ tests/lib/bes/fs/test_file_attributes.py
@@ -18,21 +18,27 @@ class test_file_attributes(unit_test):
 
   def test_empty_keys(self):
     tmp = self._make_temp_file('this is foo\n')
-    self.assertEqual( [], FA.keys(tmp) )
+    self.assertEqual( [], self._munge_attr_keys(FA.keys(tmp)) )
 
   def test_keys(self):
     tmp = self._make_temp_file('this is foo\n')
     FA.set(tmp, 'foo', 'hi')
     FA.set(tmp, 'bar', '99')
-    self.assertEqual( [ 'bar', 'foo' ], FA.keys(tmp) )
+    self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(FA.keys(tmp)) )
     
   def test_clear(self):
     tmp = self._make_temp_file('this is foo\n')
     FA.set(tmp, 'foo', 'hi')
     FA.set(tmp, 'bar', '99')
-    self.assertEqual( [ 'bar', 'foo' ], FA.keys(tmp) )
+    self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(FA.keys(tmp)) )
     FA.clear(tmp)
     self.assertEqual( [], FA.keys(tmp) )
+
+  @classmethod
+  def _munge_attr_keys(clazz, keys):
+    'On some linux systems, there is an extra selinux key in many attr results'
+    assert isinstance(keys, list)
+    return [ key for key in keys if key != 'selinux' ]
 
   def _make_temp_file(self, content):
     tmp_dir = path.join(path.dirname(__file__), '.tmp')
