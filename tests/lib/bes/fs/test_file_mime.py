b9d375a246b19af34c3fec7df029816c1578610c
Ramiro Estrugo <ramiro@fateware.com>
2019-02-24 22:29:31 -0800

    find a binary without using SHELL in env.

--- tests/lib/bes/fs/test_file_mime.py
+++ tests/lib/bes/fs/test_file_mime.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 import os
+from os import path
 import unittest
 from bes.fs import file_mime
 
@@ -12,12 +13,26 @@ class test_file_mime(unittest.TestCase):
     self.assertEqual( ( 'text/plain', 'us-ascii' ), mt )
     self.assertEqual( 'text/plain; charset=us-ascii', str(mt) )
 
+  @classmethod
+  def _find_binary_file(clazz):
+    possible = [
+      '/bin/bash',
+      '/bin/sh',
+      '/bin/busybox',
+    ]
+    for f in possible:
+      if path.isfile(f):
+        return f
+    return None
+    
   def test_shell_is_binary(self):
-    self.assertTrue( file_mime.mime_type(os.environ['SHELL']).mime_type in file_mime.BINARY_TYPES )
+    f = self._find_binary_file()
+    self.assertTrue( file_mime.mime_type(f).mime_type in file_mime.BINARY_TYPES )
 
   def test_is_text(self):
     self.assertTrue( file_mime.is_text('/etc/passwd') )
-    self.assertFalse( file_mime.is_text(os.environ['SHELL']) )
+    f = self._find_binary_file()
+    self.assertFalse( file_mime.is_text(f) )
 
   def test__parse_file_output_fat(self):
     text = '''\
