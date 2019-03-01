2145b5a169be223c90f7046f8e172c0adc5693a2
Ramiro Estrugo <ramiro@fateware.com>
2019-02-28 16:14:33 -0800

    be explicit about the root dir so that it works outside of git.

--- rebuild/recipes/bes/reb-bes-test-unit-tests.sh
+++ rebuild/recipes/bes/reb-bes-test-unit-tests.sh
@@ -5,4 +5,4 @@ set -e -x
 EGO_BES_UNIT_TESTS=${EGO_BES_UNIT_TESTS:-"*"}
 _TESTS="${REBUILD_BUILD_DIR}/tests/${EGO_BES_UNIT_TESTS}"
 
-${_BES_TEST} --verbose --file-ignore-file .bes_test_internal_ignore --dont-hack-env ${_TESTS}
+${_BES_TEST} --root-dir ${REBUILD_BUILD_DIR} --verbose --file-ignore-file .bes_test_internal_ignore --dont-hack-env ${_TESTS}
