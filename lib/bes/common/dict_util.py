18478fe8d9f9dde63f9ceafd720f995eea546a56
Ramiro Estrugo <ramiro@fateware.com>
2019-03-09 16:12:40 -0800

    add get-recursively

--- lib/bes/common/dict_util.py
+++ lib/bes/common/dict_util.py
@@ -96,3 +96,50 @@ class dict_util(object):
     for key in keys:
       if key in d:
         del d[key]
+
+  @staticmethod
+  def get_recursively(search_dict, field):
+    """
+    Takes a dict with nested lists and dicts,
+    and searches all dicts for a key of the field
+    provided.
+    """
+    fields_found = []
+
+    for key, value in search_dict.iteritems():
+
+        if key == field:
+            fields_found.append(value)
+
+        elif isinstance(value, dict):
+            results = dict_util.get_recursively(value, field)
+            for result in results:
+                fields_found.append(result)
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    more_results = dict_util.get_recursively(item, field)
+                    for another_result in more_results:
+                        fields_found.append(another_result)
+
+    return fields_found
+  
+  @staticmethod
+  def search(d, key, default=None):
+    'https://stackoverflow.com/questions/14962485/finding-a-key-recursively-in-a-dictionary'
+    """Return a value corresponding to the specified key in the (possibly
+    nested) dictionary d. If there is no item with that key, return
+    default.
+    """
+    stack = [iter(d.items())]
+    while stack:
+        for k, v in stack[-1]:
+            if isinstance(v, dict):
+                stack.append(iter(v.items()))
+                break
+            elif k == key:
+                return v
+        else:
+            stack.pop()
+    return default        
