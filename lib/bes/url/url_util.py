ceae9f361179c348ee84764e987ec22894fe5f8f
Ramiro Estrugo <ramiro@fateware.com>
2019-03-18 22:39:28 -0700

    add cookie support.  fix the abstractions for python 2 and 3 more.

--- lib/bes/url/url_util.py
+++ lib/bes/url/url_util.py
@@ -3,16 +3,20 @@
 from os import path
 from collections import namedtuple
 
+from bes.common import check
+from bes.system import compat
 from bes.compat.url_compat import urljoin, urlopen, urlencode, urlparse, Request
 from bes.fs import file_util, temp_file
+from bes.key_value import key_value
 
 class url_util(object):
   'Url util'
 
   @classmethod
-  def download_to_stream(clazz, url, stream, chunk_size = None):
+  def download_to_stream(clazz, url, stream, chunk_size = None, cookies = None):
     'Download url to a stream in chunks.'
-    response = urlopen(url)
+    check.check_dict(cookies, check.STRING_TYPES, check.STRING_TYPES, allow_none = True)
+    response = clazz._url_open(url, cookies = cookies)
     status_code = response.getcode()
     if status_code != 200:
       return status_code
@@ -25,14 +29,14 @@ class url_util(object):
     return status_code
 
   @classmethod
-  def download_to_file(clazz, url, filename, chunk_size = None):
+  def download_to_file(clazz, url, filename, chunk_size = None, cookies = None):
     'Download url to filename.'
-    tmp = clazz.download_to_temp_file(url, chunk_size = chunk_size)
+    tmp = clazz.download_to_temp_file(url, chunk_size = chunk_size, cookies = cookies)
     file_util.copy(tmp, filename)
     file_util.remove(tmp)
 
   @classmethod
-  def download_to_temp_file(clazz, url, chunk_size = None, basename = None, delete = True):
+  def download_to_temp_file(clazz, url, chunk_size = None, basename = None, delete = True, cookies = None):
     'Download url to a temporary file.'
     if basename:
       assert file_util.is_basename(basename)
@@ -40,7 +44,7 @@ class url_util(object):
     else:  
       tmp = temp_file.make_temp_file(suffix = '.download')
     with open(tmp, 'wb') as fout:
-      result = clazz.download_to_stream(url, fout, chunk_size = chunk_size)
+      result = clazz.download_to_stream(url, fout, chunk_size = chunk_size, cookies = cookies)
       fout.close()
     return tmp
       
@@ -61,3 +65,35 @@ class url_util(object):
   def url_path_baename(clazz, url):
     return path.basename(urlparse(url).path)
   
+  @classmethod
+  def _url_open(clazz, url, cookies = None):
+    'Python 2 and 3 url open compatibility wrapper.'
+    if compat.IS_PYTHON2:
+      return clazz._url_open_2(url, cookies = cookies)
+    elif compat.IS_PYTHON3:
+      return clazz._url_open_3(url, cookies = cookies)
+    else:
+      raise RuntimeError('Unknown python version')
+  
+  @classmethod
+  def _url_open_2(clazz, url, cookies = None):
+    'Python 2 url open.'
+    import urllib2
+    opener = urllib2.build_opener()
+    if cookies:
+      flat_cookies = '; '.join('%s=%s' % (key, value) for key, value in cookies.items())
+      opener.addheaders.append(('Cookie', flat_cookies))
+    response = opener.open(url)
+    return response
+
+  @classmethod
+  def _url_open_3(clazz, url, cookies = None):
+    'Python 3 url open.'
+    headers = None
+    if cookies:
+      headers = headers or {}
+      flat_cookies = '; '.join('%s=%s' % (key, value) for key, value in cookies.items())
+      headers['Cookie'] = flat_cookies
+    req = Request(url, headers = headers)
+    response = urlopen(req)
+    return response
