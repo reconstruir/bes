a22ae851a83f1f6212ae09ad647ef1dad5464891
Ramiro Estrugo <restrugo@imvu.com>
2019-06-14 14:13:24 -0700

    make username work on windows.

--- lib/bes/system/user.py
+++ lib/bes/system/user.py
@@ -1,7 +1,13 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import os, pwd
+import os
+
+from .host import host
 
 class user(object):
 
-  USERNAME = pwd.getpwuid(os.getuid()).pw_name
+  if host.is_unix():
+    import pwd
+    USERNAME = pwd.getpwuid(os.getuid()).pw_name
+  elif host.is_windows():
+    USERNAME = os.environ.get('USERNAME')
