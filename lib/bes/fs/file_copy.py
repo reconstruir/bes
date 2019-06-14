2b29a8f268b4d418c88490b53ae8b3951f0782e9
Ramiro Estrugo <restrugo@imvu.com>
2019-06-13 19:32:14 -0700

    file-cpy wip

--- lib/bes/fs/file_copy.py
+++ lib/bes/fs/file_copy.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+import shutil
+
 from bes.system.host import host
 
 from .tar_util import tar_util
@@ -9,6 +11,8 @@ class file_copy(object):
   @classmethod
   def copy_tree(clazz, src_dir, dst_dir, excludes = None):
     'Copy src_dir to dst_dir recursively, preserving permissions and optionally excluding excludes.'
+#    shutil.copytree(src_dir, dst_dir, symlinks = True)
+#    return
     if host.is_unix():
       return tar_util.copy_tree(src_dir, dst_dir, excludes = excludes)
     else:
---
+++ tests/lib/bes/fs/test_file_copy.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+import os, os.path as path, tarfile
+from bes.testing.unit_test import unit_test
+from bes.fs.file_find import file_find
+from bes.fs.file_copy import file_copy
+from bes.fs.temp_file import temp_file
+
+class test_file_copy(unit_test):
+
+  __unit_test_data_dir__ = '${BES_TEST_DATA_DIR}/bes.fs/tar_util'
+
+  def test_copy_tree(self):
+    self.maxDiff = None
+    src_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
+    dst_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
+    with tarfile.open(self.data_path('test.tar'), mode = 'r') as f:
+      f.extractall(path = src_tmp_dir)
+    file_copy.copy_tree(src_tmp_dir, dst_tmp_dir)
+    
+    expected_files = [
+      '1',
+      '1/2',
+      '1/2/3',
+      '1/2/3/4',
+      '1/2/3/4/5',
+      '1/2/3/4/5/apple.txt',
+      '1/2/3/4/5/kiwi.txt',
+      'bar.txt',
+      'empty',
+      'foo.txt',
+      'kiwi_link.txt',
+    ]
+    actual_files = file_find.find(dst_tmp_dir, file_type = file_find.ANY)
+    self.assertEqual( expected_files, actual_files )
+    
+  def test_copy_tree_and_excludes(self):
+    self.maxDiff = None
+    src_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
+    dst_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
+    with tarfile.open(self.data_path('test.tar'), mode = 'r') as f:
+      f.extractall(path = src_tmp_dir)
+    file_copy.copy_tree(src_tmp_dir, dst_tmp_dir, excludes = [ 'bar.txt', 'foo.txt' ])
+    
+    expected_files = [
+      '1',
+      '1/2',
+      '1/2/3',
+      '1/2/3/4',
+      '1/2/3/4/5',
+      '1/2/3/4/5/apple.txt',
+      '1/2/3/4/5/kiwi.txt',
+      'empty',
+      'kiwi_link.txt',
+    ]
+    actual_files = file_find.find(dst_tmp_dir, file_type = file_find.ANY)
+    self.assertEqual( expected_files, actual_files )
+
+  def test_copy_tree_spaces_in_filenames(self):
+    self.maxDiff = None
+    src_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG, suffix = '-has 2 spaces-')
+    dst_tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG, suffix = '-has 2 spaces-')
+    with tarfile.open(self.data_path('test.tar'), mode = 'r') as f:
+      f.extractall(path = src_tmp_dir)
+    file_copy.copy_tree(src_tmp_dir, dst_tmp_dir)
+    
+    expected_files = [
+      '1',
+      '1/2',
+      '1/2/3',
+      '1/2/3/4',
+      '1/2/3/4/5',
+      '1/2/3/4/5/apple.txt',
+      '1/2/3/4/5/kiwi.txt',
+      'bar.txt',
+      'empty',
+      'foo.txt',
+      'kiwi_link.txt',
+    ]
+    actual_files = file_find.find(dst_tmp_dir, file_type = file_find.ANY)
+    self.assertEqual( expected_files, actual_files )
+    
+if __name__ == '__main__':
+  unit_test.main()
