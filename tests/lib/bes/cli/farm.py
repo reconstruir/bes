278ca272e3a4d0ca9b59740ef712042ad21e73e1
Ramiro Estrugo <restrugo@imvu.com>
2019-06-18 17:31:30 -0700

    add version_cli metadata

--- tests/lib/bes/cli/farm.py
+++ tests/lib/bes/cli/farm.py
@@ -3,5 +3,12 @@
 
 from farm_cli import farm_cli
 
+__author__ = 'chupacabra@example.com'
+__bes_address__ = 'https://www.example.com/'
+__bes_author_name__ = 'Chupacabra'
+__bes_tag__ = 'master'
+__bes_timestamp__ = 'yesterday'
+__version__ = '6.6.6'
+
 if __name__ == '__main__':
   farm_cli.run()
