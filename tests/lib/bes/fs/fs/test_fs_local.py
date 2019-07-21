a64c72c5e9fffbe9d4de17b48ee2c3eb32e728bf
Ramiro Estrugo <restrugo@imvu.com>
2019-07-21 13:23:25 -0700

    add a test of file_info

--- tests/lib/bes/fs/fs/test_fs_local.py
+++ tests/lib/bes/fs/fs/test_fs_local.py
@@ -12,16 +12,16 @@ from bes.fs.fs.fs_local import fs_local
 
 class test_fs_local(unit_test):
 
+  _TEST_ITEMS = [
+    'file foo.txt "foo.txt\n"',
+    'file subdir/bar.txt "bar.txt\n"',
+    'file subdir/subberdir/baz.txt "baz.txt\n"',
+    'file emptyfile.txt',
+    'dir emptydir',
+  ]
+  
   def test_list_dir(self):
-    tmp_dir = self._make_temp_content([
-      'file foo.txt "foo.txt\n"',
-      'file subdir/bar.txt "bar.txt\n"',
-      'file subdir/subberdir/baz.txt "baz.txt\n"',
-      'file emptyfile.txt',
-      'dir emptydir',
-    ])
-
-    fs = fs_local(tmp_dir)
+    fs = self._make_temp_fs()
     self.assertEqual( [
       ( 'emptyfile.txt', 0, 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', {} ),
       ( 'foo.txt', 8, 'b6a5ff9795209b3d64cb5c04d574515413f9fec7abde49d66b44de90d1e0db14', {} ),
@@ -37,10 +37,21 @@ class test_fs_local(unit_test):
     file_util.remove(tmp_dir)
     fs = fs_local(tmp_dir)
     self.assertEqual( [], fs.list_dir('/', False) )
+
+  def test_file_info(self):
+    fs = self._make_temp_fs()
+    self.assertEqual(
+      ( 'foo.txt', 8, 'b6a5ff9795209b3d64cb5c04d574515413f9fec7abde49d66b44de90d1e0db14', {} ),
+      fs.file_info('foo.txt') )
     
   @classmethod
   def _make_temp_content(clazz, items):
     return temp_content.write_items_to_temp_dir(items, delete = not clazz.DEBUG)
+
+  @classmethod
+  def _make_temp_fs(self):
+    tmp_dir = self._make_temp_content(self._TEST_ITEMS)
+    return fs_local(tmp_dir)
   
 if __name__ == '__main__':
   unit_test.main()
