12771653112473d60f9eb66485e682144cf4ca2f
Ramiro Estrugo <restrugo@imvu.com>
2019-08-01 21:16:52 -0700

    add to_string()

--- lib/bes/fs/fs/fs_file_info.py
+++ lib/bes/fs/fs/fs_file_info.py
@@ -3,8 +3,10 @@
 from collections import namedtuple
 from bes.common.check import check
 from bes.property.cached_property import cached_property
+from bes.compat.StringIO import StringIO
 
 from .fs_error import fs_error
+from .fs_list_options import fs_list_options
 
 class fs_file_info(namedtuple('fs_file_info', 'filename, ftype, size, checksum, attributes, children')):
 
@@ -32,6 +34,9 @@ class fs_file_info(namedtuple('fs_file_info', 'filename, ftype, size, checksum, 
   def __iter__(self):
     return iter(self.children)
 
+  def __str__(self):
+    return self.to_string()
+
   @cached_property
   def display_filename(self):
     if self.ftype == self.DIR:
@@ -46,5 +51,31 @@ class fs_file_info(namedtuple('fs_file_info', 'filename, ftype, size, checksum, 
   
   def is_file(self):
     return self.ftype == self.FILE
+
+  def to_string(self, options = None):
+    options = options or fs_list_options()
+    buf = StringIO()
+    self._entry_to_string(self, buf, options, 0)
+    return buf.getvalue()
+
+  @classmethod
+  def _entry_to_string(clazz, entry, buf, options, depth):
+    indent = '  ' * depth
+    if entry.ftype == 'file':
+      if options.show_details:
+        buf.write('{}{} {} {} {} {}'.format(indent, entry.display_filename, entry.ftype, entry.size, entry.checksum, entry.attributes))
+      else:
+        buf.write('{}{}'.format(indent, entry.display_filename))
+      buf.write('\n')
+    elif entry.ftype == 'dir':
+      for child in entry:
+        if options.show_details:
+          buf.write('{}{} {} {} {} {}'.format(indent, child.display_filename, child.ftype, child.size, child.checksum, child.attributes))
+        else:
+          buf.write('{}{}'.format(indent, child.display_filename))
+          
+        buf.write('\n')
+        if child.is_dir():
+          clazz._entry_to_string(child, buf, options, depth + 1)
   
 check.register_class(fs_file_info, include_seq = False)
