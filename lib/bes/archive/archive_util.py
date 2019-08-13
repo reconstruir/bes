300bec41dfaaa0452844e1917e278ee11a761cd4
Ramiro Estrugo <restrugo@imvu.com>
2019-08-13 14:45:48 -0700

    add duplicate_members and unit tests.

--- lib/bes/archive/archive_util.py
+++ lib/bes/archive/archive_util.py
@@ -4,6 +4,7 @@ import os.path as path
 from bes.fs.temp_file import temp_file
 from bes.fs.file_util import file_util
 from bes.common.object_util import object_util
+from bes.common.dict_util import dict_util
 
 from .archiver import archiver
 
@@ -12,6 +13,7 @@ class archive_util(object):
 
   @classmethod
   def remove_members(clazz, archive, members, debug = False):
+    'Remove memvers from an archive and then recreate it.'
     members = object_util.listify(members)
     tmp_dir = archiver.extract_all_temp_dir(archive, delete = not debug)
     if debug:
@@ -19,3 +21,33 @@ class archive_util(object):
     members = [ path.join(tmp_dir, m) for m in members ]
     file_util.remove(members)
     archiver.create(archive, tmp_dir)
+
+  @classmethod
+  def duplicate_members(clazz, archives):
+    '''
+    Return a dict of members in archives that are duplicates in this form:
+    {
+      'foo.txt': { 'archive1.zip', 'archive2.zip' },
+      'bar.txt': { 'archive3.zip', 'archive4.zip', 'archive5.zip' },
+    }
+    '''
+    counts = {}
+    archive_to_members = {}
+    for archive in archives:
+      archive_to_members[archive] = set(archiver.members(archive))
+      
+    for archive in archives:
+      for member in archive_to_members[archive]:
+        if not member in counts:
+          counts[member] = 1
+        else:
+          counts[member] += 1
+    result = {}
+    for key, value in counts.items():
+      if value > 1:
+        if key not in result:
+          result[key] = set()
+        for archive, members in archive_to_members.items():
+          if key in members:
+            result[key].add(archive)
+    return result
--- tests/lib/bes/archive/test_archive_util.py
+++ tests/lib/bes/archive/test_archive_util.py
@@ -31,5 +31,31 @@ class test_archive_util(unit_test):
       'foo-1.2.3/fruits/kiwi.txt',
     ], archiver.members(tmp_archive))
     
+  def test_duplicate_members(self):
+    a1 = temp_archive.make_temp_archive(temp_archive.make_temp_item_list([
+      ( self.xp_path('foo-1.2.3/fruits/apple.txt'), 'apple.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/durian.txt'), 'durian.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/kiwi.txt'), 'kiwi.txt' ),
+    ]), 'zip', delete = not self.DEBUG)
+    a2 = temp_archive.make_temp_archive(temp_archive.make_temp_item_list([
+      ( self.xp_path('foo-1.2.3/fruits/apple.txt'), 'apple.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/lemon.txt'), 'lemon.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/melon.txt'), 'melon.txt' ),
+      ( self.xp_path('foo-1.2.3/wine/barolo.txt'), 'barolo.txt' ),
+      ( self.xp_path('foo-1.2.3/cheese/brie.txt'), 'brie.txt' ),
+    ]), 'zip', delete = not self.DEBUG)
+    a3 = temp_archive.make_temp_archive(temp_archive.make_temp_item_list([
+      ( self.xp_path('foo-1.2.3/fruits/strawberry.txt'), 'strawberry.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/blueberry.txt'), 'blueberry.txt' ),
+      ( self.xp_path('foo-1.2.3/fruits/banana.txt'), 'banana.txt' ),
+      ( self.xp_path('foo-1.2.3/wine/barolo.txt'), 'barolo.txt' ),
+      ( self.xp_path('foo-1.2.3/cheese/brie.txt'), 'brie.txt' ),
+    ]), 'zip', delete = not self.DEBUG)
+    self.assertEqual( {
+      'foo-1.2.3/cheese/brie.txt': { a2, a3 },
+      'foo-1.2.3/fruits/apple.txt': { a1, a2 },
+      'foo-1.2.3/wine/barolo.txt': { a2, a3 },
+    }, archive_util.duplicate_members([ a1, a2, a3 ]) )
+    
 if __name__ == "__main__":
   unit_test.main()
