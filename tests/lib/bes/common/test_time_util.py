e9368d136231547279c0a2ccb4b9d51c09abde36
Ramiro Estrugo <restrugo@imvu.com>
2019-09-28 01:49:54 -0700

    timestmap wip

--- tests/lib/bes/common/test_time_util.py
+++ tests/lib/bes/common/test_time_util.py
@@ -9,15 +9,7 @@ from bes.common.time_util import time_util
 
 class test_time_util(unittest.TestCase):
 
-  def test_timestamp_parse(self):
-    f = time_util.timestamp_parse
-    self.assertEqual( DT(1999, 1, 1, 1, 1, 1), f('1999-01-01-01-01-01') )
-    self.assertEqual( DT(1999, 1, 1, 1, 1, 1), f('1999:01:01:01:01:01', delimiter = ':' ) )
-    self.assertEqual( DT(1999, 1, 1, 1, 1, 1, .66), f('1999-01-01-01-01-01-660000', milliseconds = True) )
-  
-  def xtest_format_for_timestamp(self):
-    f = time_util.format_for_timestamp
-    self.assertEqual( '1999-01-01-01-01-01', f(datetime(year = 1999, month = 1, day = 1, hour = 1, minute = 1, second = 1)) )
+  pass
 
 if __name__ == "__main__":
   unittest.main()
