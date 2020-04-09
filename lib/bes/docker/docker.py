8c7ef5ce1e7b46521f73b562320470577fd99641
Ramiro Estrugo <restrugo@imvu.com>
2020-04-09 14:15:59 -0700

    add ability to override whether running under docker for unit testing

--- lib/bes/docker/docker.py
+++ lib/bes/docker/docker.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from os import path
+from functools import wraps
 
 from bes.system.host import host
 from bes.fs.file_util import file_util
@@ -10,9 +11,13 @@ class docker(object):
   'Class to deal with docker.'
 
   _CGROUPS_FILE = '/proc/1/cgroup'
+  _FORCE_IS_RUNNING_UNDER_DOCKER = None
   
   @classmethod
   def is_running_inside_docker(clazz):
+    if clazz._FORCE_IS_RUNNING_UNDER_DOCKER != None:
+      return clazz._FORCE_IS_RUNNING_UNDER_DOCKER
+    
     if not host.is_linux():
       return False
 
@@ -25,3 +30,27 @@ class docker(object):
   @staticmethod
   def raise_skip_if_running_under_docker():
     raise_skip_if(not docker.is_running_inside_docker(), 'running under docekr')
+
+class is_running_under_docker_override(object):
+  'A class with context support for overriding whether running under docker.  Mostly for unit testing.'
+  
+  def __init__(self, running_under_docker):
+    assert isinstance(running_under_docker, bool)
+    self._running_under_docker = running_under_docker
+    
+  def __enter__(self):
+    docker._FORCE_IS_RUNNING_UNDER_DOCKER = self._running_under_docker
+    return self
+  
+  def __exit__(self, type, value, traceback):
+    docker._FORCE_IS_RUNNING_UNDER_DOCKER = None
+
+def is_running_under_docker_override_func(running_under_docker):
+  'A decarator that calls is_running_under_docker_override.'
+  def _wrap(func):
+    @wraps(func)
+    def _caller(self, *args, **kwargs):
+      with is_running_under_docker_override(running_under_docker) as over:
+        return func(self, *args, **kwargs)
+    return _caller
+  return _wrap
