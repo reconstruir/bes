d2bf6a525571c9e688e8f12c306cd57d1013a82e
Ramiro Estrugo <restrugo@imvu.com>
2020-05-04 07:46:08 -0700

    throw an error if the file does not exists

--- lib/bes/fs/detail/file_mime_type_unix_file_exe.py
+++ lib/bes/fs/detail/file_mime_type_unix_file_exe.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+from os import path
 import re
 from bes.compat.map import map
 from bes.system.execute import execute
@@ -13,6 +14,8 @@ class file_mime_type_unix_file_exe(object):
   @classmethod
   def mime_type(clazz, filename):
     cmd = 'file --brief --mime %s' % (filename)
+    if not path.isfile(filename):
+      raise IOError('file not found: "{}"'.format(filename))
     rv = execute.execute(cmd, raise_error = False)
     if rv.exit_code != 0:
       return ( None, None )
