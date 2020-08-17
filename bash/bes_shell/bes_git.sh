f65e94129e61955d5290d9beb69c05c04915600f
Ramiro Estrugo <restrugo@imvu.com>
2020-08-16 22:16:25 -0700

    Squashed 'bes_shell/' changes from d01ec90a..7a72508c
    7a72508c retire
    12c97304 add unit test that proves subtree works when there are conflicts in the dst repo
    48bf670c fix it for the latest subtree interface
    f7097555 add unit tests that prove that both specific tag and @latest@ work
    82abc987 print failed if failed
    cce9364f fix messaging for successful cases
    09033635 add support for windows
    cf2a5c78 less verbose by default.  move debug messages to BES_DEBUG=1
    176eab92 working subtree and unit test
    e2618257 wip
    f949decd git_subtree unit test skeleton
    10e71868 version not needed for git.
    6168501c fix a git unit tests refactor subtree to be a function first and the script now calls that
    2b8271be add support for the latest repo tag
    9c01ae10 add bes_git_repo_latest_tag and unit test
    210a070e add
    59a590be add windows support
    29fd8a4f add bes_git_commit_date
    9b2d3984 add bes_question_yes_no()
    5c8623a2 add bes_git_commit_message() and unit tests
    7ee96144 fix the version comparison to be string like instead of arithmetic
    0f35109d add functions for comparing version numbers
    124c706e redirect to the log file variable to be able to debug stuff
    d4922aba add test_bes_git_repo_commit_for_ref() and unit tests
    cbe3118a aff bes_git_commit_for_ref() and unit tests
    51c67d12 move bes_version_bump_prefixed from git to version
    dd2bd2e8 add bes_git_bump_prefixed_tag and unit test
    58bdf066 add test_bes_str_remove_tail() and unit tests
    85cc68d5 add test_bes_str_remove_head() and unit test
    f40b020f add bes_git_greatest_remote_prefixed_tag() and unit test
    83665c96 add test_bes_git_list_remote_prefixed_tags() and unit tests
    d0425a0b add bes_str_starts_with() and unit test
    d4ebcce6 s/tag/tag_name
    8a644579 add functions to bump versions
    9b332505 add bes_version moduel and unit tests
    7890744a add bes_str_is_integer() and unit test
    aff1a518 add has tag tests
    41af903a add a list tags test
    f6ea4370 wip for git tag unit tests also enhance the git framework to allow logging to a file if BES_GIT_LOG_FILE is set.
    9321408b wip for a tag api
    fc77da6f add support for setting a BES_GIT_EXE environment variable to be able to override the git exe.  Plus a unit test that proves it works.
    0821138e fix downloads that result in 302 codes to work by not using the curl --silent flag and instead redirecting stderr out of the way.  Fix a typo in a method help
    29ce1840 if running in CI (jenkins or bitbucket) then dont ever write to the console
    40031925 add bes_git_lfs_file_needs_pull() and fix a typo in a comment.
    ace06249 add bes_str_split and unit test
    
    git-subtree-dir: bes_shell
    git-subtree-split: 7a72508cfc0463c7af2d8dee6cb9a373cb490dd6

--- bash/bes_shell/bes_git.sh
+++ bash/bes_shell/bes_git.sh
@@ -2,6 +2,8 @@
 
 _bes_trace_file "begin"
 
+_BES_GIT_LOG_FILE=${BES_GIT_LOG_FILE:-/dev/null}
+
 # Return 0 if ${1} (or pwd if not given) is a bare git repo
 function bes_git_is_bare_repo()
 {
@@ -121,7 +123,7 @@ function bes_git_repo_has_uncommitted_changes()
   if ! bes_git_is_repo "${_path}"; then
     return 1
   fi
-  if $(cd "${_path}" && git diff-index --quiet HEAD --); then
+  if $(cd "${_path}" && ${BES_GIT_EXE:-git} diff-index --quiet HEAD --); then
     return 1
   fi
   return 0
@@ -143,7 +145,7 @@ function bes_git_repo_has_untracked_files()
   if ! bes_git_is_repo "${_path}"; then
     return 1
   fi
-  local _num=$(cd "${_path}" && git status --untracked=all --porcelain | grep "?? " | wc -l)
+  local _num=$(cd "${_path}" && ${BES_GIT_EXE:-git} status --untracked=all --porcelain | grep "?? " | wc -l)
   if [[ $(expr ${_num}) > 0 ]]; then
     return 0
   fi
@@ -177,7 +179,7 @@ function bes_git_call()
   fi
   local _repo="${1}"
   shift
-  ( cd "${_repo}" && git ${1+"$@"} )
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} ${1+"$@"} )
   return $?
 }
 
@@ -189,7 +191,7 @@ function bes_git_local_branch_exists()
   fi
   local _root="${1}"
   local _branch_name="${2}"
-  if bes_git_call "${_root}" branch | sed -E 's/^\*/ /' | awk '{ print $1; }' | grep -w ${_branch_name} >& /dev/null; then
+  if bes_git_call "${_root}" branch | sed -E 's/^\*/ /' | awk '{ print $1; }' | grep -w ${_branch_name} >& "${_BES_GIT_LOG_FILE}"; then
     return 0
   fi
   return 1
@@ -217,7 +219,7 @@ function bes_git_remote_is_added()
   fi
   local _root="${1}"
   local _remote_name=${2}
-  if bes_git_call "${_root}" ls-remote --exit-code ${_remote_name} >& /dev/null; then
+  if bes_git_call "${_root}" ls-remote --exit-code ${_remote_name} >& "${_BES_GIT_LOG_FILE}"; then
     return 0
   fi
   return 1
@@ -226,7 +228,7 @@ function bes_git_remote_is_added()
 function bes_git_remote_remove()
 {
   if [[ $# != 2 ]]; then
-    bes_message "usage: bes_git_has_remote root remote_name"
+    bes_message "usage: bes_git_remote_remove root remote_name"
     return 1
   fi
   local _root="${1}"
@@ -251,6 +253,31 @@ function bes_git_last_commit_hash()
   fi
   if [[ ${_want_short_hash} == "true" ]]; then
     _hash=$(bes_git_short_hash "${_repo}" ${_long_hash})
+  else
+    _hash=${_long_hash}
+  fi
+  echo ${_hash}
+  return 0
+}  
+
+# return the commit hash for ref
+function bes_git_commit_for_ref()
+{
+  if [[ $# < 2 ]]; then
+    bes_message "usage: bes_git_commit_for_ref root_dir ref <short_hash>"
+    return 1
+  fi
+  local _root_dir="${1}"
+  shift
+  local _ref="${1}"
+  shift
+  local _short_hash="false"
+  if [[ $# > 1 ]]; then
+    _short_hash=${1}
+  fi
+  local _long_hash=$(bes_git_call "${_root_dir}" rev-list -n 1 ${_ref})
+  if [[ ${_short_hash} == "true" ]]; then
+    _hash=$(bes_git_short_hash "${_root_dir}" ${_long_hash})
   else
     _hash=${_long_hash}
   fi
@@ -301,7 +328,7 @@ function bes_git_submodule_init()
       _recursive_flag="--recursive"
     fi
   fi
-  git submodule update --init ${_recursive_flag} "${_submodule}"
+  ${BES_GIT_EXE:-git} submodule update --init ${_recursive_flag} "${_submodule}"
   return 0
 }
 
@@ -328,16 +355,16 @@ function bes_git_submodule_update()
   fi
     
   local _old_revision=$(bes_git_submodule_revision "${_repo}" ${_submodule} true)
-  ( cd "${_repo}" && git submodule update --init ${_submodule} )
-  ( cd "${_repo}" && git submodule update --remote --merge ${_submodule} )
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} submodule update --init ${_submodule} )
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} submodule update --remote --merge ${_submodule} )
   local _new_revision=$(bes_git_last_commit_hash "${_repo}/${_submodule}" true)
   if [[ ${_old_revision} == ${_new_revision} ]]; then
     bes_message "submodule ${_submodule} already at latest revision ${_new_revision}"
     return 0
   fi
-  ( cd "${_repo}" && git add ${1} )
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} add ${1} )
   local _message="submodule ${_submodule} updated from ${_old_revision} to ${_new_revision}"
-  ( cd "${_repo}" && git commit -m"${_message}" . )
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} commit -m"${_message}" . )
   bes_message "${_message}"
   return 0
 }
@@ -367,7 +394,7 @@ function bes_git_pack_size()
   else
     _repo="$(pwd)"
   fi
-  local _pack_size=$(bes_git_call "${_repo}" && git count-objects -v | grep size-pack  | awk '{ print $2; }')
+  local _pack_size=$(bes_git_call "${_repo}" && ${BES_GIT_EXE:-git} count-objects -v | grep size-pack  | awk '{ print $2; }')
   echo ${_pack_size}
   return 0
 }
@@ -387,7 +414,7 @@ function bes_git_gc()
   fi
   local _pack_size_before=$(bes_git_pack_size "${_repo}")
   local _gc_log="$(pwd)"/gc.log
-  ( cd "${_repo}" && git reflog expire --expire=now --all && git gc --prune=now --aggressive ) >& ${_gc_log}
+  ( cd "${_repo}" && ${BES_GIT_EXE:-git} reflog expire --expire=now --all && ${BES_GIT_EXE:-git} gc --prune=now --aggressive ) >& ${_gc_log}
   local _pack_size_after=$(bes_git_pack_size "${_repo}")
   bes_message "bes_git_gc: delta: before=${_pack_size_before} after=${_pack_size_after} gc_log=${_gc_log}"
   return 0
@@ -420,7 +447,7 @@ function bes_git_gc2()
   return 0
 }
 
-# Return true (1) if the repo has git lfs files
+# Return true (0) if the repo has git lfs files
 function bes_git_repo_has_lfs_files()
 {
   local _repo=
@@ -434,6 +461,212 @@ function bes_git_repo_has_lfs_files()
     return 0
   fi
   return 1
+}
+
+# Return true (0) if a git lfs file needs pulling
+function bes_git_lfs_file_needs_pull()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_lfs_file_needs_pull repo filename"
+    return 1
+  fi
+  local _repo="${1}"
+  local _filename="${2}"
+  local _found=$(cd "${_repo}" && ${BES_GIT_EXE:-git} lfs ls-files | grep -w "${_filename}")
+  if [[ -z "${_found}" ]]; then
+    bes_message "lfs file ${_filename} not found in ${_repo}"
+    return 1
+  fi
+  local _status=$(echo "${_found}" | awk '{ print $2; }')
+  if [[ ${_status} == "*" ]]; then
+    return 1
+  fi
+  return 0
+}
+
+# Return all the remote tags ordered by software version
+function bes_git_list_remote_tags()
+{
+  if [[ $# != 1 ]]; then
+    echo "usage: bes_git_list_remote_tags root_dir"
+    return 1
+  fi
+  local _root_dir="${1}"
+  if ! bes_git_is_repo "${_root_dir}"; then
+    bes_message "not a git repo: ${_root_dir}"
+    return 1
+  fi
+  bes_git_call "${_root_dir}" \
+    ls-remote --tags --sort=version:refname 2> "${_BES_GIT_LOG_FILE}" | \
+    awk '{ print $2; }' | \
+    sed 's/refs\/tags\///'
+  return 0
+}
+
+function bes_git_greatest_remote_tag()
+{
+  if [[ $# != 1 ]]; then
+    echo "usage: bes_git_greatest_remote_tag root_dir"
+    return 1
+  fi
+  local _root_dir="${1}"
+  if ! bes_git_is_repo "${_root_dir}"; then
+    bes_message "not a git repo: ${_root_dir}"
+    return 1
+  fi
+  bes_git_list_remote_tags "${_root_dir}" | tail -1
+  return 0
+}
+
+function bes_git_has_remote_tag()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_has_remote_tag root_dir tag"
+    return 1
+  fi
+  local _root_dir="${1}"
+  local _tag="${2}"
+  if bes_git_list_remote_tags "${_root_dir}" | grep ${_tag} >& "${_BES_GIT_LOG_FILE}"; then
+    return 0
+  fi
+  return 1
+}
+
+function bes_git_tag()
+{
+  if [[ $# < 2 ]]; then
+    echo "usage: bes_git_tag root_dir tag_name <commit>"
+    return 1
+  fi
+  local _root_dir="${1}"
+  shift
+  local _tag_name="${1}"
+  shift
+  local _commit=
+  if [[ $# > 1 ]]; then
+    _commit=${1}
+  fi
+  
+  if ! bes_git_is_repo "${_root_dir}"; then
+    bes_message "not a git repo: ${_root_dir}"
+    return 1
+  fi
+  if bes_git_has_remote_tag "${_root_dir}" ${_tag_name}; then
+    bes_message "tag already exists in remote: ${_tag_name}"
+    return 1
+  fi
+  bes_git_call "${_root_dir}" tag ${_tag_name} ${_commit} >& "${_BES_GIT_LOG_FILE}"
+  bes_git_call "${_root_dir}" push origin ${_tag_name} >& "${_BES_GIT_LOG_FILE}"
+  bes_git_call "${_root_dir}" fetch --tags >& "${_BES_GIT_LOG_FILE}"
+  return 0
+}
+
+# Return all the remote tags that start with prefix
+function bes_git_list_remote_prefixed_tags()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_list_remote_prefixed_tags root_dir prefix"
+    return 1
+  fi
+  local _root_dir="${1}"
+  local _prefix="${2}"
+  
+  if ! bes_git_is_repo "${_root_dir}"; then
+    bes_message "not a git repo: ${_root_dir}"
+    return 1
+  fi
+
+  local _all_tags=( $(bes_git_list_remote_tags ${_root_dir}) )
+  local _tag
+  for _tag in "${_all_tags[@]}"; do
+    if bes_str_starts_with ${_tag} ${_prefix}; then
+      echo ${_tag}
+    fi
+  done
+  return 0
+}
+
+function bes_git_greatest_remote_prefixed_tag()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_greatest_remote_prefixed_tag root_dir prefix"
+    return 1
+  fi
+  local _root_dir="${1}"
+  local _prefix="${2}"
+  
+  if ! bes_git_is_repo "${_root_dir}"; then
+    bes_message "not a git repo: ${_root_dir}"
+    return 1
+  fi
+  
+  bes_git_list_remote_prefixed_tags "${_root_dir}" ${_prefix} | tail -1
+  
+  return 0
+}
+
+function bes_git_repo_commit_for_ref()
+{
+  if [[ $# != 2 ]]; then
+    bes_message "usage: bes_git_repo_commit_for_ref address ref"
+    return 1
+  fi
+  local _address=${1}
+  local _ref=${2}
+  local _tmp=/tmp/bes_git_repo_commit_for_ref_$$
+  rm -rf "${_tmp}"
+  mkdir -p "${_tmp}"
+  ${BES_GIT_EXE:-git} clone ${_address} "${_tmp}" >& "${_BES_GIT_LOG_FILE}"
+  local _commit_hash=$(bes_git_commit_for_ref "${_tmp}" ${_ref})
+  rm -rf "${_tmp}"
+  echo ${_commit_hash}
+  return 0
+}
+
+# print the latest tag in a repo by address
+function bes_git_repo_latest_tag()
+{
+  if [[ $# != 1 ]]; then
+    bes_message "usage: bes_git_repo_latest_tag address"
+    return 1
+  fi
+  local _address=${1}
+  local _tmp=/tmp/bes_git_repo_latest_tag_$$
+  rm -rf "${_tmp}"
+  mkdir -p "${_tmp}"
+  git clone ${_address} "${_tmp}" >& /dev/null
+  local _latest_tag=$(cd "${_tmp}" && git tag -l --sort=version:refname | tail -1)
+  rm -rf "${_tmp}"
+  echo ${_latest_tag}
+  return 0
+}
+
+# Print the date for commit
+function bes_git_commit_date()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_commit_message root_dir ref"
+    return 1
+  fi
+  local _root_dir="${1}"
+  local _commit="${2}"
+  local _message=$(bes_git_call "${_root_dir}" log -n 1 --format=%ai ${_commit})
+  echo ${_message}
+  return 0
+}
+
+# Print the message for commit
+function bes_git_commit_message()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: bes_git_commit_message root_dir ref"
+    return 1
+  fi
+  local _root_dir="${1}"
+  local _commit="${2}"
+  local _message=$(bes_git_call "${_root_dir}" log -n 1 --pretty=format:%s ${_commit})
+  echo ${_message}
+  return 0
 }
 
 _bes_trace_file "end"
