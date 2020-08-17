8386a056638dbf1122869021ff984e15a1d97468
Ramiro Estrugo <restrugo@imvu.com>
2020-08-16 22:17:27 -0700

    skeleton

---
+++ lib/bes/git/git_change_info.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from collections import namedtuple
+
+from bes.common.check import check
+
+from .git_error import git_error
+
+class git_change_info(namedtuple('git_change_info', 'uncommitted, untracked, unpushed')):
+  'A class to deal with git head info.'
+
+  def __new__(clazz, uncommitted, untracked, unpushed):
+    check.check_bool(uncommitted)
+    check.check_bool(untracked)
+    check.check_bool(unpushed)
+    return clazz.__bases__[0].__new__(clazz, uncommitted, untracked, unpushed)
+
+  def __str__(self):
+    return ''
+
