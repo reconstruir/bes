9b4d39ff79f6cab016e025d413f8f9926d801874
Ramiro Estrugo <restrugo@imvu.com>
2020-08-29 16:24:05 -0700

    add git_ref_info to hold the outout of show-ref and unit tests

---
+++ tests/lib/bes/git/test_git_ref_info.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+
+from bes.git.git_ref_info import git_ref_info
+from bes.git.git_error import git_error
+
+class test_git_ref_info(unit_test):
+
+  def test_parse_show_ref_output_tag(self):
+    f = git_ref_info.parse_show_ref_output
+
+    output = '''\
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/tags/1.2.3
+'''
+    self.assertEqual( ( '1.2.3', 'refs/tags/1.2.3', 'tag', 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeef', 'deadbee', False),
+                      f(output) )
+
+  def test_parse_show_ref_output_branch(self):
+    f = git_ref_info.parse_show_ref_output
+
+    output = '''\
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/heads/master
+'''
+    self.assertEqual( ( 'master', 'refs/heads/master', 'branch', 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeef', 'deadbee', False),
+                      f(output) )
+    
+  def test_parse_show_ref_output_branch_with_remote(self):
+    f = git_ref_info.parse_show_ref_output
+
+    output = '''\
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/heads/master
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/remotes/origin/master
+'''
+    self.assertEqual( ( 'master', 'refs/heads/master', 'branch', 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeef', 'deadbee', True),
+                      f(output) )
+    
+  def test_parse_show_ref_output_tag_that_is_also_branch(self):
+    f = git_ref_info.parse_show_ref_output
+
+    output = '''\
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/tags/master
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/heads/master
+deadbeefdeadbeefdeadbeefdeadbeefdeadbeef refs/remotes/origin/master
+'''
+    with self.assertRaises(git_error) as ctx:
+      f(output)
+      self.assertTrue( 'Ref that is both a branch and tag is not supported' in str(ctx.exception) )
+    
+if __name__ == '__main__':
+  unit_test.main()
