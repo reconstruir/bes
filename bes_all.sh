66684117a98da80d0c1efde26f2a644a96f544e9
Ramiro Estrugo <restrugo@imvu.com>
2020-11-14 22:07:32 -0800

    Squashed 'bes_shell/' changes from 727146a7..3c24f834
    3c24f834 fix the list all case and add unit tests
    1f0ac7db mostly working hand rolled which implementation for when /usr/bin/which is not found
    47096ff4 add section line number return to the find entry api
    83e1fc41 wip for set
    dc72d54c fix the tmp config creation function
    4db1cd38 add bes_config_has_section and unit tests
    8becadde use the new tokenizer for the parser for find entry
    06daea84 working tokenize and unit test
    18cbb60b working _bes_config_parse_entry and unit tests
    168ccb54 wip for tokenizer
    7623fd95 working escape and unescape for later using in config file tokenizer
    53385351 fix delimiter wrong bug when no partition is found and enhance the unit test setup to make it easier to write tests
    d13df188 simplify the check functions by having them determine the label directly using FUNCNAME
    485cc766 reimplement as a state machine and add more unit tests
    d9bfa444 working bes_config_get impl
    706b59a4 add docs
    6f3d5882 add check for delimiter length
    f61cc0fa add bes_string_partition
    3a19d2e9 add
    f035c080 wip config and devenv
    c58f4813 fix bes_pipenv_version argument number check
    f3655ce3 fix bes_pipenv_call to properly print the stdout result
    e2c91827 wip
    195eb5d9 lots of pipenv wip to not crap droppings all over ~/ instead make everything include droppings, pip cache dir pipenv cache dir go to the project dir.
    99469d9e make sure to cleanup the bootstrap get-pip.py tmp file
    8bf89f5a dont rm temp git repos if BES_DEBUG is set
    c1c0a839 add BES_PIP_EXTRA_ARGS and use it in the unit test to disble pipe cache to deal with unit test side effects
    c07f4dc1 add BES_UNIT_TEST_FAIL option to fail unit tests right away better unit test failure printout
    6362a217 fix handful of linux issues with pip
    3f115020 lots of pip and pipenv wip
    7b5d46d7 rename to get rid of the "user" bit
    c2afb2e2 wip on pip module.
    2ca4b633 wip for system vs. user pipe functions
    68aa1eef working pipenv module and unit tests
    11a2469d working bes_pip_ensure and unit test
    1f8f1d81 working bes_pip_install and unit test
    bf08c473 add pip_exe_full_version() and unit test
    7deeff21 check both the builtin and use base dir when looking for pip
    f2667a8a a bunch of wip for dealing with pip and brew
    c905772e add user site and base and bin getters
    e6548532 wip install
    eb5fcc30 add _bes_python_macos_is_from_brew
    2d6eebd1 add bes_path_is_symlink and unit tests
    cc0c9a7d add _bes_python_macos_is_system_python and unit tests
    4c7f6c08 add bes_path_is_abs
    440301db add bes_python.sh
    9587f881 Merge branch 'master' of gitlab.com:rebuilder/bes_shell
    a066893b wip
    02f5e8a8 better failure if neither curl or wget are found
    a74ce334 add both version and full version and unit tests
    80c0391c add bes_python_exe_version with unit test
    df2f2be8 fix the num args
    9ffcfd87 Merge branch 'master' of gitlab.com:rebuilder/bes_shell
    ba0268fc add bes_str_ends_with
    05477856 wip
    1529056b add skeleton bes_python.sh to deal with python
    c40c78a5 fix remote tag list sorting
    7d20699e remove the caret and curly brackets from tags that have them.
    
    git-subtree-dir: bes_shell
    git-subtree-split: 3c24f8349ff19d5aa93141b5a9ff512755d1cbb8

--- bes_all.sh
+++ bes_all.sh
@@ -12,3 +12,6 @@ source $(_bes_all_this_dir)/bes_git_subtree.sh
 source $(_bes_all_this_dir)/bes_download.sh
 source $(_bes_all_this_dir)/bes_bfg.sh
 source $(_bes_all_this_dir)/bes_version.sh
+source $(_bes_all_this_dir)/bes_python.sh
+source $(_bes_all_this_dir)/bes_pip.sh
+source $(_bes_all_this_dir)/bes_pipenv.sh
