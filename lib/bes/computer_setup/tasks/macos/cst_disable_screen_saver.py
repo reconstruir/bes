a7d0baa33fb45463ff24f1c245cc09a9e68f0152
Ramiro Estrugo <restrugo@imvu.com>
2020-11-22 23:31:37 -0800

    rename to make the names more manageable

---
+++ lib/bes/computer_setup/tasks/macos/cst_disable_screen_saver.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.common.check import check
+
+from bes.computer_setup.computer_setup_task_base import computer_setup_task_base
+from bes.macos.defaults.defaults import defaults
+
+class cst_disable_screen_saver(computer_setup_task_base):
+
+  def __init__(self, *args, **kargs):
+    super(cst_disable_screen_saver, self).__init__(*args, **kargs)
+
+  #@abstractmethod
+  def name(self):
+    'Name for task.'
+    return 'disable_screen_saver'
+
+  #@abstractmethod
+  def description(self):
+    'Description for task.'
+    return 'Disable the screensaver'
+
+  #@abstractmethod
+  def average_duration(self):
+    'Average duration in seconds.'
+    return 0
+
+  @abstractmethod
+  def is_needed(self):
+    'Return True of the task needs to run.'
+    value = defaults.get_value('com.apple.screensaver', 'idleTime')
+    return value != '0'
+  
+  #@abstractmethod
+  def run(self):
+    'Run the task.'
+    defaults.set_value('com.apple.screensaver', 'idleTime', 0)
