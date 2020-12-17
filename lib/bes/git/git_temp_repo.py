ac2160d2e03244999792d26fc104bbd8013e600d
Ramiro Estrugo <restrugo@imvu.com>
2020-12-16 19:46:49 -0800

    config parsing skeleton

--- lib/bes/git/git_temp_repo.py
+++ lib/bes/git/git_temp_repo.py
@@ -1,12 +1,16 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 import inspect
+from collections import namedtuple
 
 from bes.common.check import check
+from bes.common.string_util import string_util
 from bes.fs.temp_file import temp_file
+from bes.fs.file_mode import file_mode
+from bes.config.simple_config import simple_config
 
-from .git_error import git_error
 from .git_repo import git_repo
+from .git_temp_repo_error import git_temp_repo_error
 
 class _method_caller(object):
 
@@ -81,7 +85,7 @@ class git_temp_repo(object):
     method_names = [ name for name in method_names if not name.startswith('_') ]
     for method_name in method_names:
       if hasattr(self, method_name):
-        raise git_error('{} already has method \"{}\"'.format(self, method_name))
+        raise git_temp_repo_error('{} already has method \"{}\"'.format(self, method_name))
       setattr(self, method_name, _method_caller(target, method_name))
       
   def make_temp_cloned_repo(self, prefix = None):
@@ -104,3 +108,139 @@ class git_temp_repo(object):
       check.check_string_seq(content)
       r.write_temp_content(content, commit = True, commit_message = commit_message)
     return r
+  
+  def apply_config_text(self, config_text):
+    check.check_string(config_text)
+
+    config = simple_config.from_text(config_text,
+                                     source = '<git_temp_repo>',
+                                     check_env_vars = False,
+                                     validate_key_characters = False)
+    cmds = []
+    for section in config:
+      cmd = self._parse_command(section)
+      cmds.append(cmd)
+
+    for cmd in cmds:
+      print(cmd)
+    return cmds
+
+  @classmethod
+  def _parse_command(clazz, section):
+    command_name = section.header_.name
+    handler_name = '_parse_command_{}'.format(command_name)
+    if not hasattr(clazz, handler_name):
+      raise git_temp_repo_error('unknown command: "{}"'.format(command_name))
+    handler = getattr(clazz, handler_name)
+    return handler(section)
+
+  _command_file = namedtuple('_command_file', 'filename, perm, content')
+  @classmethod
+  def _parse_command_file(clazz, section):
+    assert section.header_.name == 'file'
+
+    parts = string_util.split_by_white_space(section.header_.extra_text, strip = True)
+    if len(parts) != 1:
+      raise git_temp_repo_error('Invalid file section header: "{}"'.format(str(section.header_)))
+    filename = parts[0]
+    content = ''
+    if section.has_key('content'):
+      content = section.get_value('content')
+    perm = 0o644
+    if section.has_key('perm'):
+      perm = file_mode.parse_mode(section.get_value('perm'))
+    return clazz._command_file(filename, perm, content)
+
+  _command_add = namedtuple('_command_add', 'name, files, commit_alias, message')
+  @classmethod
+  def _parse_command_add(clazz, section):
+    assert section.header_.name == 'add'
+
+    parts = string_util.split_by_white_space(section.header_.extra_text, strip = True)
+    if len(parts) != 2:
+      raise git_temp_repo_error('Invalid "add" section header: "{}"'.format(str(section.header_)))
+    commit_alias = parts[0]
+    name = parts[1]
+
+    files = []
+    for entry in section:
+      message = ''
+      if entry.value.key == 'message':
+        message = entry.value.value
+      else:
+        perm = 0o644
+        filename = entry.value.key
+        content = entry.value.value or ''
+        for annotation in entry.annotations or []:
+          if annotation.key == 'perm':
+            perm = file_mode.parse_mode(annotation.value)
+        files.append(clazz._command_file(filename, perm, content))
+    return clazz._command_add(name, files, commit_alias, message)
+
+  _command_tag = namedtuple('_command_tag', 'name, tag_name, from_commit, annotation')
+  @classmethod
+  def _parse_command_tag(clazz, section):
+    assert section.header_.name == 'tag'
+
+    parts = string_util.split_by_white_space(section.header_.extra_text, strip = True)
+    if len(parts) != 2:
+      raise git_temp_repo_error('Invalid "tag" section header: "{}"'.format(str(section.header_)))
+    tag_name = parts[0]
+    name = parts[1]
+
+    from_commit = None
+    annotation = None
+    if section.has_key('from_commit'):
+      from_commit = section.get_value('from_commit')
+    if section.has_key('annotation'):
+      annotation = section.get_value('annotation')
+
+    if not from_commit:
+      raise git_temp_repo_error('"tag" missing "from_commit"')
+      
+    return clazz._command_tag(name, tag_name, from_commit, annotation)
+    
+  _command_branch = namedtuple('_command_branch', 'name, branch_name, from_commit, from_branch')
+  @classmethod
+  def _parse_command_branch(clazz, section):
+    assert section.header_.name == 'branch'
+
+    parts = string_util.split_by_white_space(section.header_.extra_text, strip = True)
+    if len(parts) != 2:
+      raise git_temp_repo_error('Invalid "branch" section header: "{}"'.format(str(section.header_)))
+    branch_name = parts[0]
+    name = parts[1]
+
+    from_commit = None
+    from_branch = None
+    if section.has_key('from_commit'):
+      from_commit = section.get_value('from_commit')
+    if section.has_key('from_branch'):
+      from_branch = section.get_value('from_branch')
+
+    if not from_commit and not from_branch:
+      raise git_temp_repo_error('"branch" missing one of "from_commit" or "from_branch"')
+
+    if from_commit and from_branch:
+      raise git_temp_repo_error('"branch" should have only one of "from_commit" or "from_branch"')
+    
+    return clazz._command_branch(name, branch_name, from_commit, from_branch)
+
+  _command_remove = namedtuple('_command_remove', 'name, filename')
+  @classmethod
+  def _parse_command_remove(clazz, section):
+    assert section.header_.name == 'remove'
+
+    parts = string_util.split_by_white_space(section.header_.extra_text, strip = True)
+    if len(parts) != 1:
+      raise git_temp_repo_error('Invalid "remove" section header: "{}"'.format(str(section.header_)))
+    name = parts[0]
+
+    filename = None
+    if section.has_key('filename'):
+      filename = section.get_value('filename')
+
+    if not filename:
+      raise git_temp_repo_error('"remove" missing one of "filename"')
+    
+    return clazz._command_remove(name, filename)
