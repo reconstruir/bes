db9b4ca1336ca1cdbd1af43e7a0aa63293560acc
Ramiro Estrugo <restrugo@imvu.com>
2020-12-16 23:31:03 -0800

    convert to properly testing the cli interface not just the cli_command interface

--- tests/lib/bes/git/test_git_cli_args.py
+++ tests/lib/bes/git/test_git_cli_args.py
@@ -9,12 +9,14 @@ from bes.fs.file_util import file_util
 from bes.fs.temp_file import temp_file
 from bes.system.env_override import env_override
 
-from bes.testing.unit_test import unit_test
+from bes.testing.program_unit_test import program_unit_test
 from bes.git.git_cli_command import git_cli_command
 from bes.git.git_cli_options import git_cli_options
 
-class test_git_cli_command(unit_test):
+class test_git_cli_command(program_unit_test):
 
+  _program = program_unit_test.resolve_program(__file__, '..', '..', '..', '..', 'bin', 'best.py')
+  
   @git_temp_home_func()
   def test_bump_tag(self):
     'Just a smoke test bes.git has more test cases for bump_tag'
@@ -26,24 +28,53 @@ class test_git_cli_command(unit_test):
     r2 = r1.make_temp_cloned_repo()
     self.assertEqual( None, r1.greatest_local_tag() )
 
-    options = git_cli_options()
-    options.root_dir = r1.root
-    
-    git_cli_command.bump_tag(options, 'revision', False, False, True, False)
+    args = [
+      'git',
+      'bump_tag',
+      '--root-dir', r1.root,
+      '--component', 'revision',
+      '--reset-lower',
+    ]
+    rv = self.run_program(self._program, args)
+    self.assertEqual(0, rv.exit_code)
     r2.pull()
     self.assertEqual( '1.0.0', r2.greatest_local_tag() )
 
-    git_cli_command.bump_tag(options, 'revision', False, False, True, False)
+    args = [
+      'git',
+      'bump_tag',
+      '--root-dir', r1.root,
+      '--component', 'revision',
+      '--reset-lower',
+    ]
+    rv = self.run_program(self._program, args)
+    self.assertEqual(0, rv.exit_code)
     r2.pull()
     self.assertEqual( '1.0.1', r2.greatest_local_tag() )
-  
-    git_cli_command.bump_tag(options, 'minor', False, False, True, False)
+
+    args = [
+      'git',
+      'bump_tag',
+      '--root-dir', r1.root,
+      '--component', 'minor',
+      '--reset-lower',
+    ]
+    rv = self.run_program(self._program, args)
+    self.assertEqual(0, rv.exit_code)
     r2.pull()
     self.assertEqual( '1.1.0', r2.greatest_local_tag() )
-    
-    git_cli_command.bump_tag(options, 'major', False, False, True, False)
+
+    args = [
+      'git',
+      'bump_tag',
+      '--root-dir', r1.root,
+      '--component', 'major',
+      '--reset-lower',
+    ]
+    rv = self.run_program(self._program, args)
+    self.assertEqual(0, rv.exit_code)
     r2.pull()
     self.assertEqual( '2.0.0', r2.greatest_local_tag() )
-    
+
 if __name__ == '__main__':
-  unit_test.main()
+  program_unit_test.main()
