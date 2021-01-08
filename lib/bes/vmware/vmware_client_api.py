5db4d09f0cdc3f1c7bb177608f4e6482e878dc0f
Ramiro Estrugo <restrugo@imvu.com>
2021-01-08 02:25:40 -0800

    working vms api

--- lib/bes/vmware/vmware_client_api.py
+++ lib/bes/vmware/vmware_client_api.py
@@ -1,6 +1,9 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import pprint, requests, sys, time
+import pprint
+import requests
+import sys
+import time
 
 from collections import namedtuple
 
@@ -18,14 +21,16 @@ class vmware_client_api(object):
   def __init__(self, address, auth):
     check.check_tuple(address)
     check.check_credentials(auth)
-    print(address)
+    
     self._address = address
     self._auth = auth
+    self._auth_tuple = ( self._auth.username, self._auth.password )
 
   @property
   def base_url(self):
     return 'http://{}:{}'.format(self._address[0], self._address[1])
 
+  _vm = namedtuple('_vm', 'vm_id, path')
   def vms(self):
     'Return a list of vms'
     url = self._make_url('api/vms')
@@ -35,13 +40,29 @@ class vmware_client_api(object):
     }
     
 #curl 'http://localhost:8697/api/vms' -X GET --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' -u"fred:FRED#1flint"
+#    params = {
+#      'q': 'name~"{}"'.format(ref_name),
+#    }
+#    params = params
 
-    auth = ( self._auth.username, self._auth.password )
-    self._log.log_d('url={} auth={} headers={}'.format(url, auth, headers))
-    response = requests.get(url, auth = auth, headers = headers)
-    print(response)
-    print(response.content)
-    return []
+#    $body = @{
+#        'name' = $newvmname;
+#        'parentId' = $sourcevmid
+#    }
+
+    self._log.log_d('url={} auth={} headers={}'.format(url, self._auth, headers))
+    response = requests.get(url, auth = self._auth_tuple, headers = headers)
+    self._log.log_d('vms: response={} url={} headers={}'.format(response,
+                                                                response.url,
+                                                                response.headers))
+    if response.status_code != 200:
+      raise vmware_error('Error querying: "{}": {}'.format(url, response.status_code))
+    response_data = response.json()
+    self._log.log_d('vms: response_data={}'.format(pprint.pformat(response_data)))
+    result = []
+    for item in response_data:
+      result.append(self._vm(item['id'], item['path']))
+    return result
   
   def _make_url(self, fragment):
     check.check_string(fragment)
