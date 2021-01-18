5d960ba0b10c9a25e67042ab427dea12d1ce3ef6
Ramiro Estrugo <restrugo@imvu.com>
2021-01-17 17:00:43 -0800

    make the sessions command handler more generic using getattr for the method handler.

--- lib/bes/vmware/vmware_session_cli_command.py
+++ lib/bes/vmware/vmware_session_cli_command.py
@@ -20,50 +20,55 @@ class vmware_session_cli_command(cli_command_handler):
   def __init__(self, cli_args):
     super(vmware_session_cli_command, self).__init__(cli_args, options_class = vmware_session_options)
     check.check_vmware_session_options(self.options)
-    self._session = vmware_session(port = None, credentials = None)
-    self._session.start()
-    self._commands = vmware_client_commands(self._session.client, self.options)
+    self._commands = None
 
-#    self._session.stop()
-
+  def _handle_session_command(self, command_name, *args, **kwargs):
+    session = vmware_session(port = None, credentials = None)
+    session.start()
+    commands = vmware_client_commands(session.client, self.options)
+    func = getattr(commands, command_name)
+    result = func(*args, **kwargs)
+    session.stop()
+    return result
+    
   def vms(self):
-    return self._commands.vms()
+    return self._handle_session_command('vms')
 
   def vm_settings(self, vm_id):
-    return self._commands.vm_settings(vm_id)
+    return self._handle_session_command('vm_settings', vm_id)
   
   def vm_config(self, vm_id, key):
-    return self._commands.vm_config(vm_id, key)
+    return self._handle_session_command('vm_config', vm_id, key)
 
   def vm_power(self, vm_id, state, wait):
-    return self._commands.vm_power(vm_id, state, wait)
+    return self._handle_session_command('vm_power', vm_id, state, wait)
 
   def request(self, endpoint, args):
-    return self._commands.request(endpoint, args)
+    return self._handle_session_command('request', endpoint, args)
   
   def vm_mac_address(self, vm_id):
-    return self._commands.vm_mac_address(vm_id)
+    return self._handle_session_command('vm_mac_address', vm_id)
 
   def vm_ip_address(self, vm_id):
-    return self._commands.vm_ip_address(vm_id)
+    return self._handle_session_command('vm_ip_address', vm_id)
 
   def vm_shared_folders(self, vm_id):
-    return self._commands.vm_shared_folders(vm_id)
+    return self._handle_session_command('vm_shared_folders', vm_id)
 
   def vm_update_shared_folder(self, vm_id, folder_id, host_path, flags):
-    return self._commands.vm_update_shared_folder(vm_id, folder_id, host_path, flags)
+    return self._handle_session_command('vm_update_shared_folder', vm_id, folder_id, host_path, flags)
 
   def vm_add_shared_folder(self, vm_id, folder_id, host_path, flags):
-    return self._commands.vm_add_shared_folder(vm_id, folder_id, host_path, flags)
+    return self._handle_session_command('vm_add_shared_folder', vm_id, folder_id, host_path, flags)
 
   def vm_delete_shared_folder(self, vm_id, folder_id):
-    return self._commands.vm_delete_shared_folder(vm_id, folder_id)
+    return self._handle_session_command('vm_delete_shared_folder', vm_id, folder_id)
 
   def vm_copy(self, vm_id, new_vm_id):
-    return self._commands.vm_copy(vm_id, new_vm_id)
+    return self._handle_session_command('vm_copy', vm_id, new_vm_id)
 
   def vm_delete(self, vm_id):
-    return self._commands.vm_delete(vm_id)
+    return self._handle_session_command('vm_delete', vm_id)
   
   def vm_restart(self, vm_id, wait):
-    return self._commands.vm_restart(vm_id)
+    return self._handle_session_command('vm_restart', vm_id, wait)
