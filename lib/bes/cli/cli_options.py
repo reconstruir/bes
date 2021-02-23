3dc806122bc97ae0b0e0237ec4700cd70b3de495
Ramiro Estrugo <restrugo@imvu.com>
2021-02-23 05:52:17 -0800

    refactor to not have any cut n paste code in config file reading

--- lib/bes/cli/cli_options.py
+++ lib/bes/cli/cli_options.py
@@ -11,6 +11,8 @@ from bes.common.check import check
 from bes.config.simple_config import simple_config
 from bes.system.log import logger
 
+_HINT_CACHE = {}
+
 class cli_options(with_metaclass(ABCMeta, object)):
 
   _log = logger('cli_options')
@@ -23,7 +25,7 @@ class cli_options(with_metaclass(ABCMeta, object)):
     self._log.log_d('__init__: config_values={}'.format(pprint.pformat(config_values)))
     self._do_update_values(config_values)
     self._log.log_d('__init__: kargs={}'.format(pprint.pformat(kargs)))
-    non_default_kargs = self._extract_non_default_values(kargs, default_values)
+    non_default_kargs = self._extract_valid_non_default_values(kargs, default_values)
     self._log.log_d('__init__: non_default_kargs={}'.format(pprint.pformat(non_default_kargs)))
     self._do_update_values(non_default_kargs)
     self.check_value_types()
@@ -70,11 +72,6 @@ class cli_options(with_metaclass(ABCMeta, object)):
     'Check the type of each option.'
     raise NotImplemented('check_value_types')
   
-  @classmethod
-  def _morph_value_type(clazz, values, key, type_class):
-    if key in values:
-      values[key] = type_class(values[key])
-  
   def _valid_keys(self):
     'Return a list of valid keys for values in these options'
     return [ key for key in self.__dict__.keys() ]
@@ -90,14 +87,29 @@ class cli_options(with_metaclass(ABCMeta, object)):
     'update values only with no checking'
     self._log.log_d('_do_update_values: update before: {}'.format(str(self)))
     for key, value in values.items():
-      self._log.log_d('_do_update_values: setattr({}, {}, {})'.format(id(self), key, value))
-      setattr(self, key, value)
+      self._do_set_value(key, value)
     self._log.log_d('_do_update_values: update after: {}'.format(str(self)))
+
+  def _get_value_type_hint(self, key):
+    global _HINT_CACHE
+    if not self.__class__ in _HINT_CACHE:
+      _HINT_CACHE[self.__class__] = self.value_type_hints()
+    value_type_hints = _HINT_CACHE[self.__class__]
+    return value_type_hints.get(key, None)
     
-  def _read_config_file(self, cli_values):
+  def _do_set_value(self, key, value):
+    'update one value'
+    self._log.log_d('_do_update_value: setattr({}, {}, {}:{})'.format(id(self), key, value, type(value)))
+    value_type_hint = self._get_value_type_hint(key)
+    if value_type_hint and value != None:
+      value = value_type_hint(value)
+    setattr(self, key, value)
+
+  @classmethod
+  def _read_config_file(clazz, cli_values):
     check.check_dict(cli_values)
 
-    config_file_key = self.config_file_key()
+    config_file_key = clazz.config_file_key()
     if not config_file_key:
       return {}
     
@@ -108,33 +120,10 @@ class cli_options(with_metaclass(ABCMeta, object)):
 
     if config_filename == None:
       return {}
+    return clazz._values_from_config_file(config_filename)
 
-    error_class = self.error_class()
-    if not path.exists(config_filename):
-      raise error_class('Config file not found: "{}"'.format(config_filename))
-      
-    if not path.isfile(config_filename):
-      raise error_class('Config file is not a file: "{}"'.format(config_filename))
-
-    try:
-      config = simple_config.from_file(config_filename)
-    except Exception as ex:
-      raise error_class(str(ex))
-
-    config_file_section = self.config_file_section()
-    if not config.has_section(config_file_section):
-      raise error_class('No section "{}" found in config file: "{}"'.format(config_file_section,
-                                                                                  config_filename))
-    section = config.section(config_file_section)
-    values = section.to_dict()
-
-    for hint_key, hint_class in self.value_type_hints().items():
-      self._morph_value_type(values, hint_key, hint_class)
-      
-    filtered_values = dict_util.filter_with_keys(values, self._valid_keys())
-    return filtered_values
-
-  def _extract_non_default_values(clazz, values, default_values):
+  def _extract_valid_non_default_values(clazz, values, default_values):
+    'Extract and return only the valid non default values'
     result = {}
     for key, value in values.items():
       if key in default_values:
@@ -144,7 +133,7 @@ class cli_options(with_metaclass(ABCMeta, object)):
     return result
 
   @classmethod
-  def from_config_file(clazz, config_filename):
+  def _values_from_config_file(clazz, config_filename):
     error_class = clazz.error_class()
     if not path.exists(config_filename):
       raise error_class('Config file not found: "{}"'.format(config_filename))
@@ -164,9 +153,11 @@ class cli_options(with_metaclass(ABCMeta, object)):
     section = config.section(config_file_section)
     values = section.to_dict()
 
-    for hint_key, hint_class in clazz.value_type_hints().items():
-      clazz._morph_value_type(values, hint_key, hint_class)
-
     valid_keys = clazz.default_values().keys()
     filtered_values = dict_util.filter_with_keys(values, valid_keys)
-    return clazz(**filtered_values)
+    return filtered_values
+  
+  @classmethod
+  def from_config_file(clazz, config_filename):
+    values = clazz._values_from_config_file(config_filename)
+    return clazz(**values)
