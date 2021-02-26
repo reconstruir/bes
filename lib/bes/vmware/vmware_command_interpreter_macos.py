f02a7afabf20f419e9cd54cb786d58fac9ddca51
Ramiro Estrugo <restrugo@imvu.com>
2021-02-26 13:45:42 -0800

    skeleton of command interpreter interface, registry and some implementations

--- lib/bes/vmware/vmware_command_interpreter_macos.py
+++
@@ -1,7 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-from .vmware_command_interpreter_unix import vmware_command_interpreter_unix
-from .vmware_error import vmware_error
-
-class vmware_command_interpreter_macos(vmware_command_interpreter_unix):
-  pass
