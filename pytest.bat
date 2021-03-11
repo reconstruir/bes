c393be3d68c499392500800ad940fc6bd80e845d
Ramiro Estrugo <git@fateware.com>
2021-03-10 21:24:30 -0800

    dont supress collection errors anymore

--- pytest.bat
+++ pytest.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
 set PYTHONPATH=C:\Users\ramiro\proj\bes\lib
-python -m pytest --continue-on-collection-errors %*
+python -m pytest %*
+rem python -m pytest --continue-on-collection-errors %*
 
