d75f79c2e26923dd67a6dd6ee9cb1fd81328c2f3
Ramiro Estrugo <git@fateware.com>
2021-03-17 04:07:30 -0700

    make PATH and PYTHONPATH be relative to the current dir

--- misc/setup.bat
+++ misc/setup.bat
@@ -1,5 +1,6 @@
 @echo off
-set PYTHONPATH=C:\Users\ramiro\proj\bes\lib
+set PYTHONPATH=%~dp0\..\lib;%PYTHONPATH%
+set PATH=%~dp0\..\bin;%PATH%
 set VISUAL=emacs
 
 rem set PROMPT="$E[m$E[32m$E]9;8;\"USERNAME\"$E\@$E]9;8;\"COMPUTERNAME\"$E\$S$E[92m$P$E[90m$_$E[90m$$$E[m$S$E]9;12$E\"
