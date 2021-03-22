647b61a296b64d2cf95c2b9c79ec6aae7a922836
Ramiro Estrugo <git@fateware.com>
2021-03-21 20:53:56 -0700

    wip for decoding install dir from exe

--- lib/bes/pip/pip_installation_values.py
+++ lib/bes/pip/pip_installation_values.py
@@ -1,28 +1,17 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-#from collections import namedtuple
-#import json
 from os import path
-#
+
 from bes.common.check import check
-#
-#from bes.fs.dir_util import dir_util
-#from bes.fs.file_find import file_find
-#from bes.fs.file_util import file_util
-#from bes.system.execute import execute
-from bes.system.host import host
 from bes.property.cached_property import cached_property
+from bes.system.host import host
 from bes.system.log import logger
 from bes.system.os_env import os_env
-#from bes.url.url_util import url_util
-#
-#from bes.python.python_exe import python_exe
-#from bes.python.python_version import python_version
-#
-#from .pip_error import pip_error
-#from .pip_exe import pip_exe
-#from .pip_installer_options import pip_installer_options
-#
+from bes.fs.file_mime import file_mime
+from bes.fs.filename_util import filename_util
+
+from .pip_error import pip_error
+
 class pip_installation_values(object):
   'Class to determine the filename and directory values of a pip installatiuon.'
 
@@ -60,9 +49,6 @@ class pip_installation_values(object):
     'Return the pip site-packages dir sometimes needed for PYTHONPATH'
     if self._system == host.WINDOWS:
       python_dir = 'Python{}'.format(self._python_version.replace('.', ''))
-      #if self._python_version == '2.7':
-      #  site_packaged_dir = path.join(self._install_dir, 'site-packages')
-      #else:
       site_packaged_dir = path.join(self._install_dir, python_dir, 'site-packages')
     elif self._system in ( host.LINUX, host.MACOS ):
       site_packaged_dir = path.join(self._install_dir, 'lib/python/site-packages')
@@ -78,3 +64,75 @@ class pip_installation_values(object):
       'PYTHONPATH': self.site_packages_dir,
     }
     return os_env.make_clean_env(update = extra_env)
+
+  @classmethod
+  def find_install_dir(clazz, pip_exe, system = None):
+    'Find the install dir from the pip exe'
+    check.check_string(pip_exe)
+
+    if self._system == host.WINDOWS:
+      result = self._find_install_dir_windows(pip_exe)
+    elif self._system in ( host.LINUX, host.MACOS ):
+      result = self._find_install_dir_unix(pip_exe)
+    else:
+      host.raise_unsupported_system()
+    return result
+
+  @classmethod
+  def _find_install_dir_unix(clazz, pip_exe):
+    parent_dir = file_path.parent_dir(pip_exe)
+    print('CACA: parent_dir={}'.format(parent_dir))
+    
+    root_dir = path.dirname(pip_exe)
+    lib_dir = path.normpath(path.join(path.join(root_dir, path.pardir), 'lib'))
+    possible_python_libdirs = file_path.glob(lib_dir, 'python*')
+    num_possible_python_libdirs = len(possible_python_libdirs)
+    if num_possible_python_libdirs == 1:
+      python_libdir = possible_python_libdirs[0]
+    else:
+      python_libdir = None
+    if not python_libdir:
+      return None
+    if not python_libdir:
+      return None
+    possible_site_packages_dir = path.join(python_libdir, 'site-packages')
+    if path.isdir(possible_site_packages_dir):
+      return possible_site_packages_dir
+    return None
+  
+  @classmethod
+  def _find_install_dir_windows(clazz, pip_exe):
+    basename = path.basename(pip_exe)
+
+    f = re.findall(r'^pip(\d+\.\d+)\....$', basename, flags = re.IGNORECASE)
+    if not f:
+      raise pip_error('pip_exe does not contain a python version: "{}"'.format(pip_exe))
+    if len(f) != 1:
+      raise pip_error('pip_exe does not contain a python version: "{}"'.format(pip_exe))
+    python_version = f[0]
+    parent_dir = file_path.parent_dir(pip_exe)
+
+    if python_version == '2.7':
+      result = file_path.parent_dir(parent_dir)
+    else:
+      result = file_path.parent_dir(parent_dir)
+      
+    
+    return 'caca'
+    root_dir = path.dirname(pip_exe)
+    lib_dir = path.normpath(path.join(path.join(root_dir, path.pardir), 'lib'))
+    possible_python_libdirs = file_path.glob(lib_dir, 'python*')
+    num_possible_python_libdirs = len(possible_python_libdirs)
+    if num_possible_python_libdirs == 1:
+      python_libdir = possible_python_libdirs[0]
+    else:
+      python_libdir = None
+    if not python_libdir:
+      return None
+    if not python_libdir:
+      return None
+    possible_site_packages_dir = path.join(python_libdir, 'site-packages')
+    if path.isdir(possible_site_packages_dir):
+      return possible_site_packages_dir
+    return None
+  
