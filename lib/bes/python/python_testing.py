00c714fa59744d0e76bf089511f3f9d38c02c1e6
Ramiro Estrugo <git@fateware.com>
2021-03-25 18:39:28 -0700

    mvoe testing stuff to python_testing

---
+++ lib/bes/python/python_testing.py
@@ -0,0 +1,33 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from .python_exe import python_exe
+
+from bes.system.log import logger
+
+class python_testing(object):
+  'Class to deal with the python testing.'
+
+  _log = logger('python_exe')
+  
+  # The python 3.8 that comes with xcode is very non standard
+  # crapping all kinds of droppings in non standard places such
+  # as ~/Library/Caches even though the --no-cache-dir was given
+  # so never use them for tests since they create side effects
+  _EXCLUDE_SOURCES = ( 'xcode', )
+  
+  PYTHON_27 = python_exe.find_version('2.7', exclude_sources = _EXCLUDE_SOURCES)
+  PYTHON_37 = python_exe.find_version('3.7', exclude_sources = _EXCLUDE_SOURCES)
+  PYTHON_38 = python_exe.find_version('3.8', exclude_sources = _EXCLUDE_SOURCES)
+  PYTHON_39 = python_exe.find_version('3.9', exclude_sources = _EXCLUDE_SOURCES)
+
+  ALL_PYTHONS = [ p for p in [ _PYTHON_27, _PYTHON_37, _PYTHON_38, _PYTHON_39 ] if p ]
+  ANY_PYTHON2 = next(iter([ p for p in _ALL_PYTHONS if python_exe.major_version(p) == 2]), None)
+  ANY_PYTHON3 = next(iter([ p for p in _ALL_PYTHONS if python_exe.major_version(p) == 3]), None)
+
+  _log.log_d('PYTHON_27: {}'.format(PYTHON_27))
+  _log.log_d('PYTHON_37: {}'.format(PYTHON_37))
+  _log.log_d('PYTHON_38: {}'.format(PYTHON_38))
+  _log.log_d('PYTHON_39: {}'.format(PYTHON_39))
+  _log.log_d('ALL_PYTHONS: {}'.format(' '.join(ALL_PYTHONS)))
+  _log.log_d('ANY_PYTHON2: {}'.format(ANY_PYTHON2))
+  _log.log_d('ANY_PYTHON3: {}'.format(ANY_PYTHON3))
--- tests/lib/bes/pip/test_pip_installer.py
+++ tests/lib/bes/pip/test_pip_installer.py
@@ -11,6 +11,7 @@ from bes.pip.pip_installer import pip_installer
 from bes.pip.pip_installer_options import pip_installer_options
 from bes.python.python_exe import python_exe as bes_python_exe
 from bes.python.python_version import python_version as bes_python_version
+from bes.python.python_testing import python_testing
 from bes.system.host import host
 from bes.testing.unit_test import unit_test
 from bes.testing.unit_test_skip import skip_if
@@ -21,71 +22,52 @@ class test_pip_installer(unit_test):
 
   @classmethod
   def setUpClass(clazz):
-    raise_skip('Not ready')
+    #raise_skip('Not ready')
+    pass
   
-  # The python 3.8 that comes with xcode is very non standard
-  # crapping all kinds of droppings in non standard places such
-  # as ~/Library/Caches even though the --no-cache-dir was given
-  # so never use them for tests since they create side effects
-  _EXCLUDE_SOURCES = ( 'xcode', )
-  
-  _PYTHON_27 = bes_python_exe.find_version('2.7', exclude_sources = _EXCLUDE_SOURCES)
-  _PYTHON_37 = bes_python_exe.find_version('3.7', exclude_sources = _EXCLUDE_SOURCES)
-  _PYTHON_38 = bes_python_exe.find_version('3.8', exclude_sources = _EXCLUDE_SOURCES)
-  _PYTHON_39 = bes_python_exe.find_version('3.9', exclude_sources = _EXCLUDE_SOURCES)
-
-  _ALL_PYTHONS = [ p for p in [ _PYTHON_27, _PYTHON_37, _PYTHON_38, _PYTHON_39 ] if p ]
-  _ANY_PYTHON2 = next(iter([ p for p in _ALL_PYTHONS if bes_python_exe.major_version(p) == 2]), None)
-  _ANY_PYTHON3 = next(iter([ p for p in _ALL_PYTHONS if bes_python_exe.major_version(p) == 3]), None)
-
-  #print('_ALL_PYTHONS: {}'.format(_ALL_PYTHONS))
-  #print('_ANY_PYTHON2: {}'.format(_ANY_PYTHON2))
-  #print('_ANY_PYTHON3: {}'.format(_ANY_PYTHON3))
-  #raise SystemExit(1)
-  
-  @skip_if(not _PYTHON_27, 'test_install_python_27 - python 2.7 not found', warning = True)
+  @skip_if(not python_testing.PYTHON_27, 'test_install_python_27 - python 2.7 not found', warning = True)
   def test_install_python_27(self):
-    tester = self._make_tester(self._PYTHON_27, 'test')
+    tester = self._make_tester(python_testing.PYTHON_27, 'test')
     self.assertFalse( path.exists(tester.installer.pip_exe()) )
     tester.installer.install('latest', False)
     self.assertTrue( path.exists(tester.installer.pip_exe()) )
     self.assertEqual( 1, software_version.compare(tester.installer.pip_version(), '19.0.0') )
 
-  @skip_if(not _PYTHON_37, 'test_install_python_37 - python 3.7 not found', warning = True)
+  @skip_if(not python_testing.PYTHON_37, 'test_install_python_37 - python 3.7 not found', warning = True)
   def test_install_python_37(self):
-    tester = self._make_tester(self._PYTHON_37, 'test')
+    tester = self._make_tester(python_testing.PYTHON_37, 'test')
     self.assertFalse( path.exists(tester.installer.pip_exe()) )
     tester.installer.install('latest', False)
     self.assertTrue( path.exists(tester.installer.pip_exe()) )
     self.assertEqual( 1, software_version.compare(tester.installer.pip_version(), '19.0.0') )
 
-  @skip_if(not _PYTHON_38, 'test_install_python_38 - python 3.8 not found', warning = True)
+  @skip_if(not python_testing.PYTHON_38, 'test_install_python_38 - python 3.8 not found', warning = True)
   def test_install_python_38(self):
-    tester = self._make_tester(self._PYTHON_38, 'test')
+    tester = self._make_tester(python_testing.PYTHON_38, 'test')
     self.assertFalse( path.exists(tester.installer.pip_exe()) )
     tester.installer.install('latest', False)
     self.assertTrue( path.exists(tester.installer.pip_exe()) )
     self.assertEqual( 1, software_version.compare(tester.installer.pip_version(), '19.0.0') )
 
-  @skip_if(not _PYTHON_39, 'test_install_python_39 - python 3.9 not found', warning = True)
+  @skip_if(not python_testing.PYTHON_39, 'test_install_python_39 - python 3.9 not found', warning = True)
   def test_install_python_39(self):
-    tester = self._make_tester(self._PYTHON_39, 'test')
+    tester = self._make_tester(python_testing.PYTHON_39, 'test')
     self.assertFalse( path.exists(tester.installer.pip_exe()) )
     tester.installer.install('latest', False)
     self.assertTrue( path.exists(tester.installer.pip_exe()) )
     self.assertEqual( 1, software_version.compare(tester.installer.pip_version(), '19.0.0') )
 
-  @skip_if(not _ANY_PYTHON3, 'test_uninstall_python3 - python 3 not found', warning = True)
+  @skip_if(not python_testing.ANY_PYTHON3, 'test_uninstall_python3 - python 3 not found', warning = True)
   def test_uninstall_python3(self):
-    tester = self._make_tester(self._ANY_PYTHON3, 'test')
+    tester = self._make_tester(python_testing.ANY_PYTHON3, 'test')
     tester.installer.install('latest', False)
     self.assertTrue( tester.installer.is_installed() )
     tester.installer.uninstall()
     self.assertFalse( tester.installer.is_installed() )
 
-  @skip_if(not _ANY_PYTHON3, 'test_install_specific_version - python 3 not found', warning = True)
+  @skip_if(not python_testing.ANY_PYTHON3, 'test_install_specific_version - python 3 not found', warning = True)
   def test_install_specific_version(self):
-    tester = self._make_tester(self._ANY_PYTHON3, 'test')
+    tester = self._make_tester(python_testing.ANY_PYTHON3, 'test')
     tester.installer.install('19.2.3', False)
     self.assertEqual( '19.2.3', tester.installer.pip_version() )
     
