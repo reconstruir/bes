70139e588bf51c926a32dd2ea920aeb17cdbe864
Ramiro Estrugo <git@fateware.com>
2021-04-04 07:11:42 -0700

    add a brew macos test

--- tests/lib/bes/python/test_python_installation_v2.py
+++ tests/lib/bes/python/test_python_installation_v2.py
@@ -40,17 +40,17 @@ class test_python_installation_v2(unit_test):
     self.assert_filename_list_equal( [ path.join(tmp_dir, 'bin') ], piv.PATH )
 
   @skip_if(not host.is_macos(), 'not macos')
-  def xtest_macos_xcode_38(self):
-    tmp_dir = python_testing.make_temp_fake_python_installation('3.8',
-                                                                '19.2.3',
-                                                                'xcode',
+  def test_macos_brew_37(self):
+    tmp_dir = python_testing.make_temp_fake_python_installation('3.7',
+                                                                '21.0.1',
+                                                                'brew',
                                                                 system = 'macos',
                                                                 debug = self.DEBUG)
 
-    piv = python_installation_v2(path.join(tmp_dir, 'bin/python3'), system = 'macos')
-    self.assert_filename_equal( path.join(tmp_dir, 'bin/python3'), piv.python_exe )
-    self.assert_filename_equal( path.join(tmp_dir, 'bin/pip3'), piv.pip_exe )
-    self.assert_filename_equal( '3.8', piv.python_version )
+    piv = python_installation_v2(path.join(tmp_dir, 'bin/python3.7'), system = 'macos')
+    self.assert_filename_equal( path.join(tmp_dir, 'bin/python3.7'), piv.python_exe )
+    self.assert_filename_equal( path.join(tmp_dir, 'bin/pip3.7'), piv.pip_exe )
+    self.assert_filename_equal( '3.7', piv.python_version )
     self.assert_filename_list_equal( [ path.join(tmp_dir, 'bin') ], piv.PATH )
     
   @skip_if(not host.is_windows(), 'not windows')
@@ -70,9 +70,6 @@ class test_python_installation_v2(unit_test):
       path.join(tmp_dir, 'Scripts'),
     ], piv.PATH )
 
-    #self.assert_filename_equal( '/tmp/foo/lib/python/site-packages', piv.site_packages_dir )
-    #self.assert_filename_list_equal( [ '/tmp/foo/lib/python/site-packages' ], piv.PYTHONPATH )
-
   @skip_if(not host.is_windows(), 'not windows')
   def test_windows_python_27(self):
     tmp_dir = python_testing.make_temp_fake_python_installation('2.7',
@@ -85,54 +82,6 @@ class test_python_installation_v2(unit_test):
     self.assert_filename_equal( path.join(tmp_dir, 'python.bat'), piv.python_exe )
     self.assert_filename_equal( path.join(tmp_dir, 'Scripts', 'pip2.7.bat'), piv.pip_exe )
     self.assert_filename_equal( '2.7', piv.python_version )
-    
-  def xtest_macos_py27(self):
-    piv = python_installation('/tmp/foo', '2.7', system = 'macos')
-    self.assert_filename_equal( '/tmp/foo/bin/pip2.7', piv.pip_exe )
-    self.assert_filename_equal( '/tmp/foo/lib/python/site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ '/tmp/foo/lib/python/site-packages' ], piv.PYTHONPATH )
-
-  def xtest_macos_py37(self):
-    piv = python_installation('/tmp/foo', '3.7', system = 'macos')
-    self.assert_filename_equal( '/tmp/foo/bin/pip3.7', piv.pip_exe )
-    self.assert_filename_equal( '/tmp/foo/lib/python/site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ '/tmp/foo/lib/python/site-packages' ], piv.PYTHONPATH )
-
-  def xtest_macos_py38(self):
-    piv = python_installation('/tmp/foo', '3.8', system = 'macos')
-    self.assert_filename_equal( '/tmp/foo/bin/pip3.8', piv.pip_exe )
-    self.assert_filename_equal( '/tmp/foo/lib/python/site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ '/tmp/foo/lib/python/site-packages' ], piv.PYTHONPATH )
-
-  def xtest_macos_py39(self):
-    piv = python_installation('/tmp/foo', '3.9', system = 'macos')
-    self.assert_filename_equal( '/tmp/foo/bin/pip3.9', piv.pip_exe )
-    self.assert_filename_equal( '/tmp/foo/lib/python/site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ '/tmp/foo/lib/python/site-packages' ], piv.PYTHONPATH )
-
-  def xtest_windows_py27(self):
-    piv = python_installation(r'c:\tmp\foo', '2.7', system = 'windows')
-    self.assert_filename_equal( r'c:\tmp\foo\Scripts\pip2.7.exe', piv.pip_exe )
-    self.assert_filename_equal( r'c:\tmp\foo\Python27\site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ r'c:\tmp\foo\Python27\site-packages' ], piv.PYTHONPATH )
-
-  def xtest_windows_py37(self):
-    piv = python_installation(r'c:\tmp\foo', '3.7', system = 'windows')
-    self.assert_filename_equal( r'c:\tmp\foo\Python37\Scripts\pip3.7.exe', piv.pip_exe )
-    self.assert_filename_equal( r'c:\tmp\foo\Python37\site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ r'c:\tmp\foo\Python37\site-packages' ], piv.PYTHONPATH )
-
-  def xtest_windows_py38(self):
-    piv = python_installation(r'c:\tmp\foo', '3.8', system = 'windows')
-    self.assert_filename_equal( r'c:\tmp\foo\Python38\Scripts\pip3.8.exe', piv.pip_exe )
-    self.assert_filename_equal( r'c:\tmp\foo\Python38\site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ r'c:\tmp\foo\Python38\site-packages' ], piv.PYTHONPATH )
-
-  def xtest_windows_py39(self):
-    piv = python_installation(r'c:\tmp\foo', '3.9', system = 'windows')
-    self.assert_filename_equal( r'c:\tmp\foo\Python39\Scripts\pip3.9.exe', piv.pip_exe )
-    self.assert_filename_equal( r'c:\tmp\foo\Python39\site-packages', piv.site_packages_dir )
-    self.assert_filename_list_equal( [ r'c:\tmp\foo\Python39\site-packages' ], piv.PYTHONPATH )
     
 if __name__ == '__main__':
   unit_test.main()
