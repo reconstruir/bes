72dd0dd88235612cb3f015a9a32cd735a81c273d
Ramiro Estrugo <git@fateware.com>
2021-04-17 18:14:08 -0700

    add

---
+++ lib/bes/python/python_exe_config.py
@@ -0,0 +1,71 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from os import path
+
+from bes.common.check import check
+from bes.system.log import logger
+from bes.system.host import host
+
+from bes.config.simple_config_editor import simple_config_editor
+from bes.python.python_version import python_version
+
+from .python_error import python_error
+
+class python_exe_config(object):
+  '''
+  Class to manage python executables by system and version.  For example:
+
+macos
+  2.7: /usr/bin/python2.7
+  3.7: /usr/local/opt/python@3.7/bin/python3.7
+  3.8: /usr/local/opt/python@3.8/bin/python3.8
+  3.9: /usr/local/opt/python@3.9/bin/python3.9
+
+windows
+  2.7: C:\Python27\python.exe
+  3.7: C:\Program Files\Python37\python.exe
+  3.8: C:\Program Files\Python38\python.exe
+  3.9: C:\Program Files\Python39\python.exe
+  '''
+
+  _log = logger('pip')
+  
+  def __init__(self, filename):
+    check.check_string(filename)
+
+    self._config = simple_config_editor(filename)
+
+  def get_python_exe(self, version, system = None, distro = None):
+    python_version.check_version(version)
+    check.check_string(system, allow_none = True)
+    check.check_string(distro, allow_none = True)
+
+    section_name = self._make_section_name(system, distro)
+    return self._config.get_value_with_default(section_name, version, None)
+
+  def set_python_exe(self, version, exe, system = None, distro = None):
+    python_version.check_version(version)
+    check.check_string(system, allow_none = True)
+    check.check_string(distro, allow_none = True)
+
+    section_name = self._make_section_name(system, distro)
+    return self._config.set_value(section_name, version, exe)
+  
+  @classmethod
+  def _make_section_name(clazz, system, distro):
+    system = system or host.SYSTEM
+    distro = distro or host.DISTRO
+
+    if system != host.LINUX and distro:
+      raise python_error('distro should only be given when system is linux')
+    
+    if system in ( host.WINDOWS, host.MACOS ):
+      section_name = system
+    elif system in ( host.LINUX ):
+      if distro:
+        section_name = '{}.{}'.format(system, distro)
+      else:
+        section_name = system
+    else:
+      host.raise_unsupported_system(system = system)
+    return section_name
