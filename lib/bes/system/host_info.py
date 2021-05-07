1f6a7294a19683a134dd041de301087ef2593f96
Ramiro Estrugo <git@fateware.com>
2021-05-07 04:06:02 -0700

    add parse() and unit tests

--- lib/bes/system/host_info.py
+++ lib/bes/system/host_info.py
@@ -10,3 +10,29 @@ class host_info(namedtuple('host_info', 'system, version_major, version_minor, a
   @property
   def version(self):
     return '{}.{}'.format(self.version_major, self.version_minor)
+
+  @property
+  def triple(self):
+    parts = [ self.system, self.distro, self.version_major, self.arch ]
+    parts = [ p for p in parts if p ]
+    return '-'.join(parts)
+
+  @classmethod
+  def parse(clazz, s):
+    parts = s.split('-')
+    system = parts.pop(0)
+    if system in ( 'macos', 'windows' ):
+      if not parts:
+        raise ValueError('invalid host_info: "{}"'.format(s))
+      version = parts.pop(0)
+      version_major, _, version_minor = version.partition('.')
+      version_minor = version_minor or '0'
+      if parts:
+        arch = parts.pop(0)
+      else:
+        arch = 'x86_64'
+      distro = ''
+      family = None
+    else:
+      assert False, 'fix for loonix'
+    return host_info(system, version_major, version_minor, arch, distro, family)
