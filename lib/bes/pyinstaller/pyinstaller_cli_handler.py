d973f90586b24bb3544a2a5386f3be2d5463cf24
Ramiro Estrugo <git@fateware.com>
2021-05-26 11:28:43 -0700

    clean works copy to right output works

--- lib/bes/pyinstaller/pyinstaller_cli_handler.py
+++ lib/bes/pyinstaller/pyinstaller_cli_handler.py
@@ -6,6 +6,7 @@ from bes.cli.cli_command_handler import cli_command_handler
 from bes.common.Script import Script
 from bes.common.check import check
 from bes.fs.file_util import file_util
+from bes.fs.filename_util import filename_util
 from bes.fs.file_check import file_check
 from bes.python.python_version import python_version
 from bes.script.blurber import blurber
@@ -27,6 +28,12 @@ class pyinstaller_cli_handler(cli_command_handler):
 
     build_dir = path.abspath(self.options.build_dir)
     script_filename_abs = path.abspath(script_filename)
+    output_filename_abs = path.abspath(output_filename)
+
+    if self.options.clean:
+      file_util.remove(build_dir)
+      
+    file_util.mkdir(build_dir)
 
     pyinstaller_build.build(script_filename_abs,
                             log_level = self.options.log_level,
@@ -36,4 +43,11 @@ class pyinstaller_cli_handler(cli_command_handler):
                             cwd = build_dir,
                             replace_env = None,
                             exe = None)
+
+    output_exe = path.join(build_dir, 'dist', self._binary_basename(script_filename_abs))
+    file_util.copy(output_exe, output_filename_abs)
     return 0
+
+  @classmethod                          
+  def _binary_basename(clazz, script_filename):
+    return path.basename(filename_util.without_extension(script_filename))
