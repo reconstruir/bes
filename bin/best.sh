4558563f1ba14ad956df3ba4cc89b9a3148af6d4
Ramiro Estrugo <git@fateware.com>
2021-07-06 11:47:20 -0700

    add a shell script wrappter for best.py that can run without env setup

---
+++ bin/best.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+function main()
+{
+  local _this_dir="$(_best_sh_this_dir)"
+  source "${_this_dir}/../bes_shell/bes_all.bash"
+  local _root_dir="$(bes_abs_dir ${_this_dir}/..)"
+  local _python_lib_dir="$(bes_abs_dir ${_this_dir}/../lib)"
+
+  export PYTHONPATH="${_python_lib_dir}:${PYTHONPATH}"
+  exec ${_root_dir}/bin/best.py ${1+"$@"}
+
+  return 0
+}
+
+function _best_sh_this_dir()
+{
+  echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  return 0
+}
+
+main ${1+"$@"}
