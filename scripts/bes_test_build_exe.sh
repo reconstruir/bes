5a8494faa365817dd45687c93ae9d7e1ce440ec9
Ramiro Estrugo <git@fateware.com>
2021-07-10 18:23:59 -0700

    add

---
+++ scripts/bes_test_build_exe.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+function main()
+{
+  source $(_bes_test_build_exe_this_dir)/../bes_shell/bes_all.bash
+
+  local _this_dir="$(_bes_test_build_exe_this_dir)"
+  local _root_dir="$(bes_abs_path ${_this_dir}/..)"
+  local _best="${_root_dir}/bin/best.py"
+  local _bes_test_script="${_root_dir}/bin/bes_test.py"
+  local _bes_test_exe="${_root_dir}/bes_test.exe"
+
+  ${_best} pyinstaller build \
+           --build-dir _BES_TEST_BUILD \
+           --clean \
+           --log-level INFO \
+           --python-version 3.8 \
+           "${_bes_test_script}" \
+           "${_bes_test_exe}"
+  
+  return 0
+}
+
+function _bes_test_build_exe_this_dir()
+{
+  echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  return 0
+}
+
+main ${1+"$@"}
