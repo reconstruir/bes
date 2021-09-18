38a883e190b373537a25cc345904d7272ca11818
Ramiro Estrugo <git@fateware.com>
2021-09-18 15:02:53 -0700

    wip

--- lib/bes/text_file/text_file_token_type.py
+++ lib/bes/text_file/text_file_token_type.py
@@ -1,23 +1,14 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.common.check import check
-from bes.enum_util.checked_int_enum import checked_int_enum
+from bes.enum_util.checked_enum import checked_enum
 
-class text_file_token_type(checked_int_enum):
-  # Requirement needed at runtime.  For example a dynamically linked library.
-  RUN = 1
+class text_file_token_type(checked_enum):
+  COMMENT = 'comment'
+  COMMENT_DELIMITER = 'comment_delimiter'
+  SPACE = 'space'
+  TEXT = 'text'
 
-  # Requirement is tool needed at build time.  For example a compiler.
-  TOOL = 2
-
-  # Requirement needed only at build time.  For example a statically linked library.
-  BUILD = 3
-    
-  # Requirement needed only for testing.
-  TEST = 4
-    
-  DEFAULT = RUN
-
-check.register_class(requirement_hardness,
+check.register_class(text_file_token_type,
                      include_seq = False,
-                     cast_func = requirement_hardness.parse)
+                     cast_func = text_file_token_type.parse)
