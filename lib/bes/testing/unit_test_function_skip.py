d89894a47afbec72743ce5608350f83d74da09ec
Ramiro Estrugo <git@fateware.com>
2021-11-22 20:04:10 -0800

    add

---
+++ lib/bes/testing/unit_test_function_skip.py
@@ -0,0 +1,45 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+# Borrowed from: http://stackoverflow.com/questions/21936292/conditional-skip-testcase-decorator-in-nosetests
+
+import functools, unittest, types, sys
+from bes.system.host import host
+from bes.system.compat import compat
+
+class unit_test_function_skip(object):
+
+  def _id(clazz, obj):
+    return obj
+
+  def skip(clazz, reason):
+    """Unconditionally skip a test."""
+    def decorator(test_item):
+      if not isinstance(test_item, compat.CLASS_TYPES):
+        @functools.wraps(test_item)
+        def skip_wrapper(*args, **kwargs):
+          raise unittest.SkipTest(reason)
+        test_item = skip_wrapper
+      elif issubclass(test_item, unittest.TestCase):
+        @classmethod
+        @functools.wraps(test_item.setUpClass)
+        def skip_wrapper(*args, **kwargs):
+          raise unittest.SkipTest(reason)
+        test_item.setUpClass = skip_wrapper
+      test_item.__unittest_skip__ = True
+      test_item.__unittest_skip_why__ = reason
+      return test_item
+    return decorator
+
+  def skip_if(clazz, condition, reason, warning = False):
+    """Skip a test if the condition is true."""
+    if condition:
+      if warning:
+        print('SKIPPED: %s' % (reason))
+      return clazz.skip(reason)
+    return clazz._id
+
+  def skip_if_not_unix(clazz, warning = False):
+    return clazz.skip_if(not host.is_unix(), 'not unix', warning = warning)
+
+  def skip_if_not_windows(clazz, warning = False):
+    return clazz.skip_if(not host.is_windows(), 'not windows', warning = warning)
