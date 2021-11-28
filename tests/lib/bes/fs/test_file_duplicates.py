436c73f4234eec58b7dc473d343b12839344dbd2
Ramiro Estrugo <git@fateware.com>
2021-11-28 13:25:04 -0800

    work in progress for better unit tests

--- tests/lib/bes/fs/test_file_duplicates.py
+++ tests/lib/bes/fs/test_file_duplicates.py
@@ -9,6 +9,7 @@ from bes.fs.file_util import file_util
 from bes.fs.file_find import file_find
 from bes.fs.dir_util import dir_util
 from bes.fs.file_duplicates import file_duplicates
+from bes.common.string_util import string_util
 
 from bes.fs.testing.temp_content import temp_content
   
@@ -40,26 +41,33 @@ class test_file_duplicates(unit_test):
       'file b/lemon_dup2.txt "this is lemon.txt" 644',
     ], [ 'b', 'a' ]) )
     
+  _extra_dir = namedtuple('_extra_dir', 'dir, label')
   _test_result = namedtuple('_test_result', 'tmp_dir, dups, files')
-  def _test(self, items, dirs):
-    tmp_dir = temp_content.write_items_to_temp_dir(items)
-    dirs = [ path.join(tmp_dir, d) for d in dirs ]
-    dups = file_duplicates.find_duplicates(dirs)
-    return self._hack_dup_item_list(dups, tmp_dir, '${_root}')
+  def _test(self, items, dirs, extra_dirs_before = [], extra_dirs_after = []):
+    tmp_root = temp_content.write_items_to_temp_dir(items)
+    tmp_root_dirs = [ path.join(tmp_root, d) for d in dirs ]
+    dirs_for_find_dups_before = [ d.dirname for d in extra_dirs_before ]
+    dirs_for_find_dups_after = [ d.dirname for d in extra_dirs_after ]
+    dirs_for_find_dups = dirs_for_find_dups_before + tmp_root_dirs + dirs_for_find_dups_after
+    dups = file_duplicates.find_duplicates(dirs_for_find_dups)
+    replacements = {
+      tmp_root: '${_root}',
+    }
+    return self._hack_dup_item_list(dups, replacements)
 
   @classmethod
-  def _hack_dup_item(clazz, dup_item, tmp_dir, replacement):
-    new_filename = dup_item.filename.replace(tmp_dir, replacement)
+  def _hack_dup_item(clazz, dup_item, replacements):
+    new_filename = string_util.replace(dup_item.filename, replacements)
     new_duplicates = []
     for dup in dup_item.duplicates:
-      new_duplicates.append(dup.replace(tmp_dir, replacement))
+      new_duplicates.append(string_util.replace(dup, replacements))
     return file_duplicates._dup_item(new_filename, new_duplicates)
     
   @classmethod
-  def _hack_dup_item_list(clazz, dup_item_list, tmp_dir, replacement):
+  def _hack_dup_item_list(clazz, dup_item_list, replacements):
     result = []
     for dup_item in dup_item_list:
-      result.append(clazz._hack_dup_item(dup_item, tmp_dir, replacement))
+      result.append(clazz._hack_dup_item(dup_item, replacements))
     return result
     
 if __name__ == '__main__':
