45f9c10b2f5c201c8e092d2d2d80149e059a9e59
Ramiro Estrugo <git@fateware.com>
2021-12-12 12:53:55 -0800

    actually use the impl and cleanup the names

--- tests/lib/bes/fs/test_file_attributes.py
+++ tests/lib/bes/fs/test_file_attributes.py
@@ -12,9 +12,9 @@ from bes.docker.docker import docker
 from bes.testing.unit_test_class_skip import unit_test_class_skip
 from bes.system.host import host
 
-def _test_case(impl):
+def _make_test_case(impl):
   
-  class _test_case_caca(unit_test):
+  class _test_case(unit_test):
   
     # 1x1 PNG file
     _TEST_DATA_PNG = '89504e470d0a1a0a0000000d4948445200000001000000010802000000907753de00000184694343504943432070726f66696c65000028917d913d48c3401886dfa68a452a22161471c8509d2c888a386a158a5021d40aad3a985cfa074d1a92141747c1b5e0e0cf62d5c1c5595707574110fc0171727452749112bf4b0a2d62bce3b887f7bef7e5ee3b40a8979966758c039a6e9ba9445ccc6457c5ae5784d087019a82cc2c634e9292f01d5ff708f0fd2ec6b3fcebfe1c3d6ace624040249e658669136f104f6fda06e77de2082bca2af139f1984917247ee4bae2f11be782cb02cf8c98e9d43c7184582cb4b1d2c6ac686ac453c45155d3295fc878ac72dee2ac95abac794ffec2704e5f59e63aad6124b088254810a1a08a12cab011a35d27c5428acee33efe21d72f914b2157098c1c0ba84083ecfac1ffe0776fadfce48497148e039d2f8ef3310274ed028d9ae37c1f3b4ee304083e03577acb5fa903339fa4d75a5af408e8dd062eae5b9ab2075cee00834f866ccaae14a425e4f3c0fb197d5316e8bf05bad7bcbe35cf71fa00a4a957c91be0e010182d50f6bacfbb43ed7dfbb7a6d9bf1f20f472864f68cb260000000970485973000021380000213801459631600000000774494d4507e40812121d17e87cb9240000000c4944415408d763f8ffff3f0005fe02fedccc59e70000000049454e44ae426082'
@@ -22,46 +22,46 @@ def _test_case(impl):
 
     def test_has_key_false(self):
       tmp = self._make_temp_file('this is foo\n')
-      self.assertFalse( file_attributes.has_key(tmp, 'foo') )
+      self.assertFalse( impl.has_key(tmp, 'foo') )
   
     def test_has_key_true(self):
       tmp = self._make_temp_file('this is foo\n')
-      file_attributes.set(tmp, 'foo', 'hi'.encode('utf-8'))
-      self.assertTrue( file_attributes.has_key(tmp, 'foo') )
+      impl.set(tmp, 'foo', 'hi'.encode('utf-8'))
+      self.assertTrue( impl.has_key(tmp, 'foo') )
     
     def test_get_never_set(self):
       tmp = self._make_temp_file('this is foo\n')
-      self.assertEqual( None, file_attributes.get(tmp, 'foo') )
+      self.assertEqual( None, impl.get(tmp, 'foo') )
   
     def test_set_get(self):
       tmp = self._make_temp_file('this is foo\n')
-      file_attributes.set(tmp, 'foo', 'hi'.encode('utf-8'))
-      self.assertEqual( 'hi', file_attributes.get(tmp, 'foo').decode('utf-8') )
-      file_attributes.set(tmp, 'bar', '99'.encode('utf-8'))
-      self.assertEqual( '99', file_attributes.get(tmp, 'bar').decode('utf-8') )
+      impl.set(tmp, 'foo', 'hi'.encode('utf-8'))
+      self.assertEqual( 'hi', impl.get(tmp, 'foo').decode('utf-8') )
+      impl.set(tmp, 'bar', '99'.encode('utf-8'))
+      self.assertEqual( '99', impl.get(tmp, 'bar').decode('utf-8') )
 
     def test_empty_keys(self):
       tmp = self._make_temp_file('this is foo\n')
-      self.assertEqual( [], self._munge_attr_keys(file_attributes.keys(tmp)) )
+      self.assertEqual( [], self._munge_attr_keys(impl.keys(tmp)) )
 
     def test_keys(self):
       tmp = self._make_temp_file('this is foo\n')
-      file_attributes.set(tmp, 'foo', 'hi'.encode('utf-8'))
-      file_attributes.set(tmp, 'bar', '99'.encode('utf-8'))
-      self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(file_attributes.keys(tmp)) )
+      impl.set(tmp, 'foo', 'hi'.encode('utf-8'))
+      impl.set(tmp, 'bar', '99'.encode('utf-8'))
+      self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(impl.keys(tmp)) )
     
     def test_clear(self):
       tmp = self._make_temp_file('this is foo\n')
-      file_attributes.set(tmp, 'foo', 'hi'.encode('utf-8'))
-      file_attributes.set(tmp, 'bar', '99'.encode('utf-8'))
-      self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(file_attributes.keys(tmp)) )
-      file_attributes.clear(tmp)
-      self.assertEqual( [], self._munge_attr_keys(file_attributes.keys(tmp)) )
+      impl.set(tmp, 'foo', 'hi'.encode('utf-8'))
+      impl.set(tmp, 'bar', '99'.encode('utf-8'))
+      self.assertEqual( [ 'bar', 'foo' ], self._munge_attr_keys(impl.keys(tmp)) )
+      impl.clear(tmp)
+      self.assertEqual( [], self._munge_attr_keys(impl.keys(tmp)) )
 
     def test_set_png_get_png(self):
       tmp = self._make_temp_file('this is foo\n')
-      file_attributes.set(tmp, 'picture', self._TEST_DATA_BYTES)
-      self.assertEqual( self._TEST_DATA_BYTES, file_attributes.get(tmp, 'picture') )
+      impl.set(tmp, 'picture', self._TEST_DATA_BYTES)
+      self.assertEqual( self._TEST_DATA_BYTES, impl.get(tmp, 'picture') )
   
     @classmethod
     def _munge_attr_keys(clazz, keys):
@@ -77,9 +77,9 @@ def _test_case(impl):
       tmp_dir = path.join(path.dirname(__file__), '.tmp')
       return self.make_temp_file(content = content, dir = tmp_dir, suffix = '.txt')
 
-  return _test_case_caca
+  return _test_case
 
-class test_file_attributes(_test_case(file_attributes)):
+class test_file_attributes(_make_test_case(file_attributes)):
   
   @classmethod
   def setUpClass(clazz):
@@ -87,7 +87,7 @@ class test_file_attributes(_test_case(file_attributes)):
 
 if host.is_macos():
   from bes.fs._file_attributes_macos import _file_attributes_macos
-  class test__file_attributes_macos(_test_case(_file_attributes_macos)):
+  class test__file_attributes_macos(_make_test_case(_file_attributes_macos)):
 
     @classmethod
     def setUpClass(clazz):
@@ -96,7 +96,7 @@ if host.is_macos():
 
 if host.is_linux():
   from bes.fs._file_attributes_linux import _file_attributes_linux
-  class test__file_attributes_linux(_test_case(_file_attributes_linux)):
+  class test__file_attributes_linux(_make_test_case(_file_attributes_linux)):
 
     @classmethod
     def setUpClass(clazz):
@@ -105,7 +105,7 @@ if host.is_linux():
 
 if host.is_windows():    
   from bes.fs._file_attributes_windows import _file_attributes_windows
-  class test__file_attributes_windows(_test_case(_file_attributes_windows)):
+  class test__file_attributes_windows(_make_test_case(_file_attributes_windows)):
 
     @classmethod
     def setUpClass(clazz):
@@ -114,7 +114,7 @@ if host.is_windows():
 
 if HAS_XATTR:
   from bes.fs._file_attributes_xattr import _file_attributes_xattr
-  class test__file_attributes_xattr(_test_case(_file_attributes_xattr)):
+  class test__file_attributes_xattr(_make_test_case(_file_attributes_xattr)):
 
     @classmethod
     def setUpClass(clazz):
