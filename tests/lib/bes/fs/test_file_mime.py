d70f19ee8bb78fd82f8bf9ab9af235f1e975fd0f
Ramiro Estrugo <git@fateware.com>
2021-12-12 22:03:56 -0800

    cleanup a bit

--- tests/lib/bes/fs/test_file_mime.py
+++ tests/lib/bes/fs/test_file_mime.py
@@ -15,33 +15,6 @@ from _bes_unit_test_common.unit_test_media import unit_test_media
 
 class test_file_mime(unit_test):
 
-  def test_mime_type(self):
-    tmp = self.make_temp_file(content = 'this is text\n', suffix = '.txt')
-    mt = file_mime.mime_type(tmp)
-    self.assertEqual( 'text/plain', mt.mime_type )
-    if mt.charset:
-      self.assertEqual( 'us-ascii', mt.charset )
-
-  def test_is_false(self):
-    self.assertFalse( file_mime.is_binary(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
-    
-  @unit_test_function_skip.skip_if_not_unix()
-  def test_is_binary_unix(self):
-    self.assertTrue( file_mime.is_binary(file_symlink.resolve('/bin/sh')) )
-    
-  @unit_test_function_skip.skip_if_not_windows()
-  def test_is_binary_windows(self):
-    self.assertTrue( file_mime.is_binary(sys.executable) )
-    
-  def test_is_text(self):
-    self.assertTrue( file_mime.is_text(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
-
-  def test_content_is_text_true(self):
-    self.assertTrue( file_mime.content_is_text(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
-    
-  def test_content_is_text_false(self):
-    self.assertFalse( file_mime.content_is_text(self.make_temp_file(content = unit_test_media.PNG_SMALLEST_POSSIBLE, suffix = '.png')) )
-
   @cached_property
   def _png_file(self):
     return self.make_temp_file(content = unit_test_media.PNG_SMALLEST_POSSIBLE, suffix = '.png')
@@ -66,6 +39,36 @@ class test_file_mime(unit_test):
   def _mp4_file_wrong_extension(self):
     return self.make_temp_file(content = unit_test_media.MP4_SMALLEST_POSSIBLE, suffix = '.txt')
   
+  def test_mime_type(self):
+    tmp = self.make_temp_file(content = 'this is text\n', suffix = '.txt')
+    mt = file_mime.mime_type(tmp)
+    self.assertEqual( 'text/plain', mt.mime_type )
+    if mt.charset:
+      self.assertEqual( 'us-ascii', mt.charset )
+
+  def test_is_binary_false(self):
+    self.assertFalse( file_mime.is_binary(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
+    
+  @unit_test_function_skip.skip_if_not_unix()
+  def test_is_binary_unix(self):
+    self.assertTrue( file_mime.is_binary(file_symlink.resolve('/bin/sh')) )
+    
+  @unit_test_function_skip.skip_if_not_windows()
+  def test_is_binary_windows(self):
+    self.assertTrue( file_mime.is_binary(sys.executable) )
+    
+  def test_is_text_true(self):
+    self.assertTrue( file_mime.is_text(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
+
+  def test_is_text_false(self):
+    self.assertFalse( file_mime.is_text(self._png_file) )
+    
+  def test_content_is_text_true(self):
+    self.assertTrue( file_mime.content_is_text(self.make_temp_file(content = 'this is text\n', suffix = '.txt')) )
+    
+  def test_content_is_text_false(self):
+    self.assertFalse( file_mime.content_is_text(self.make_temp_file(content = unit_test_media.PNG_SMALLEST_POSSIBLE, suffix = '.png')) )
+
   def test_png(self):
     self.assertEqual( 'image/png', file_mime.mime_type(self._png_file).mime_type )
 
@@ -86,10 +89,6 @@ class test_file_mime(unit_test):
   @unit_test_function_skip.skip_if_not_unix(warning = True)
   def test_mp4_wrong_extension(self):
     self.assertEqual( 'video/mp4', file_mime.mime_type(self._mp4_file_wrong_extension).mime_type )
-    
-  @unit_test_function_skip.skip_if_not_unix()
-  def test_is_text_false_unix(self):
-    self.assertFalse( file_mime.is_text(file_symlink.resolve('/bin/sh')) )
 
 if __name__ == '__main__':
   unit_test.main()
