4be7b0748002d0cbbdb2558312444a681dd7d83d
Ramiro Estrugo <git@fateware.com>
2021-12-19 10:55:49 -0800

    search_files skeleton and unit test

--- lib/bes/refactor/refactor_files.py
+++ lib/bes/refactor/refactor_files.py
@@ -1,12 +1,15 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from os import path
+
+from bes.common.algorithm import algorithm
 from bes.fs.file_check import file_check
+from bes.fs.file_mime import file_mime
 from bes.fs.file_resolver import file_resolver
 from bes.fs.file_resolver_options import file_resolver_options
-from bes.fs.file_mime import file_mime
-from bes.system.python import python
+from bes.fs.file_search import file_search
 from bes.fs.filename_util import filename_util
+from bes.system.python import python
 
 class refactor_files(object):
 
@@ -24,3 +27,15 @@ class refactor_files(object):
                                     match_basename = False)
     resolved = file_resolver.resolve_files(files, options = options)
     return resolved.files()
+
+  @classmethod
+  def search_files(clazz, files, text, word_boundary = False, ignore_case = False):
+    'Search for text in files.'
+    
+    result = []
+    for filename in files:
+      next_matches = file_search.search_file(filename, text,
+                                             word_boundary = word_boundary,
+                                             ignore_case = ignore_case)
+      result.extend(next_matches)
+    return sorted(algorithm.unique([ item.filename for item in result ]))
--- tests/lib/bes/refactor/test_refactor_files.py
+++ tests/lib/bes/refactor/test_refactor_files.py
@@ -36,6 +36,47 @@ class test_refactor_files(unit_test, unit_test_media_files):
       f'{tmp_dir}/wine/bin/wscript',
       f'{tmp_dir}/wine/src/barolo.py',
     ], refactor_files.resolve_python_files([ path.join(tmp_dir, f) for f in [ 'fruit', 'wine', 'foo.py' ] ]) )
+
+  def test_search_files(self):
+    self.maxDiff = None
+
+    kiwi_py = '''\
+class kiwi(object):
+  def __init__(self, x):
+    self._x = x
+
+  @property
+  def x(self):
+    return self._x
+
+  @classmethod
+  def make_kiwi(clazz, x):
+    return kiwi(x)
+'''
+
+    test_kiwi_py = '''\
+from bes.testing.unit_test import unit_test
+class test_kiwi(unit_test):
+
+  def test__init__(self):
+    self.assertEqual( 666, kiwi(666).x )
+
+if __name__ == '__main__':
+  unit_test.main()
+'''
+    
+    tmp_dir = self._make_temp_content([
+      temp_content('file', 'lib/fruit/kiwi.py', kiwi_py, 0o0644),
+      temp_content('file', 'tests/lib/fruit/test_kiwi.py', test_kiwi_py, 0o0644),
+    ])
+    [ path.join(tmp_dir, f) for f in [ 'fruit', 'wine', 'foo.py' ] ]
+    self.assert_filename_list_equal( [
+      f'{tmp_dir}/lib/fruit/kiwi.py',
+      f'{tmp_dir}/tests/lib/fruit/test_kiwi.py',
+    ], refactor_files.search_files([
+      f'{tmp_dir}/lib/fruit/kiwi.py',
+      f'{tmp_dir}/tests/lib/fruit/test_kiwi.py',
+    ], 'kiwi', word_boundary = False, ignore_case = False) )
     
 if __name__ == '__main__':
   unit_test.main()
