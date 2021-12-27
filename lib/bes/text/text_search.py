5d4571e451450e0f44394df294a94ba8435e89b7
Ramiro Estrugo <git@fateware.com>
2021-12-27 09:21:24 -0800

    move all the replace functions into text_search

--- lib/bes/text/text_search.py
+++ lib/bes/text/text_search.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+import string
+
 from collections import namedtuple
 
 from bes.system.check import check
@@ -48,7 +50,7 @@ class text_search(object):
         do_yield = True
       if do_yield:
         yield clazz._span(start, end)
-
+        
   @classmethod
   def replace_span(clazz, text, start, end, replacement):
     'Replace a span of text from start to end with replacement'
@@ -81,50 +83,43 @@ class text_search(object):
     check.check_bool(word_boundary)
     check.check_set(boundary_chars, allow_none = True)
 
-    spans = clazz.find_all(text, src_string, word_boundary = word_boundary, boundary_chars = boundary_chars)
+    spans = clazz.find_all(text,
+                           src_string,
+                           word_boundary = word_boundary,
+                           boundary_chars = boundary_chars)
     for span in reversed(spans):
-      text = clazz.replace_span(text, span.start, span.end, dst_string,
-                                word_boundary = word_boundary,
-                                underscore = underscore)
+      text = clazz.replace_span(text, span.start, span.end, dst_string)
     return text
-  
-'''      
+
   @classmethod
-  def replace_all(clazz, s, src_string, dst_string, word_boundary = True, underscore = False):
+  def replace_all_fast(clazz, text, src_string, dst_string, word_boundary = False, boundary_chars = None):
     'Replace src_string with dst_string optionally respecting word boundaries.'
-    check.check_string(s)
+    check.check_string(text)
     check.check_string(src_string)
     check.check_string(dst_string)
     check.check_bool(word_boundary)
-    check.check_bool(underscore)
+    check.check_set(boundary_chars, allow_none = True)
 
-    indeces = [ i for i in clazz.find_all(s, src_string) ]
-    rindeces = reversed(indeces)
-    n = len(src_string)
-    for i in reversed(indeces):
-      s = clazz.replace_span(s, i, n, dst_string,
-                             word_boundary = word_boundary,
-                             underscore = underscore)
-    return s
-  
-  @classmethod
-  def is_string(clazz, s):
-    'Return True if s is a string.'
-    return compat.is_string(s)
-    
-  @classmethod
-  def is_char(clazz, s):
-    'Return True if s is 1 character.'
-    return clazz.is_string(s) and len(s) == 1
-
-  @classmethod
-  def is_ascii(clazz, s):
-    'Return True if s is ascii.'
-    try:
-      s.decode('ascii')
-      return True
-    except Exception as ex:
-      return False
+    spans = clazz.find_all(text,
+                           src_string,
+                           word_boundary = word_boundary,
+                           boundary_chars = boundary_chars)
+    if not spans:
+      return text
+    last_start = 0
+    buf = StringIO()
+    last_span = None
+    for span in spans:
+      left = text[last_start : span.start]
+      if left:
+        buf.write(left)
+      buf.write(dst_string)
+      last_start = span.end + 1
+      last_span = span
+    if last_span:
+      right = text[last_span.end + 1:]
+      buf.write(right)
+    return buf.getvalue()
 
   @classmethod
   def replace_punctuation(clazz, s, replacement):
@@ -139,135 +134,23 @@ class text_search(object):
     return buf.getvalue()
 
   @classmethod
-  def flatten(clazz, s, delimiter = ' '):
-    'Flatten the given collection to a string.'
-    'If s is already a string just return it.'
-    if clazz.is_string(s):
-      return s
-    if isinstance(s, list):
-      return delimiter.join(s)
-    raise TypeError('Not a string or list')
+  def replace_white_space(clazz, s, replacement):
+    'Replace white space sequences in s with replacement.'
+    buf = StringIO()
+    STATE_CHAR = 1
+    STATE_SPACE = 2
 
-  JUSTIFY_LEFT = 1
-  JUSTIFY_RIGHT = 2
-  @classmethod
-  def justify(clazz, s, justification, width, fill = ' '):
-    'Justify a string within width with fill.'
-    assert len(fill) == 1
-    assert justification in [ clazz.JUSTIFY_LEFT, clazz.JUSTIFY_RIGHT ]
-    length = len(s)
-    if length >= width:
-      return s
-    fill_string = fill * (width - length)
-    if justification == clazz.JUSTIFY_LEFT:
-      return s + fill_string
-    else:
-      return fill_string + s
-
-  @classmethod
-  def left_justify(clazz, s, width, fill = ' '):
-    'Left justify a string within width with fill.'
-    return clazz.justify(s, clazz.JUSTIFY_LEFT, width, fill = fill)
-
-  @classmethod
-  def right_justify(clazz, s, width, fill = ' '):
-    'Right justify a string within width with fill.'
-    return clazz.justify(s, clazz.JUSTIFY_RIGHT, width, fill = fill)
-
-  @classmethod
-  def quote(clazz, s, quote_char = None):
-    'Quote a string.'
-    if quote_char:
-      if clazz.is_quoted(s, quote_char = quote_char):
-        return s
-      if quote_char == '"' and clazz.is_single_quoted(s) or quote_char == "'" and clazz.is_double_quoted(s):
-        s = clazz.unquote(s)
-      return quote_char + s + quote_char
-    if clazz.is_quoted(s):
-      return s
-    has_single_quote = "'" in s
-    has_double_quote = '"' in s
-    if has_single_quote and has_double_quote:
-      raise RuntimeError('Cannot quote a string with both single and double quotes: %s' % (s))
-    if has_double_quote:
-      return "'" + s + "'"
-    return '"' + s + '"'
-
-  @classmethod
-  def unquote(clazz, s):
-    'Unquote a string.'
-    if not clazz.is_quoted(s):
-      return s
-    return s[1:-1]
-  
-  @classmethod
-  def is_quoted(clazz, s, quote_char = None):
-    'Return True if s is quoted.'
-    if len(s) < 2:
-      return False
-    if quote_char:
-      quote_chars = [ quote_char ]
-    else:
-      quote_chars = [ r'"', r"'" ]
-    for quote in quote_chars:
-      if s[0] == s[-1] == quote:
-        return True
-    return False
-
-  @classmethod
-  def is_single_quoted(clazz, s):
-    'Return True if s is single quoted.'
-    return clazz.is_quoted(s, quote_char = "'")
-
-  @classmethod
-  def is_double_quoted(clazz, s):
-    'Return True if s is doubleb quoted.'
-    return clazz.is_quoted(s, quote_char = '"')
-
-  @classmethod
-  def quote_if_needed(clazz, s, quote_char = None):
-    if clazz.has_white_space(s):
-      return clazz.quote(s, quote_char = quote_char)
-    else:
-      return s
-
-  @classmethod
-  def escape_quotes(clazz, s):
-    'Escape any single or double quotes in s.'
-    s = s.replace(r"'", "\\'")
-    s = s.replace(r'"', '\\"')
-    return s
-
-  @classmethod
-  def escape_spaces(clazz, s):
-    'Escape spaces in s.'
-    s = s.replace(r" ", "\\ ")
-    return s
-
-  @classmethod
-  def has_white_space(clazz, s):
-    'Return True if s has any white space.'
+    state = STATE_CHAR
     for c in s:
-      if c.isspace():
-        return True
-    return False
-
-  @classmethod
-  def reverse(clazz, s):
-    return ''.join(reversed(s))
-
-  @classmethod
-  def strip_ends(clazz, s, strip_head = False, strip_tail = False):
-    if strip_head and strip_tail:
-      return s.strip()
-    elif strip_head:
-      return s.lstrip()
-    elif strip_tail:
-      return s.rstrip()
-    return s
-
-  @classmethod
-  def insert(clazz, s, what, index):
-    'Insert what into s at index'
-    return s[:index] + what + s[index:]
-'''
+      if state == STATE_CHAR:
+        if c.isspace():
+          buf.write(replacement)
+          state = STATE_SPACE
+        else:
+          buf.write(c)
+      elif state == STATE_SPACE:
+        if not c.isspace():
+          buf.write(c)
+          state = STATE_CHAR
+    return buf.getvalue()
+  
--- tests/lib/bes/text/test_text_search.py
+++ tests/lib/bes/text/test_text_search.py
@@ -31,50 +31,21 @@ class test_text_search(unit_test):
       f('foo', -1, 1, 'x')
     with self.assertRaises(ValueError) as ctx:
       f('foo', 1, 0, 'x')
-    
-#    self.assertEqual( [], f('bar', 'foo') )
-#    self.assertEqual( [ 0 ], f('foo', 'foo') )
-#    self.assertEqual( [ 4, 16 ], f('foo and bar foo and foo', 'and') )
-'''
-  def test_replace_span(self):
-    f = SU.replace_span
-    self.assertEqual( 'foo & bar foo and foo', f('foo and bar foo and foo', 4, 3, '&', word_boundary = False) )
-    self.assertEqual( 'xoo', f('foo', 0, 1, 'x', word_boundary = False) )
-    self.assertEqual( 'fxo', f('foo', 1, 1, 'x', word_boundary = False) )
-    self.assertEqual( 'fox', f('foo', 2, 1, 'x', word_boundary = False) )
 
-    with self.assertRaises(ValueError) as ctx:
-      f('foo', 3, 1, 'x')
-    with self.assertRaises(ValueError) as ctx:
-      f('foo', -1, 1, 'x')
-    with self.assertRaises(ValueError) as ctx:
-      f('foo', 0, 0, 'x')
-
-  def test_replace_span_with_word_boundary(self):
-    f = SU.replace_span
-    self.assertEqual( 'foo.&:bar', f('foo.and:bar', 4, 3, '&', word_boundary = True) )
-    self.assertEqual( 'fooxandybar', f('fooxandybar', 4, 3, '&', word_boundary = True) )
-    self.assertEqual( 'x', f('foo', 0, 3, 'x', word_boundary = True) )
-    self.assertEqual( 'foo_', f('foo_', 0, 3, 'x', word_boundary = True) )
-    self.assertEqual( '_foo', f('_foo', 1, 3, 'x', word_boundary = True) )
-
-  def test_replace_span_with_word_boundary_and_underscore(self):
-    f = SU.replace_span
-    self.assertEqual( 'x', f('foo', 0, 3, 'x', word_boundary = True, underscore = True) )
-    self.assertEqual( 'x_', f('foo_', 0, 3, 'x', word_boundary = True, underscore = True) )
-    self.assertEqual( '_x', f('_foo', 1, 3, 'x', word_boundary = True, underscore = True) )
-    
   def test_replace_all(self):
-    f = SU.replace_all
+    f = text_search.replace_all_fast
+    self.assertEqual( 'this is nothing!', f('this is nothing!', 'foo', 'kiwi', word_boundary = False) )
     self.assertEqual( 'foo & bar foo & foo', f('foo and bar foo and foo', 'and', '&', word_boundary = False) )
     self.assertEqual( 'foo and bar foo and foo', f('foo and bar foo and foo', 'kiwi', '&', word_boundary = False) )
     self.assertEqual( 'fxx', f('foo', 'o', 'x', word_boundary = False) )
     self.assertEqual( 'foo', f('foo', 'o', 'o', word_boundary = False) )
     self.assertEqual( 'fxxxx', f('foo', 'o', 'xx', word_boundary = False) )
     self.assertEqual( 'foooo', f('foo', 'o', 'oo', word_boundary = False) )
+    self.assertEqual( 'kiwi', f('foo', 'foo', 'kiwi', word_boundary = False) )
+    self.assertEqual( 'kiwi ', f('foo ', 'foo', 'kiwi', word_boundary = False) )
 
   def test_replace_all_with_word_boundary(self):
-    f = SU.replace_all
+    f = text_search.replace_all_fast
     self.assertEqual( 'foo & bar foo & foo', f('foo and bar foo and foo', 'and', '&', word_boundary = True) )
     self.assertEqual( 'foo and bar foo and foo', f('foo and bar foo and foo', 'kiwi', '&', word_boundary = True) )
     self.assertEqual( 'foo', f('foo', 'o', 'x', word_boundary = True) )
@@ -82,6 +53,26 @@ class test_text_search(unit_test):
     self.assertEqual( 'foo', f('foo', 'o', 'xx', word_boundary = True) )
     self.assertEqual( 'foo', f('foo', 'o', 'oo', word_boundary = True) )
     self.assertEqual( 'o', f('foo', 'foo', 'o', word_boundary = True) )
-'''    
+
+  def test_replace_punctuation(self):
+    f = text_search.replace_punctuation
+    self.assertEqual( 'foo', f('foo', None) )
+    self.assertEqual( 'foo', f('foo!', None) )
+    self.assertEqual( 'foo1', f('foo!', '1') )
+    self.assertEqual( 'foo bar', f('foo,bar', ' ') )
+
+  def test_replace_white_space(self):
+    f = text_search.replace_white_space
+    self.assertEqual( 'a b c', f('a   b   c', ' ') )
+    self.assertEqual( 'a b c', f('a b c', ' ') )
+    self.assertEqual( 'a b c', f('a  b  c', ' ') )
+    self.assertEqual( ' a b c', f(' a  b  c', ' ') )
+    self.assertEqual( ' a b c', f('  a  b  c', ' ') )
+    self.assertEqual( 'a b c ', f('a   b   c ', ' ') )
+    self.assertEqual( 'a b c ', f('a   b   c  ', ' ') )
+    self.assertEqual( 'a b c ', f('a   b   c   ', ' ') )
+    self.assertEqual( 'a_b_c_', f('a   b   c   ', '_') )
+    self.assertEqual( '_a_b_c_', f(' a   b   c   ', '_') )
+    
 if __name__ == '__main__':
   unit_test.main()
