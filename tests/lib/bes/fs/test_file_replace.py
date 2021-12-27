e678821b925ad1a6e28f15a1ae9a9271da09fb7c
Ramiro Estrugo <git@fateware.com>
2021-12-27 13:28:49 -0800

    add word and no word boundary tests

--- tests/lib/bes/fs/test_file_replace.py
+++ tests/lib/bes/fs/test_file_replace.py
@@ -7,22 +7,31 @@ from bes.fs.file_replace import file_replace
 class test_file_replace(unit_test):
 
   def test_file_replace_ascii(self):
-    tmp = self.make_temp_file(content = 'This is foo.\n')
+    tmp = self.make_temp_file(content = 'This is foo fooey.\n')
     replacements = {
       'This': 'That',
       'foo': 'bar',
     }
-    file_replace.replace(tmp, replacements, backup = False, word_boundary = True)
-    self.assert_text_file_equal( 'That is bar.\n', tmp )
+    file_replace.replace(tmp, replacements, backup = False, word_boundary = False)
+    self.assert_text_file_equal( 'That is bar barey.\n', tmp )
 
   def test_file_replace_utf8(self):
-    tmp = self.make_temp_file(content = 'This is bér.\n')
+    tmp = self.make_temp_file(content = 'This is bér béry.\n')
     replacements = {
       'This': 'That',
       'bér': 'föö',
     }
     file_replace.replace(tmp, replacements, backup = False, word_boundary = True)
-    self.assert_text_file_equal( 'That is föö.\n', tmp )
+    self.assert_text_file_equal( 'That is föö béry.\n', tmp )
+
+  def test_file_replace_ascii_with_word_boundary(self):
+    tmp = self.make_temp_file(content = 'This is foo fooey.\n')
+    replacements = {
+      'This': 'That',
+      'foo': 'bar',
+    }
+    file_replace.replace(tmp, replacements, backup = False, word_boundary = True)
+    self.assert_text_file_equal( 'That is bar fooey.\n', tmp )
 
 if __name__ == '__main__':
   unit_test.main()
