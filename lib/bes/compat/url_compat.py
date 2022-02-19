eb0ec032d2fbcf34a153cc7e12ab6163ca123885
Ramiro Estrugo <git@fateware.com>
2022-02-19 11:51:51 -0800

    update

--- lib/bes/compat/url_compat.py
+++ lib/bes/compat/url_compat.py
@@ -4,13 +4,13 @@ from bes.system.compat import compat
 from bes.system.host import host
 
 if compat.IS_PYTHON2:
-  from urlparse import urlparse, urljoin, urlsplit, urlsplit
+  from urlparse import urlparse, urlunparse, urljoin, urlsplit, urlsplit
   from urllib import urlencode
   from urllib2 import urlopen as urlopen
   from urllib2 import Request
   from urllib2 import HTTPError
 else:
-  from urllib.parse import urlparse, urljoin, urlencode, urlsplit
+  from urllib.parse import urlparse, urlunparse, urljoin, urlencode, urlsplit
   from urllib.request import urlopen as urlopen
   from urllib.request import Request
   from urllib.error import HTTPError
