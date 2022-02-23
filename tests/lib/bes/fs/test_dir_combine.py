65f2cb7772c06a418f6c7ed128d590e13d75bafa
Ramiro Estrugo <git@fateware.com>
2022-02-23 15:39:21 -0800

    more tests

--- tests/lib/bes/fs/test_dir_combine.py
+++ tests/lib/bes/fs/test_dir_combine.py
@@ -14,6 +14,32 @@ from _bes_unit_test_common.dir_operation_tester import dir_operation_tester
 
 class test_dir_combine(unit_test, unit_test_media_files):
 
+  def test_combine_recursive(self):
+    t = self._combine_test([
+      'file src/a/kiwi-30.jpg      "kiwi-30.txt"    644',
+      'file src/a/lemon-30.jpg     "lemon-30.txt"   644',
+      'file src/a/grape-30.jpg     "grape-30.txt"   644',
+      'file src/b/brie-30.jpg      "brie-30.txt"    644',
+      'file src/b/cheddar-30.jpg   "cheddar-30.txt" 644',
+      'file src/b/gouda-30.jpg     "gouda-30.txt"   644',
+      'file src/c/barolo-10.jpg    "barolo-10.txt"  644',
+      'file src/c/chablis-10.jpg   "chablis-10.txt"  644',
+      'file src/d/steak-10.jpg     "steak-10.txt"  644',
+    ], recursive = True, files = [ 'a', 'b', 'c', 'd' ])
+    expected = [
+      'a',
+      'a/barolo-10.jpg',
+      'a/brie-30.jpg',
+      'a/chablis-10.jpg',
+      'a/cheddar-30.jpg',
+      'a/gouda-30.jpg',
+      'a/grape-30.jpg',
+      'a/kiwi-30.jpg',
+      'a/lemon-30.jpg',
+      'a/steak-10.jpg',
+    ]
+    self.assert_filename_list_equal( expected, t.src_files )
+  
   def test_combine(self):
     items = [
       temp_content('file', 'src/readme.md', 'readme.md', 0o0644),
@@ -33,20 +59,8 @@ class test_dir_combine(unit_test, unit_test_media_files):
     t = self._combine_test(extra_content_items = items,
                            dst_dir_same_as_src = False,
                            recursive = False,
-                           partition_type = 'prefix',
-                           files = [ 'a', 'b' ])
+                           partition_type = 'prefix')
     dst_after_expected = [
-      'a',
-      'a/kiwi-10.jpg',
-      'a/kiwi-20.jpg',
-      'a/kiwi-30.jpg',
-      'a/lemon-10.jpg',
-      'a/lemon-20.jpg',
-      'a/lemon-30.jpg',
-      'c',
-      'c/cheese-10.jpg',
-      'icons',
-      'icons/foo.png',
       'kiwi-40.jpg',
       'kiwi-50.jpg',
       'lemon-40.jpg',
@@ -105,84 +119,6 @@ class test_dir_combine(unit_test, unit_test_media_files):
       'readme.md',
     ]
     self.assert_filename_list_equal( dst_after_expected, t.src_files )
-    
-  def xtest_partition_with_prefix_recursive(self):
-    items = [
-      temp_content('file', 'src/readme.md', 'readme.md', 0o0644),
-      temp_content('file', 'src/a/kiwi-10.jpg', 'kiwi-10.txt', 0o0644),
-      temp_content('file', 'src/a/kiwi-20.jpg', 'kiwi-20.txt', 0o0644),
-      temp_content('file', 'src/a/kiwi-30.jpg', 'kiwi-30.txt', 0o0644),
-      temp_content('file', 'src/b/lemon-10.jpg', 'lemon-10.txt', 0o0644),
-      temp_content('file', 'src/b/lemon-20.jpg', 'lemon-20.txt', 0o0644),
-      temp_content('file', 'src/b/lemon-30.jpg', 'lemon-30.txt', 0o0644),
-      temp_content('file', 'src/c/cheese-10.jpg', 'cheese-10.jpg', 0o0644),
-      temp_content('file', 'src/icons/foo.png', 'foo.png', 0o0644),
-      temp_content('file', 'src/kiwi-40.jpg', 'kiwi-40.txt', 0o0644),
-      temp_content('file', 'src/lemon-40.jpg', 'lemon-40.txt', 0o0644),
-    ]
-    t = self._combine_test(extra_content_items = items,
-                           dst_dir_same_as_src = False,
-                           recursive = True,
-                           partition_type = 'prefix')
-    dst_after_expected = [
-      'kiwi',
-      'kiwi/kiwi-10.jpg',
-      'kiwi/kiwi-20.jpg',
-      'kiwi/kiwi-30.jpg',
-      'kiwi/kiwi-40.jpg',
-      'lemon',
-      'lemon/lemon-10.jpg',
-      'lemon/lemon-20.jpg',
-      'lemon/lemon-30.jpg',
-      'lemon/lemon-40.jpg',
-    ]
-    self.assert_filename_list_equal( dst_after_expected, t.dst_files )
-    src_after_expected = [
-      'c',
-      'c/cheese-10.jpg',
-      'icons',
-      'icons/foo.png',
-      'readme.md',
-    ]
-    self.assert_filename_list_equal( src_after_expected, t.src_files )
-
-  def xtest_partition_with_media_type(self):
-    items = [
-      temp_content('file', 'src/apple.jpg', unit_test_media.JPG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/barolo.mp4', unit_test_media.MP4_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/brie.png', unit_test_media.PNG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/chablis.mp4', unit_test_media.MP4_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/cheddar.png', unit_test_media.PNG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/kiwi.jpg', unit_test_media.JPG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/lemon.jpg', unit_test_media.JPG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/malbec.mp4', unit_test_media.MP4_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/swiss.png', unit_test_media.PNG_SMALLEST_POSSIBLE, 0o0644),
-      temp_content('file', 'src/yogurt.foo', unit_test_media.UNKNOWN, 0o0644),
-      temp_content('file', 'src/zabaglione.foo', unit_test_media.UNKNOWN, 0o0644),
-    ]
-    t = self._combine_test(extra_content_items = items,
-                             dst_dir_same_as_src = False,
-                             recursive = False,
-                             partition_type = 'media_type')
-    dst_after_expected = [
-      'image',
-      'image/apple.jpg',
-      'image/brie.png',
-      'image/cheddar.png',
-      'image/kiwi.jpg',
-      'image/lemon.jpg',
-      'image/swiss.png',
-      'video',
-      'video/barolo.mp4',
-      'video/chablis.mp4',
-      'video/malbec.mp4',
-    ]
-    self.assert_filename_list_equal( dst_after_expected, t.dst_files )
-    src_after_expected = [
-      'yogurt.foo',
-      'zabaglione.foo',
-    ]
-    self.assert_filename_list_equal( src_after_expected, t.src_files )
     
   def _combine_test(self,
                     extra_content_items = None,
