09e7f8ed2dcb2a56aa859b1740dbbe6001877c4a
Ramiro Estrugo <git@fateware.com>
2022-03-06 22:36:29 -0800

    works

--- tests/lib/bes/url/test_parsed_url.py
+++ tests/lib/bes/url/test_parsed_url.py
@@ -10,15 +10,30 @@ class test_parsed_url(unit_test):
     f = parsed_url.parse
     self.assertEqual( ( 'http', 'www.example.com', '/foo.cgi', '', '', '' ), f('http://www.example.com/foo.cgi') )
 
-  def xtest_remove_query(self):
-    f = url_util.remove_query
-    self.assertEqual( 'http://www.example.com/foo.cgi', f('http://www.example.com/foo.cgi?a=6&b=7&foo=666') )
-
-  def xtest_normalize(self):
-    f = url_util.normalize
-    self.assertEqual( 'http://www.example.com/foo.cgi', f('http://www.example.com/foo.cgi') )
-    self.assertEqual( 'http://www.example.com/', f('http://www.example.com') )
-    self.assertEqual( 'http://www.example.com/', f('http://www.example.com/') )
+  def test_netloc(self):
+    def _t(s):
+      return parsed_url.parse(s).netloc
+    self.assertEqual( 'www.example.com', _t('http://www.example.com/foo.cgi') )
     
+  def test_netloc_name(self):
+    def _t(s):
+      return parsed_url.parse(s).netloc_name
+    self.assertEqual( 'example', _t('http://www.example.com/foo.cgi') )
+    self.assertEqual( 'example', _t('http://example.com/foo.cgi') )
+    self.assertEqual( 'example', _t('http://example.edu/foo.cgi') )
+    
+  def test_remove_query(self):
+    def _t(s):
+      return parsed_url.parse(s).remove_query()
+    self.assertEqual( 'http://www.example.com/foo.cgi', _t('http://www.example.com/foo.cgi?a=6&b=7&foo=666') )
+
+  def test_normalized(self):
+    def _t(s):
+      return parsed_url.parse(s).normalized()
+    self.assertEqual( 'http://www.example.com/', _t('http://www.example.com/') )
+    self.assertEqual( 'http://www.example.com/', _t('http://www.example.com') )
+    self.assertEqual( 'http://www.example.com/foo.cgi', _t('http://www.example.com/foo.cgi') )
+
+
 if __name__ == '__main__':
   unit_test.main()
