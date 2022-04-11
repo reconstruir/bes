2f4b115809deeed2b60f554cbc163206952ecd22
Ramiro Estrugo <git@fateware.com>
2022-04-10 17:04:47 -0700

    add an error class and give the mixin the option of changing the class of the error raised

--- lib/bes/sqlite/sqlite_item_mixin.py
+++ lib/bes/sqlite/sqlite_item_mixin.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from bes.system.check import check
+from ..system.check import check
+
+from .sqlite_error import sqlite_error
 
 class sqlite_item_mixin:
   'This mixing assumes that "self" is a namedtuple'
@@ -22,12 +24,15 @@ class sqlite_item_mixin:
     return f'replace into {table_name}({keys}) values({values})'
 
   @classmethod
-  def from_sql_row(clazz, row):
+  def from_sql_row(clazz, row, error_class = None):
     check.check_tuple(row)
+    check.check_class(error_class, allow_none = True)
 
+    error_class = error_class or sqlite_error
+    
     length = len(row)
     expected_length = len(clazz._fields)
     if len(row) != len(clazz._fields):
-      raise vb_error('Length of row should be {expected_length} instead of {length} - {row}')
+      raise error_class(f'Length of row should be {expected_length} instead of {length} - {row}')
     return clazz(*row)
   
