de28c90e0e18698a9eb6e1f33e29a88865ca387b
Ramiro Estrugo <git@fateware.com>
2022-06-20 02:40:52 -0700

    update for new strategy

--- env/bes_venv_setup.cmd
+++ env/bes_venv_setup.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-call %~dp0\..\bin\best.cmd pip_project install_requirements --root-dir "%~dp0\..\VE\bes" %~dp0\..\requirements.txt
-call %~dp0\..\bin\best.cmd pip_project install_requirements --root-dir "%~dp0\..\VE\bes" %~dp0\..\requirements-dev.txt
+call %~dp0\..\bin\best.cmd pip_project install_requirements --root-dir "%~dp0\..\VE\bes" %~dp0\..\bes_requirements.txt %~dp0\..\bes_requirements_dev.txt
 
 set PATH=%~dp0\..\bin;%PATH%
