4f230bb7fcfed9e3474a65bf9627817bc69d4ca9
Ramiro Estrugo <git@fateware.com>
2022-06-29 19:14:55 -0700

    for octect stream results with the first try, use file_exe if available as a second level to get a better type

--- lib/bes/fs/file_mime_type_detector.py
+++ lib/bes/fs/file_mime_type_detector.py
@@ -18,14 +18,15 @@ class file_mime_type_detector(_file_mime_type_detector_base):
     'Return True if this class is supported on the current platform.'
     return True
 
-  _POSSIBLE = [
+  _ALL_DETECTORS = [
     _file_mime_type_detector_magic,
     _file_mime_type_detector_puremagic,
     _file_mime_type_detector_cheesy,
     _file_mime_type_detector_file_exe,
     _file_mime_type_detector_mimetypes,
   ]
-  _POSSIBLE = [ p for p in _POSSIBLE if p.is_supported() ]
+  _POSSIBLE_DETECTORS = [ p for p in _ALL_DETECTORS if p.is_supported() ]
+  _FILE_EXE_SUPPORTED = _file_mime_type_detector_file_exe in _POSSIBLE_DETECTORS
   
   @classmethod
   #@abstractmethod
@@ -33,42 +34,12 @@ class file_mime_type_detector(_file_mime_type_detector_base):
     'Detect the mime type for file.'
     filename = file_check.check_file(filename)
 
-    for p in clazz._POSSIBLE:
-      mime_type = p.detect_mime_type(filename)
+    mime_type = None
+    for next_detector in clazz._POSSIBLE_DETECTORS:
+      mime_type = next_detector.detect_mime_type(filename)
       if mime_type:
-        return mime_type
-    return None
-
-#####from bes.system.host import host
-#####
-#####HAS_MAGIC = False
-#####try:
-#####  import magic
-#####  HAS_MAGIC = True
-#####except ModuleNotFoundError as ex:
-#####  pass
-#####
-#####HAS_PUREMAGIC = False
-#####try:
-#####  import puremagic
-#####  HAS_PUREMAGIC = True
-#####except ModuleNotFoundError as ex:
-#####  pass
-#####
-######HAS_MAGIC = False
-######HAS_PUREMAGIC = False
-######print('HAS_MAGIC={} HAS_PUREMAGIC={}'.format(HAS_MAGIC, HAS_PUREMAGIC))
-#####
-#####if HAS_MAGIC:
-#####  from ._detail._file_mime_type_detector_magic import _file_mime_type_detector_magic as _file_mime_type_de#####tector_super_class
-#####elif HAS_PUREMAGIC:
-#####  from ._detail._file_mime_type_detector_puremagic import _file_mime_type_detector_puremagic as _file_mime_type_detector_super_class
-#####elif host.is_unix():
-#####  from ._detail._file_mime_type_detector_file_exe import _file_mime_type_detector_file_exe as _file_mime_type_detector_super_class
-#####elif host.SYSTEM == host.WINDOWS:
-#####  from ._detail._file_mime_type_detector_mimetypes import _file_mime_type_detector_mimetypes as _file_mime_type_detector_super_class
-#####else:
-#####  host.raise_unsupported_system()
-#####
-#####class file_mime_type_detector(_file_mime_type_detector_super_class):
-#####  pass
+        break
+    if mime_type in ( 'application/octet-stream', ):
+      if clazz._FILE_EXE_SUPPORTED:
+        mime_type = _file_mime_type_detector_file_exe.detect_mime_type(filename)
+    return mime_type
