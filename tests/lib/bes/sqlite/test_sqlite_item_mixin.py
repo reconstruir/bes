34b48f6751bb61ffb34498ccfe34774f691d5e4d
Ramiro Estrugo <git@fateware.com>
2022-07-10 16:00:42 -0700

    add test_from_sql_row

--- tests/lib/bes/sqlite/test_sqlite_item_mixin.py
+++ tests/lib/bes/sqlite/test_sqlite_item_mixin.py
@@ -8,17 +8,20 @@ from bes.sqlite.sqlite_item_mixin import sqlite_item_mixin
 
 class test_sqlite_item_mixin(unit_test):
 
-  class _fruit(namedtuple('_fruit', 'color, flavor, price'), sqlite_item_mixin):
-    def __new__(clazz, color, flavor, price):
-      return clazz.__bases__[0].__new__(clazz, color, flavor, price)
+  class _fruit(namedtuple('_fruit', 'name, color, price'), sqlite_item_mixin):
+    def __new__(clazz, name, color, price):
+      return clazz.__bases__[0].__new__(clazz, name, color, price)
 
   def test_sql_for_insert(self):
-    self.assertEqual( 'insert into fruits(color, flavor, price) values(?, ?, ?)',
-                      self._fruit('red', 'apple', 1.42).sql_for_insert('fruits') )
+    self.assertEqual( 'insert into fruits(name, color, price) values(?, ?, ?)',
+                      self._fruit('apple', 'red', 1.42).sql_for_insert('fruits') )
 
   def test_sql_for_replace(self):
-    self.assertEqual( 'replace into fruits(color, flavor, price) values(?, ?, ?)',
-                      self._fruit('red', 'apple', 1.42).sql_for_replace('fruits') )
+    self.assertEqual( 'replace into fruits(name, color, price) values(?, ?, ?)',
+                      self._fruit('apple', 'red', 1.42).sql_for_replace('fruits') )
+
+  def test_from_sql_row(self):
+    self.assertEqual( ( 'apple', 'red', 1.42 ), self._fruit.from_sql_row(( 'apple', 'red', 1.42 )) )
     
 if __name__ == '__main__':
   unit_test.main()
