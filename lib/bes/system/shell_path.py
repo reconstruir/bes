9e5d5879c691a818016f3f8fbf54dc3dab9a2b8e
Ramiro Estrugo <git@fateware.com>
2022-09-03 18:57:18 -0700

    a lot more fleshing out of the api including resolve, append, prepend and check

--- lib/bes/system/shell_path.py
+++ lib/bes/system/shell_path.py
@@ -17,7 +17,7 @@ class shell_path(object):
     'Split a path.'
     check.check_string(p, allow_none = True)
     
-    if p == None:
+    if p in ( None, '' ):
       return []
     return p.split(path.pathsep)
 
@@ -45,30 +45,155 @@ class shell_path(object):
     seen = {}
     unique_parts = [ seen.setdefault(x, x) for x in parts if x not in seen ]
     return [ x for x in unique_parts if x ]
+
+  @classmethod
+  def normalize(clazz, p):
+    check.check_string(p)
+
+    if p == '':
+      return p
+    parts = clazz.split(p)
+    nparts = clazz.normalize_parts(parts)
+    return clazz.join(nparts)
+
+  @classmethod
+  def resolve(clazz, value):
+    if check.is_string(value):
+      return value
+    if check.is_seq(value):
+      for part in value:
+        if not check.is_string(part):
+          raise TypeError(f'part should be string instead of {type(part).__name__}: {part}')
+      return clazz.join(value)
+    raise TypeError(f'value should be string or string sequence instead of {type(part).__name__}: {value}')
+
+  @classmethod
+  def resolve_parts(clazz, value):
+    if check.is_string(value):
+      return clazz.split(value)
+    if check.is_seq(value):
+      for part in value:
+        if not check.is_string(part):
+          raise TypeError(f'part should be string instead of {type(part).__name__}: {part}')
+      return value
+    raise TypeError(f'value should be string or string sequence instead of {type(part).__name__}: {value}')
   
-  _diff_result = namedtuple('_diff_result', 'added, removed')
+  @classmethod
+  def normalize_parts(clazz, parts):
+    check.check_list(parts)
+
+    result = []
+    for part in parts:
+      npart = clazz.normalize_part(part)
+      if npart:
+        result.append(npart)
+    return result
+
+  @classmethod
+  def remove(clazz, p, remove_p):
+    parts = clazz.resolve_parts(p)
+    remove_parts = clazz.resolve_parts(remove_p)
+    new_parts = clazz.remove_parts(parts, remove_parts)
+    return clazz.join(new_parts)
+
+  @classmethod
+  def remove_parts(clazz, parts, remove_parts):
+    clazz.check_parts(parts)
+    clazz.check_parts(remove_parts)
+    
+    remove_set = set(remove_parts)
+    return [ part for part in parts if part not in remove_set ]
+  
+  @classmethod
+  def append(clazz, p1, p2):
+    parts1 = clazz.resolve_parts(p1)
+    parts2 = clazz.resolve_parts(p2)
+
+    new_parts1 = clazz.remove_parts(parts1, parts2)
+    new_parts = new_parts1 + parts2
+    return clazz.join(new_parts)
+
+  @classmethod
+  def prepend(clazz, p1, p2):
+    parts1 = clazz.resolve_parts(p1)
+    parts2 = clazz.resolve_parts(p2)
+
+    new_parts1 = clazz.remove_parts(parts1, parts2)
+    new_parts = parts2 + new_parts1
+    return clazz.join(new_parts)
+  
+  @classmethod
+  def normalize_part(clazz, part):
+    check.check_string(part)
+
+    if part in ( '', None ):
+      return None
+    result = path.normpath(part)
+    count = clazz._count_leading_sep(result)
+    if count > 1:
+      result = result[count - 1:]
+    return result
+
+  @classmethod
+  def _count_leading_sep(clazz, part):
+    count = 0
+    for c in part:
+      if c == path.sep:
+        count += 1
+      else:
+        break
+    return count
+      
+  _diff_result = namedtuple('_diff_result', 'appended, prepended, removed')
   @classmethod
   def diff(clazz, p1, p2):
     check.check_string(p1)
     check.check_string(p2)
 
+    n1 = clazz.normalize(p1)
+    n2 = clazz.normalize(p2)
+    
+    clazz._log.log_d(f'shell_path.diff: p1="{p1}" p2="{p2}" n1="{n1}" n2="{n2}"')
+
+    if n1 in n2:
+      left, delimiter, right = n2.partition(n1)
+      nleft = clazz.normalize(left)
+      nright = clazz.normalize(right)
+      assert delimiter == n1
+      clazz._log.log_d(f'shell_path.diff1: nleft="{nleft}" nright="{nright}"')
+      appended = clazz.split(nright)
+      prepended = clazz.split(nleft)
+      removed = []
+      clazz._log.log_d(f'shell_path.diff1: appended={appended} prepended={prepended} removed={removed}')
+      return clazz._diff_result(appended, prepended, removed)
+    
     up1 = clazz.remove_duplicates(p1)
     up2 = clazz.remove_duplicates(p2)
 
-    clazz._log.log_d(f'shell_path.diff: up1={up1} up2={up2}')
+    clazz._log.log_d(f'shell_path.diff2: up1={up1} up2={up2}')
     
     parts1 = clazz.split(up1)
     parts2 = clazz.split(up2)
 
     for i, p in enumerate(parts1):
-      clazz._log.log_d(f'shell_path.diff: parts1: {i}: {p}')
+      clazz._log.log_d(f'shell_path.diff2: parts1: {i}: {p}')
 
     for i, p in enumerate(parts2):
-      clazz._log.log_d(f'shell_path.diff: parts2: {i}: {p}')
+      clazz._log.log_d(f'shell_path.diff2: parts2: {i}: {p}')
 
     set1 = set(parts1)
     set2 = set(parts2)
-    added = [ part for part in parts2 if not part in set1 ]
+    appended = [ part for part in parts2 if not part in set1 ]
+    prepended = []
     removed = [ part for part in parts1 if not part in set2 ]
-    clazz._log.log_d(f'shell_path.diff: added={added} removed={removed}')
-    return clazz._diff_result(added, removed)
+    clazz._log.log_d(f'shell_path.diff2: appended={appended} prepended={prepended} removed={removed}')
+    return clazz._diff_result(appended, prepended, removed)
+
+  @classmethod
+  def check_parts(clazz, parts):
+    if not check.is_seq(parts):
+      raise TypeError(f'parts should be a string sequence instead of {type(parts).__name__}: {parts}')
+      
+    for i, part in enumerate(parts, start = 1):
+      if not check.is_string(part):
+        raise TypeError(f'part {i} type should be string instead of {type(part).__name__}: {part}')
--- tests/lib/bes/system/test_shell_path.py
+++ tests/lib/bes/system/test_shell_path.py
@@ -8,6 +8,7 @@ class test_shell_path(unit_test):
 
   def test_split(self):
     self.assertEqual( [], shell_path.split(None) )
+    self.assertEqual( [], shell_path.split('') )
     self.assertEqual( [ 'a', 'b' ], shell_path.split(self.native_path('a:b')) )
     self.assertEqual( [ 'a' ], shell_path.split(self.native_path('a')) )
     self.assertEqual( [ 'a', 'b', 'a' ], shell_path.split(self.native_path('a:b:a')) )
@@ -23,12 +24,81 @@ class test_shell_path(unit_test):
     self.assertEqual( self.native_path('a'), shell_path.remove_duplicates('a') )
     self.assertEqual( self.native_path('a:b'), shell_path.remove_duplicates(self.native_path('a:b:a')) )
 
+  def test_normalize(self):
+    self.assertEqual( self.native_path(''), shell_path.normalize('') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('/usr/bin/') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('/usr/bin//') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('/usr//bin') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('//usr/bin') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('///usr/bin') )
+    self.assertEqual( self.native_path('/usr/bin'), shell_path.normalize('///usr///bin///') )
+    self.assertEqual( self.native_path('/usr/bin:/bin'), shell_path.normalize('/usr/bin::/bin') )
+    self.assertEqual( self.native_path('/usr/bin:/bin'), shell_path.normalize('://usr/bin/::/bin//:') )
+    self.assertEqual( self.native_path(''), shell_path.normalize(':') )
+    #self.assertEqual( self.native_path('a:b'), shell_path.normalize('a:b') )
+    #self.assertEqual( self.native_path('a'), shell_path.normalize('a') )
+    #self.assertEqual( self.native_path('a:b'), shell_path.normalize(self.native_path('a:b:a')) )
+    
   def test_diff(self):
-    self.assertEqual( ( [], [] ), self._call_diff('a:b', 'a:b') )
-    self.assertEqual( ( [], [] ), self._call_diff('a:b', 'b:a') )
-    self.assertEqual( ( [], [ 'b' ] ), self._call_diff('a:b', 'a') )
-    self.assertEqual( ( [ 'c', 'd' ], [] ), self._call_diff('a:b', 'b:c:d:a') )
+    self.assertEqual( ( [], [], [] ), self._call_diff('/usr/bin:/bin', '/usr/bin:/bin') )
+    self.assertEqual( ( [ '/kiwi/bin' ], [], [ '/bin' ] ), self._call_diff('/usr/bin:/bin', '/usr/bin:/kiwi/bin') )
+    
+  def xtest_diff_prepend(self):
+    self.assertEqual( ( [], [ '/kiwi/bin' ], [ '/bin' ] ), self._call_diff('/usr/bin:/bin', '/kiwi/bin:/usr/bin') )
 
+  def test_resolve(self):
+    self.assertEqual( self.native_path(''), shell_path.resolve('') )
+    self.assertEqual( self.native_path('/usr/bin:/bin'), shell_path.resolve('/usr/bin:/bin') )
+    self.assertEqual( self.native_path('/usr/bin:/bin'), shell_path.resolve([ '/usr/bin', '/bin' ]) )
+    self.assertEqual( self.native_path('/usr/bin:/bin'), shell_path.resolve(( '/usr/bin', '/bin' )) )
+
+    with self.assertRaises(TypeError) as _:
+      shell_path.resolve(( '/usr/bin', [ '/bin' ] ))
+
+  def _call_remove(self, p1, p2):
+    return shell_path.remove(self.native_path(p1), self.native_path(p2))
+      
+  def test_remove(self):
+    self.assertEqual( '', self._call_remove('', 'foo') )
+    self.assertEqual( '/usr/bin', self._call_remove('/usr/bin:/bin', '/bin') )
+    self.assertEqual( '', self._call_remove('/usr/bin:/bin', '/usr/bin:/bin') )
+    self.assertEqual( '', self._call_remove('/usr/bin:/bin', '/bin:/usr/bin') )
+    self.assertEqual( '', self._call_remove('/usr/bin:/bin', '/bin:/usr/bin:') )
+    self.assertEqual( '', self._call_remove('/usr/bin:/bin', '/bin:/usr/bin:/bin') )
+    self.assertEqual( '/usr/bin:/usr/bin', self._call_remove('/usr/bin:/usr/bin', '/bin') )
+    self.assertEqual( '', self._call_remove('/usr/bin:/bin:/usr/bin', '/bin:/usr/bin') )
+
+  def _call_append(self, p1, p2):
+    return shell_path.append(self.native_path(p1), self.native_path(p2))
+      
+  def test_append(self):
+    self.assertEqual( '/bin', self._call_append('', '/bin') )
+    self.assertEqual( '/bin:/usr/bin', self._call_append('/bin', '/usr/bin') )
+    self.assertEqual( '/bin:/usr/bin', self._call_append('/bin:/usr/bin', '/usr/bin') )
+    self.assertEqual( '/bin:/usr/bin', self._call_append('/usr/bin:/bin', '/usr/bin') )
+
+  def _call_prepend(self, p1, p2):
+    return shell_path.prepend(self.native_path(p1), self.native_path(p2))
+      
+  def test_prepend(self):
+    self.assertEqual( '/bin', self._call_prepend('', '/bin') )
+    self.assertEqual( '/usr/bin:/bin', self._call_prepend('/bin', '/usr/bin') )
+    self.assertEqual( '/usr/bin:/bin', self._call_prepend('/bin:/usr/bin', '/usr/bin') )
+    self.assertEqual( '/usr/bin:/bin', self._call_prepend('/usr/bin:/bin', '/usr/bin') )
+    
+  def xtest_diff_p1_in_p2(self):
+    self.assertEqual( (
+      [ '/lemon/bin' ],
+      [ '/kiwi/bin' ],
+      [],
+    ), self._call_diff('/bin:/usr/bin', '/kiwi/bin:/bin:/usr/bin:/lemon/bin') )
+
+    self.assertEqual( (
+      [ '/lemon/bin' ],
+      [ '/kiwi/bin' ],
+      [],
+    ), self._call_diff('/bin:/usr/bin:/bin', '/kiwi/bin:/bin:/usr/bin:/bin:/lemon/bin') )
+    
   def _call_diff(self, p1, p2):
     return shell_path.diff(self.native_path(p1), self.native_path(p2))
     
