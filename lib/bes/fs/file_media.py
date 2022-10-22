3c7005dd41e0319d9558254c91b991fefb06eb33
Ramiro Estrugo <git@fateware.com>
2022-10-21 17:15:49 -0700

    add

---
+++ lib/bes/fs/file_media.py
@@ -0,0 +1,27 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from ..system.check import check
+
+class file_media(object):
+
+  EXTENSIONS = frozenset([
+    'asf',
+    'avi',
+    'divx',
+    'flv',
+    'gif',
+    'jfif',
+    'jpeg',
+    'jpg',
+    'm4v',
+    'mkv',
+    'mov',
+    'mp4',
+    'mpeg',
+    'mpg',
+    'png',
+    'ts',
+    'webm',
+    'webp',
+    'wmv',
+  ])
