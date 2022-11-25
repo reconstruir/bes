81d8ad69fc77b4a727693346d4b377b3b7f3ba0b
Ramiro Estrugo <git@fateware.com>
2022-11-25 06:33:13 -0800

    add mime type to extension map for some types

--- lib/bes/fs/file_media.py
+++ lib/bes/fs/file_media.py
@@ -17,7 +17,12 @@ class file_media(object):
     'mkv',
     'mov',
     'mp4',
+    'mp4v',
+    'mpg4',
     'mpeg',
+    'mpe',
+    'm1v',
+    'm2v',
     'mpg',
     'png',
     'ts',
@@ -25,3 +30,26 @@ class file_media(object):
     'webp',
     'wmv',
   ])
+
+  MIME_TYPE_TO_EXT_MAP = {
+    'image/gif': 'gif',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/webp': 'webp',
+    'video/divx': 'divx',
+    'video/mp2t': 'ts',
+    'video/mp4': 'mp4',
+    'video/mpeg': 'mpeg',
+    'video/quicktime': 'mov',
+    'video/webm': 'webm',
+    'video/x-flv': 'flv',
+    'video/x-m4v': 'm4v',
+    'video/x-matroska': 'mkv',
+    'video/x-ms-asf': 'asf',
+    'video/x-ms-wmv': 'wmv',
+    'video/x-msvideo': 'avi',
+  }
+  def mime_type_to_extension(clazz, mime_type):
+    check.check_string(mime_types)    
+
+    return clazz.MIME_TYPE_TO_EXT_MAP.get(mime_type, None)
