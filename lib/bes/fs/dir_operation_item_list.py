00e5da2bf3c6639483b84d0ba343071455b280ca
Ramiro Estrugo <git@fateware.com>
2022-12-11 12:47:52 -0800

    remove silly assertion

--- lib/bes/fs/dir_operation_item_list.py
+++ lib/bes/fs/dir_operation_item_list.py
@@ -29,8 +29,9 @@ class dir_operation_item_list(type_checked_list):
     for i, item in enumerate(resolved_items, start = 1):
       need_move = False
       if path.exists(item.dst_filename):
-        assert file_util.files_are_the_same(item.src_filename,
-                                            item.dst_filename)
+        #assert file_util.files_are_the_same(item.src_filename,
+        #                                    item.dst_filename)
+        print(f'checking {item.src_filename} vs {item.dst_filename}')
         need_move = file_util.files_are_the_same(item.src_filename,
                                                  item.dst_filename)
       else:
