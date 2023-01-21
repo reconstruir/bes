a3e57d88aac68b95b65916ec1b8f372349b0be02
Ramiro Estrugo <git@fateware.com>
2023-01-20 17:08:28 -0800

    add hash_string_zfilled

--- lib/bes/common/hash_util.py
+++ lib/bes/common/hash_util.py
@@ -12,14 +12,26 @@ class hash_util(object):
   def hash_string_sha256(clazz, s, encoding = 'utf-8'):
     'Return true if the given object can be encoded as json.'
     check.check_string(s)
+    check.check_string(s, encoding)
     
     return hashlib.sha256(s.encode(encoding)).hexdigest()
 
   @classmethod
   def hash_string_unsigned(clazz, s, num_digits = None):
     check.check_string(s)
+    check.check_int(num_digits, allow_none = None)
 
     h = int(hashlib.sha1(s.encode('utf-8')).hexdigest(), 16)
     if num_digits == None:
       return h
     return h % (10 ** num_digits)
+
+  @classmethod
+  def hash_string_zfilled(clazz, s, num_digits = None):
+    check.check_string(s)
+    check.check_int(num_digits, allow_none = None)
+
+    hs = str(clazz.hash_string_unsigned(s, num_digits = num_digits))
+    if num_digits == None:
+      return hs
+    return hs.zfill(num_digits)
