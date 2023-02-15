c68a6f183ef1c12c8a3912295a556675c98a3ded
Ramiro Estrugo <git@fateware.com>
2023-02-15 11:07:14 -0800

    add bfile_metadata_with_factories that simplifies the process of using all factories.

---
+++ lib/bes/files/metadata_factories/bfile_metadata_with_factories.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.system.check import check
+from bes.system.log import logger
+
+from ..metadata.bfile_metadata import bfile_metadata
+from ..metadata.bfile_metadata_factory_registry import bfile_metadata_factory_registry
+
+from .bfile_metadata_factory_checksum import bfile_metadata_factory_checksum
+
+class bfile_metadata_with_factories(bfile_metadata):
+
+  bfile_metadata_factory_registry.register_factory(bfile_metadata_factory_checksum)
+
+  @classmethod
+  def clear_all_factories(clazz):
+    bfile_metadata_factory_registry.clear_all()
