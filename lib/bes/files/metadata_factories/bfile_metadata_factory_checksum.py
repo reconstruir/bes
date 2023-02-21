22c21cf2fd3eafb55d917477c07cdddf83c21d97
Ramiro Estrugo <git@fateware.com>
2023-02-20 22:26:59 -0800

    get rid of the hacks

--- lib/bes/files/metadata_factories/bfile_metadata_factory_checksum.py
+++ lib/bes/files/metadata_factories/bfile_metadata_factory_checksum.py
@@ -12,14 +12,8 @@ class bfile_metadata_factory_checksum(bfile_metadata_factory_base):
   @classmethod
   #@abstractmethod
   def handlers(clazz):
-    def fuck(f):
-      if clazz.metadata_class.has_key(f, 'bes_checksum_sha256'):
-        return clazz.metadata_class.get_string(f, 'bes_checksum_sha256')
-      return bfile_checksum.checksum(f, 'sha256')
-    
     return [
       ( 'bes/checksum/md5/0.0', lambda f: bfile_checksum.checksum(f, 'md5'), clazz.decode_string, clazz.encode_string, check.check_string ),
       ( 'bes/checksum/sha1/0.0', lambda f: bfile_checksum.checksum(f, 'sha1'), clazz.decode_string, clazz.encode_string, check.check_string ),
-      #( 'bes/checksum/sha256/0.0', lambda f: bfile_checksum.checksum(f, 'sha256'), clazz.decode_string, clazz.encode_string, check.check_string ),
-      ( 'bes/checksum/sha256/0.0', fuck, clazz.decode_string, clazz.encode_string, check.check_string ),
+      ( 'bes/checksum/sha256/0.0', lambda f: bfile_checksum.checksum(f, 'sha256'), clazz.decode_string, clazz.encode_string, check.check_string ),
     ]
