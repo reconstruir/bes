cb0203b66fcaaff17b8787ba9428a58538bcc786
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-05-20 07:18:32 -0700

    wip

--- lib/bes/btask/btask_result_collector_py.py
+++ lib/bes/btask/btask_result_collector_py.py
@@ -4,7 +4,6 @@ from bes.system.log import logger
 from bes.system.check import check
 
 from .btask_result_collector_i import btask_result_collector_i
-from .btask_main_thread_runner_py import btask_main_thread_runner_py
 from .btask_result import btask_result
 from .btask_pool import btask_pool
 
@@ -25,24 +24,24 @@ class btask_result_collector_py(btask_result_collector_i):
   def handle_result(self, result):
     check.check_btask_result(result)
 
-    self._log.log_d(f'btask_main_thread_runner_py.handle_result: task_id={result.task_id}')
+    self._log.log_d(f'btask_result_collector_py.handle_result: task_id={result.task_id}')
     self._runner.call_in_main_thread(self._handle_result_in_main_thread, result)
 
   #@abstractmethod
   def handle_progress(self, progress):
     check.check_btask_progress(progress)
 
-    self._log.log_d(f'btask_main_thread_runner_py.handle_progress: task_id={progress.task_id}')
+    self._log.log_d(f'btask_result_collector_py.handle_progress: task_id={progress.task_id}')
     self._runner.call_in_main_thread(self._handle_progress_in_main_thread, progress)
     
   def _handle_result_in_main_thread(self, result):
     check.check_btask_result(result)
-    self._log.log_d(f'btask_main_thread_runner_py._handle_result_in_main_thread: task_id={result.task_id}')
+    self._log.log_d(f'btask_result_collector_py._handle_result_in_main_thread: task_id={result.task_id}')
     callback = self._pool.complete(result.task_id)
     callback(result)
 
   def _handle_progress_in_main_thread(self, progress):
     check.check_btask_progress(progress)
-    self._log.log_d(f'btask_main_thread_runner_py._handle_progress_in_main_thread: task_id={progress.task_id}')
+    self._log.log_d(f'btask_result_collector_py._handle_progress_in_main_thread: task_id={progress.task_id}')
     self._pool.report_progress(task_id, progress)
     
