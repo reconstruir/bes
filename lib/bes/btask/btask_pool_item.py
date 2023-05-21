a115a7635ba984712448da2cf462619c7638ff94
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-05-21 06:43:23 -0700

    cleanup

--- lib/bes/btask/btask_pool_item.py
+++ lib/bes/btask/btask_pool_item.py
@@ -1,24 +1,23 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from collections import namedtuple
-from datetime import datetime
-import multiprocessing
 
 from bes.system.log import logger
 from bes.system.check import check
 
-from .btask_error import btask_error
-from .btask_priority import btask_priority
-from .btask_result import btask_result
-from .btask_result_metadata import btask_result_metadata
-from .btask_threading import btask_threading
 from .btask_config import btask_config
 
-class btask_pool_item(namedtuple('btask_pool_item', 'task_id, add_time, task_args, config, function, callback, progress_callback, interrupted_value')):
+class btask_pool_item(namedtuple('btask_pool_item', 'task_id, add_time, task_args, config, function, callback, progress_callback, interrupted')):
   
-  def __new__(clazz, task_id, add_time, task_args, config, function, callback, progress_callback, interrupted_value):
+  def __new__(clazz, task_id, add_time, task_args, config, function, callback, progress_callback, interrupted):
     check.check_int(task_id)
-
-    return clazz.__bases__[0].__new__(clazz, task_id, add_time, task_args, config, function, callback, progress_callback, interrupted_value)
+    check.check_datetime(add_time)
+    check.check_tuple(task_args)
+    check.check_btask_config(config)
+    check.check_callable(function)
+    check.check_callable(callback, allow_none = True)
+    check.check_callable(progress_callback, allow_none = True)
+    
+    return clazz.__bases__[0].__new__(clazz, task_id, add_time, task_args, config, function, callback, progress_callback, interrupted)
   
 check.register_class(btask_pool_item, include_seq = False)
