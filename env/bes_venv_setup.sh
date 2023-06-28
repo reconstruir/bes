6a3d67a403342586d86bd4a63f20aa851a2c373d
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-06-28 13:10:25 -0700

    figure out the version from the default exe

--- env/bes_venv_setup.sh
+++ env/bes_venv_setup.sh
@@ -8,11 +8,12 @@ function main()
   local _this_dir="$(_bes_venv_setup_this_dir)"
   local _root_dir="$(bes_path_abs_dir ${_this_dir}/..)"
   local _best="${_root_dir}/bin/best.py"
-  local _python="$(bes_python_find_default)"
+  local _python_exe="$(bes_python_find_default)"
+  local _python_version=$(bes_python_exe_version "${_python_exe}")
   
   ${_python} ${_best} pip_project install_requirements \
              --root-dir "${_root_dir}/VE/bes" \
-             --python-version 3.9 \
+             --python-version ${_python_version} \
              $@
   return 0
 }
