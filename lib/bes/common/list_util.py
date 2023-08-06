77b10c17438a74dc58770905ad382b67ed709b92
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-08-06 11:53:17 -0700

    fix misc spacing in comment

--- lib/bes/common/list_util.py
+++ lib/bes/common/list_util.py
@@ -7,7 +7,7 @@ class list_util(object):
   def reversed_enumerate(clazz, l):
     '''
     Generator that yields a list in reversed order
-      from: https://stackoverflow.com/questions/529424/traverse-a-list-in-reverse-order-in-python
+    from: https://stackoverflow.com/questions/529424/traverse-a-list-in-reverse-order-in-python
     '''
     for i in reversed(range(len(l))):
       yield i, l[i]
