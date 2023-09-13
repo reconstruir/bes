d82f80e3b3ac7efccbf3a1014e301545085c0b3a
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-09-13 08:50:41 -0700

    allow the callback to be None

--- lib/bes/btask/btask_task.py
+++ lib/bes/btask/btask_task.py
@@ -16,7 +16,7 @@ class btask_task(namedtuple('btask_task', 'task_id, add_time, config, function, 
     config = check.check_btask_config(config)
     check.check_callable(function)
     check.check_dict(args, allow_none = True)
-    check.check_callable(callback)
+    check.check_callable(callback, allow_none = True)
     check.check_callable(progress_callback, allow_none = True)
     
     return clazz.__bases__[0].__new__(clazz,
