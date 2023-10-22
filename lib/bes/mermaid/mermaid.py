ad3e1d5c53ed10fc6bd33819099cc644bfbcc79a
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-10-22 12:05:44 -0700

    skeleton of mmd file parser

---
+++ lib/bes/mermaid/mermaid.py
@@ -0,0 +1,78 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from collections import namedtuple
+
+from ..system.check import check
+from ..text.lexer_token import lexer_token
+from ..text.text_line_parser import text_line_parser
+from ..text.white_space import white_space
+from ..common.point import point
+
+class mermaid(object):
+
+  @classmethod
+  def parse_state_diagram_text(clazz, text, indent = 2):
+    check.check_string(text)
+    check.check_int(indent)
+
+    lines = text_line_parser.parse_lines(text,
+                                         strip_comments = False,
+                                         strip_text = False,
+                                         remove_empties = False)
+
+    for line_number, line in enumerate(lines, start = 1):
+      token = clazz._parse_state_diagram_line(line, line_number, indent)
+      print(token)
+    return []
+
+  @classmethod
+  def _parse_state_diagram_line(clazz, line, line_number, indent):
+    count = white_space.count_leading_spaces(line)
+    sline = line.strip()
+    #print(f'line="{line}" sline="{sline}" count={count}')
+
+    if len(sline) == 0:
+      return lexer_token(token_type = 'empty_line',
+                         value = line,
+                         position = point(count, line_number))
+
+    elif sline.startswith('%%'):
+      return lexer_token(token_type = 'comment',
+                         value = line,
+                         position = point(count, line_number))
+    elif count == 0:
+      if sline == 'stateDiagram-v2':
+        return lexer_token(token_type = 'diagram_type',
+                           value = 'stateDiagram-v2',
+                           position = point(0, line_number))
+    elif (count % indent) == 0:
+      return clazz._parse_state_diagram_directive(count, line, sline, line_number)
+    else:
+      raise RuntimeError(f'Invalid indent: "{line}"')
+
+  @classmethod
+  def _parse_state_diagram_directive(clazz, count, line, sline, line_number):
+    if '-->' in line:
+      transition = clazz._parse_state_diagram_transition(line)
+      return lexer_token(token_type = 'transition',
+                         value = transition,
+                         position = point(count, line_number))
+    else:
+      return lexer_token(token_type = 'unknown',
+                         value = line,
+                         position = point(count, line_number))
+    
+  _transition = namedtuple('_transition', 'line, from_state, to_state')
+  @classmethod
+  def _parse_state_diagram_transition(clazz, line):
+    left, delimiter, right = line.partition('-->')
+    assert delimiter == '-->'
+    from_state = left.strip()
+    to_state = clazz._parse_state_diagram_transition_to_state(right)
+    return clazz._transition(line, from_state, to_state)
+
+  @classmethod
+  def _parse_state_diagram_transition_to_state(clazz, line):
+    left, delimiter, right = line.partition(':')
+    assert delimiter in ( ':', '' )
+    return left.strip()
---
+++ tests/lib/bes/mermaid/test_mermaid.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+from bes.mermaid.mermaid import mermaid
+
+class test_mermaid(unit_test):
+
+  def test_parse_state_diagram_text(self):
+    text = '''
+stateDiagram-v2
+  direction LR
+  
+  %% start state
+  [*] --> start
+  start --> [*]: eos
+  start --> comment: [#0059;]
+  start --> space: [\t ␠]
+  start --> cr: [␍]
+  start --> start: [␤]
+  start --> section_name: [
+  start --> key: [_ a-z A-Z 0-9]
+  start --> expecting_value: [=]
+  start --> start_error: [.]
+  note right of start_error
+    Unexpected "."
+  end note
+  
+  %% space state
+  space --> space: [\t ␠]
+  space --> [*]: eos
+  space --> cr: [␍]
+  space --> start: [␤]
+  space --> key: [_ a-z A-Z 0-9]
+  space --> expecting_value: [=]
+  
+  
+  %% cr state
+  cr --> start: [␤] 
+  cr --> cr_error: [.] eos
+  note right of cr_error
+    Expecting "␤" instead of "."
+  end note
+
+  %% comment state
+  comment --> comment: [.]
+  comment --> cr: [␍]
+  comment --> start: [␤]
+  comment --> [*]: eos
+  
+  %% section_name state
+  section_name --> section_name: [_ a-z A-Z 0-9 \.]
+  section_name --> start: ]
+  section_name --> section_name_error: [\t ␠ ␍ ␤] eos
+  note left of section_name_error
+    Unexpected char in section name
+  end note
+  
+  %% key state
+  key --> key: [_ a-z A-Z 0-9 \.]
+  key --> space: [\t ␠]
+  key --> cr: [␍]
+  key --> start: [␤]
+  key --> expecting_value: [=]
+  key --> [*]: eos
+  
+  %% expecting_value state
+  expecting_value --> value_space: [\t ␠]
+  expecting_value --> cr: [␍]
+  expecting_value --> start: [␤]
+  expecting_value --> [*]: eos
+  expecting_value --> value: [.]
+
+  %% value_space state
+  value_space --> value_space: [\t ␠]
+  value_space --> cr: [␍]
+  value_space --> start: [␤]
+  value_space --> [*]: eos
+  value_space --> value: [.]
+  
+  %% value state
+  value --> value: [.]
+%%  value --> space: [\t ␠]
+  value --> cr: [␍]
+  value --> start: [␤]
+  value --> [*]: eos
+    '''
+    self.assertEqual( [
+      'start',
+      'space',
+    ], mermaid.parse_state_diagram_text(text) )
+    
+if __name__ == '__main__':
+  unit_test.main()
