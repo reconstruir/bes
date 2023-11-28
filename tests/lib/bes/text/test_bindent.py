86b2a3e5bff015505f297d2ce9f1dd6f42e0cd4e
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-11-27 21:34:31 -0800

    add another test

--- tests/lib/bes/text/test_bindent.py
+++ tests/lib/bes/text/test_bindent.py
@@ -6,19 +6,33 @@ from bes.text.bindent import bindent
 
 class test_bindent(unit_test):
 
-  def test_indent(self):
-    text = '''
+  def test_indent_two(self):
+    text = '''\
 kiwi
 orange
 melon
 '''
 
-    expected = '''  
+    expected = '''\
   kiwi
   orange
   melon
 '''
     self.assertEqual( expected, bindent.indent(text, 2) )
+
+  def test_indent_four(self):
+    text = '''\
+kiwi
+orange
+melon
+'''
+
+    expected = '''\
+    kiwi
+    orange
+    melon
+'''
+    self.assertEqual( expected, bindent.indent(text, 4) )
     
 if __name__ == '__main__':
   unit_test.main()
