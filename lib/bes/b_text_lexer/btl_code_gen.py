35bb83b126bfdbe4de62ce8345078d626e2183f4
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-12-23 13:00:26 -0800

    remove dead code and cleanup imports

--- lib/bes/b_text_lexer/btl_code_gen.py
+++ lib/bes/b_text_lexer/btl_code_gen.py
@@ -1,19 +1,10 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from collections import namedtuple
-#import os
 import os.path as path
-#import io
-import pprint
-from ..common.point import point
-from ..fs.file_check import file_check
 from ..fs.file_util import file_util
 from ..system.check import check
 from ..system.log import logger
-from ..text.lexer_token import lexer_token
-from ..text.text_line_parser import text_line_parser
-from ..text.white_space import white_space
-from ..text.bindent import bindent
+#from ..text.lexer_token import lexer_token
 
 from .btl_code_gen_error import btl_code_gen_error
 from .btl_code_gen_buffer import btl_code_gen_buffer
@@ -21,14 +12,7 @@ from .btl_code_gen_buffer import btl_code_gen_buffer
 class btl_code_gen(object):
 
   _log = logger('btl')
-  @classmethod
-  def state_diagram_parse_file(clazz, filename, indent = 2):
-    filename = file_check.check_file(filename)
-    check.check_int(indent)
-
-    text = file_util.read(filename, codec = 'utf-8')
-    return clazz.state_diagram_parse_text(text, indent = indent)
-
+  
   _HEADER = '''\
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
@@ -38,16 +22,6 @@ from bes.text.text_lexer_char import text_lexer_char
 from bes.text.text_lexer_state_base import text_lexer_state_base
 '''
   
-  _LEXER_CLASS_TEMPLATE = '''\
-class {namespace}_{name}_lexer_base(text_lexer_base):
-
-  def __init__(self, {name}, source = None):
-    super().__init__(log_tag, source = source)
-
-    self.token = {namespace}_{name}_lexer_token(self)
-    self.char = text_lexer_char
-'''
-
   @classmethod
   def state_diagram_generate_code(clazz, filename, namespace, name, output_directory,
                                   indent = 2, skip_start_state = True, skip_end_state = True):
@@ -94,103 +68,6 @@ class {namespace}_{name}_lexer_base(text_lexer_base):
         stream.write(state_instance_code)
         stream.write(os.linesep)
     return output_filename
-
-  @classmethod
-  def state_diagram_parse_text(clazz, text, indent = 2):
-    check.check_string(text)
-    check.check_int(indent)
-
-    tokens = clazz._state_diagram_tokenize(text, indent)
-    states = set()
-    lexer_tokens = set()
-    transitions = mmd_transition_list()
-    for token in tokens:
-      if token.token_type == 'transition':
-        to_state = token.value.to_state
-        from_state = token.value.from_state
-#        if to_state == '[*]':
-#          to_state = '__end'
-#        if from_state == '[*]':
-#          from_state = '__start'
-        #print(f'{from_state} --> {to_state}')
-        states.add(to_state)
-        states.add(from_state)
-        transitions.append(token.value)
-      elif token.token_type == 'comment':
-        value = token.value.strip()
-        if value.startswith('%%LEXER_TOKEN'):
-          _, _, lexer_token_name = value.partition(' ')
-          lexer_token_name = lexer_token_name.strip()
-          lexer_tokens.add(lexer_token_name)
-    mmd_states = sorted(list(states))
-    mmd_tokens = sorted(list(lexer_tokens))
-    return mmd_document(mmd_states, mmd_tokens, transitions)
-
-  @classmethod
-  def _state_diagram_tokenize(clazz, text, indent):
-    check.check_string(text)
-    check.check_int(indent)
-
-    lines = text_line_parser.parse_lines(text,
-                                         strip_comments = False,
-                                         strip_text = False,
-                                         remove_empties = False)
-    tokens = []
-    for line_number, line in enumerate(lines, start = 1):
-      token = clazz._state_diagram_parse_line(line, line_number, indent)
-      tokens.append(token)  
-    return tokens
-  
-  @classmethod
-  def _state_diagram_parse_line(clazz, line, line_number, indent):
-    count = white_space.count_leading_spaces(line)
-    sline = line.strip()
-    #print(f'line="{line}" sline="{sline}" count={count}')
-
-    if len(sline) == 0:
-      return lexer_token(token_type = 'empty_line',
-                         value = line,
-                         position = point(count, line_number))
-
-    elif sline.startswith('%%'):
-      return lexer_token(token_type = 'comment',
-                         value = line,
-                         position = point(count, line_number))
-    elif count == 0:
-      if sline == 'stateDiagram-v2':
-        return lexer_token(token_type = 'diagram_type',
-                           value = 'stateDiagram-v2',
-                           position = point(0, line_number))
-    elif (count % indent) == 0:
-      return clazz._state_diagram_parse_directive(count, line, sline, line_number)
-    else:
-      raise RuntimeError(f'Invalid indent: "{line}"')
-
-  @classmethod
-  def _state_diagram_parse_directive(clazz, count, line, sline, line_number):
-    if '-->' in line:
-      transition = clazz._state_diagram_parse_transition(line)
-      return lexer_token(token_type = 'transition',
-                         value = transition,
-                         position = point(count, line_number))
-    else:
-      return lexer_token(token_type = 'unknown',
-                         value = line,
-                         position = point(count, line_number))
-    
-  @classmethod
-  def _state_diagram_parse_transition(clazz, line):
-    left, delimiter, right = line.partition('-->')
-    assert delimiter == '-->'
-    from_state = left.strip()
-    to_state, event = clazz._state_diagram_parse_transition_to_state(right)
-    return mmd_transition(line, from_state, to_state, event)
-
-  @classmethod
-  def _state_diagram_parse_transition_to_state(clazz, line):
-    left, delimiter, right = line.partition(':')
-    assert delimiter in ( ':' )
-    return left.strip(), right.strip()
 
   _STATE_CLASS_TEMPLATE = '''\
 class {namespace}_{name}_lexer_state_{state}(text_lexer_state_base):
