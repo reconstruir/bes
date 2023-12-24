73a17fb20a9030d6f36b4a02cd9e91ec86941750
Ramiro Estrugo <github_reconstruir@fateware.com>
2023-12-24 07:29:10 -0800

    s/fruit/_fruit/ for the test namespace

--- tests/lib/bes/b_text_lexer/test_btl_code_gen.py
+++ tests/lib/bes/b_text_lexer/test_btl_code_gen.py
@@ -57,7 +57,7 @@ class test_btl_code_gen(keyval_desc_mixin, unit_test):
     state = btl_desc_state('s_juice', [ transition ])
 
     self.assert_code_equal('''
-class fruit_kiwi_lexer_state_s_juice(btl_lexer_state_base):
+class _fruit_kiwi_lexer_state_s_juice(btl_lexer_state_base):
   def __init__(self, lexer):
     super().__init__(lexer)
 
@@ -73,7 +73,7 @@ class fruit_kiwi_lexer_state_s_juice(btl_lexer_state_base):
     
     self.lexer.change_state(new_state, c)
     return tokens
-''', self._call__make_state_class_code('fruit', 'kiwi', char_map, state) )
+''', self._call__make_state_class_code('_fruit', 'kiwi', char_map, state) )
     
   def test__make_transition_code(self):
     char_map = btl_desc_char_map()
@@ -123,7 +123,7 @@ raise btl_lexer_error('Unknown buffer command: "notthere"')
 
   def test__make_token_class_code(self):
     self.assert_code_equal('''
-class fruit_kiwi_lexer_token(object):
+class _fruit_kiwi_lexer_token(object):
 
   KIWI = 'kiwi'
   LEMON = 'lemon'
@@ -142,7 +142,7 @@ class fruit_kiwi_lexer_token(object):
   
   def make_melon(self, value, position):
     return lexer_token(self.MELON, value, self._lexer.position)
-''', self._call__make_token_class_code('fruit', 'kiwi', { 'kiwi', 'lemon', 'melon' }) )
+''', self._call__make_token_class_code('_fruit', 'kiwi', { 'kiwi', 'lemon', 'melon' }) )
 
   def test__make_lexer_class_code(self):
     char_map = btl_desc_char_map()
@@ -152,18 +152,18 @@ class fruit_kiwi_lexer_token(object):
     states = [ state ]
     
     self.assert_code_equal('''
-class fruit_kiwi_lexer_base(text_lexer_base):
+class _fruit_kiwi_lexer_base(text_lexer_base):
 
   def __init__(self, kiwi, source = None):
     super().__init__(log_tag, source = source)
 
-    self.token = fruit_kiwi_lexer_token(self)
+    self.token = _fruit_kiwi_lexer_token(self)
     self.char = text_lexer_char
     
     self._states = {
-      's_juice': fruit_kiwi_lexer_state_s_juice(self),
+      's_juice': _fruit_kiwi_lexer_state_s_juice(self),
     }
-''', self._call__make_lexer_class_code('fruit', 'kiwi', states) )
+''', self._call__make_lexer_class_code('_fruit', 'kiwi', states) )
 
   def test__make_state_machine_code(self):
     desc = btl_desc.parse_text(self._keyval_desc_text)
@@ -174,7 +174,7 @@ from bes.b_text_lexer.btl_lexer_base import btl_lexer_base
 from bes.b_text_lexer.btl_lexer_state_base import btl_lexer_state_base
 
 
-class fruit_kiwi_lexer_token(object):
+class _fruit_kiwi_lexer_token(object):
 
   T_DONE = 't_done'
   T_EXPECTING_KEY = 't_expecting_key'
@@ -207,7 +207,7 @@ class fruit_kiwi_lexer_token(object):
     return lexer_token(self.T_VALUE, value, self._lexer.position)
 
 
-class fruit_kiwi_lexer_state_s_expecting_key(btl_lexer_state_base):
+class _fruit_kiwi_lexer_state_s_expecting_key(btl_lexer_state_base):
   def __init__(self, lexer):
     super().__init__(lexer)
 
@@ -235,7 +235,7 @@ class fruit_kiwi_lexer_state_s_expecting_key(btl_lexer_state_base):
     self.lexer.change_state(new_state, c)
     return tokens
 
-class fruit_kiwi_lexer_state_s_key(btl_lexer_state_base):
+class _fruit_kiwi_lexer_state_s_key(btl_lexer_state_base):
   def __init__(self, lexer):
     super().__init__(lexer)
 
@@ -258,7 +258,7 @@ class fruit_kiwi_lexer_state_s_key(btl_lexer_state_base):
     self.lexer.change_state(new_state, c)
     return tokens
 
-class fruit_kiwi_lexer_state_s_value(btl_lexer_state_base):
+class _fruit_kiwi_lexer_state_s_value(btl_lexer_state_base):
   def __init__(self, lexer):
     super().__init__(lexer)
 
@@ -282,7 +282,7 @@ class fruit_kiwi_lexer_state_s_value(btl_lexer_state_base):
     self.lexer.change_state(new_state, c)
     return tokens
 
-class fruit_kiwi_lexer_state_s_done(btl_lexer_state_base):
+class _fruit_kiwi_lexer_state_s_done(btl_lexer_state_base):
   def __init__(self, lexer):
     super().__init__(lexer)
 
@@ -296,21 +296,21 @@ class fruit_kiwi_lexer_state_s_done(btl_lexer_state_base):
     self.lexer.change_state(new_state, c)
     return tokens
 
-class fruit_kiwi_lexer_base(text_lexer_base):
+class _fruit_kiwi_lexer_base(text_lexer_base):
 
   def __init__(self, kiwi, source = None):
     super().__init__(log_tag, source = source)
 
-    self.token = fruit_kiwi_lexer_token(self)
+    self.token = _fruit_kiwi_lexer_token(self)
     self.char = text_lexer_char
     
     self._states = {
-      's_expecting_key': fruit_kiwi_lexer_state_s_expecting_key(self),
-      's_key': fruit_kiwi_lexer_state_s_key(self),
-      's_value': fruit_kiwi_lexer_state_s_value(self),
-      's_done': fruit_kiwi_lexer_state_s_done(self),
+      's_expecting_key': _fruit_kiwi_lexer_state_s_expecting_key(self),
+      's_key': _fruit_kiwi_lexer_state_s_key(self),
+      's_value': _fruit_kiwi_lexer_state_s_value(self),
+      's_done': _fruit_kiwi_lexer_state_s_done(self),
     }
-''', self._call__make_state_machine_code('fruit', 'kiwi', desc) )
+''', self._call__make_state_machine_code('_fruit', 'kiwi', desc) )
     
 if __name__ == '__main__':
   unit_test.main()
