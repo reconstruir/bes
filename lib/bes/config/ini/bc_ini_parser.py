7b09a45d7678aae3398e38a52b30fa27d8c0578f
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-02-11 11:31:52 -0800

    add skeleton of ini parser description

--- lib/bes/config/ini/bc_ini_parser.py
+++ lib/bes/config/ini/bc_ini_parser.py
@@ -1,195 +1,216 @@
+
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-# FIXME: add an exception class that handles all the state info attributes
-from bes.compat.StringIO import StringIO
-from bes.system.log import log
-from bes.text.line_numbers import line_numbers
-from bes.text.string_lexer import string_lexer_options
-from bes.text.sentence_lexer import sentence_lexer as lexer
-from bes.key_value.key_value import key_value
+from bes.system.check import check
+from bes.btl.btl_parser_base import btl_parser_base
+from bes.btl.btl_parser_runtime_error import btl_parser_runtime_error
+from bes.btl.btl_parser_state_base import btl_parser_state_base
 
-class bc_ini_parser(object):
+class bc_ini_parser(btl_parser_base):
 
-  def __init__(self, options, delimiter, empty_value, log_tag):
-    log.add_logging(self, tag = log_tag or 'bc_ini_parser')
-    self._options = options
-    self.delimiter = delimiter
-    self.empty_value = empty_value
-    self._buffer = None
-    
-    self.STATE_EXPECTING_KEY = _state_expecting_key(self)
-    self.STATE_EXPECTING_DELIMITER = _state_expecting_delimiter(self)
-    self.STATE_VALUE = _state_value(self)
-    self.STATE_DONE = _state_done(self)
-    self.state = self.STATE_EXPECTING_KEY
-    self.key = None
+  class e_unexpected_token(btl_parser_runtime_error):
+    def __init__(self, message = None):
+      super().__init__(message = message)
 
-  def _run(self, text):
-    self.log_d('_run() text=\"%s\" options=%s)' % (text, str(string_lexer_options(self._options))))
-    self.text = text
-
-    for token in lexer.tokenize(text, 'bc_ini_lexer', options = self._options):
-      key_value = self.state.handle_token(token)
-      if key_value:
-        self.log_i('parse: new key_value: %s' % (str(key_value)))
-        yield key_value
-    assert self.state == self.STATE_DONE
+  
+  class _state_s_start(btl_parser_state_base):
+    def __init__(self, parser, log_tag):
+      name = 's_start'
+      super().__init__(parser, name, log_tag)
+  
+    def handle_token(self, token, first_time):
+      token = check.check_btl_lexer_token(token)
+      check.check_bool(first_time)
+  
+      self.log_handle_token(token)
+      new_state_name = None
       
-  def _change_state(self, new_state, msg):
-    assert new_state
-    if new_state != self.state:
-      self.log_d('transition: %20s -> %-20s; %s'  % (self.state.__class__.__name__, new_state.__class__.__name__, msg))
-      self.state = new_state
+      if first_time:
+        self.node_creator.create_root()
 
-  def _token_is_delimiter(self, token):
-    assert token.token_type == lexer.TOKEN_PUNCTUATION
-    return token.value == self.delimiter
-
-  def _buffer_reset(self, text = None):
-    self._buffer = StringIO()
-    if text:
-      self._buffer_write(text)
-  
-  def _buffer_write(self, text):
-    assert text is not None
-    assert self._buffer
-    self._buffer.write(text)
-
-  def _buffer_value(self):
-    if not self._buffer:
-      return self.empty_value
-    return self._buffer.getvalue()
-
-  @classmethod
-  def parse(clazz, text, options = 0, delimiter = '=', empty_value = None, log_tag = None):
-    return bc_ini_parser(options, delimiter, empty_value, log_tag)._run(text)
-
-  @classmethod
-  def parse_to_dict(clazz, text, options = 0, delimiter = '=', empty_value = None, log_tag = None):
-    result = {}
-    for kv in clazz.parse(text, options = options, delimiter = delimiter,
-                          empty_value = empty_value, log_tag = log_tag):
-      result[kv.key] = kv.value
-    return result
-
-  @classmethod
-  def parse_to_list(clazz, text, options = 0, delimiter = '=', empty_value = None, log_tag = None):
-    result = []
-    for kv in clazz.parse(text, options = options, delimiter = delimiter,
-                          empty_value = empty_value, log_tag = log_tag):
-      result.append(kv)
-    return result
-
-class _state(object):
-
-  def __init__(self, parser):
-    self.name = self.__class__.__name__[1:]
-    log.add_logging(self, tag = '%s.%s' % (parser.__class__.__name__, self.name))
-    self.parser = parser
-  
-  def handle_token(self, token):
-    raise RuntimeError('unhandled handle_token(%c) in state %s' % (self.name))
-
-  def change_state(self, new_state, token):
-    self.parser._change_state(new_state, 'token="%s:%s"'  % (token.token_type, token.value))
-
-  def unexpected_token(self, token, expected_label):
-    text_blurb = line_numbers.add_line_numbers(self.parser.text)
-    raise RuntimeError('unexpected token \"%s:%s\" instead of \"%s\" at position %d:%d:\n%s' % (token.token_type,
-                                                                                                token.value,
-                                                                                                expected_label,
-                                                                                                token.position.x,
-                                                                                                token.position.y,
-                                                                                                text_blurb))
-  
-class _state_expecting_key(_state):
-  def __init__(self, parser):
-    super().__init__(parser)
-
-  def handle_token(self, token):
-    self.log_d('handle_token(%s)' % (str(token)))
-    new_state = None
-    if token.token_type == lexer.TOKEN_COMMENT:
-      new_state = self.parser.STATE_DONE
-    elif token.token_type == lexer.TOKEN_SPACE:
-      new_state = self.parser.STATE_EXPECTING_KEY
-    elif token.token_type == lexer.TOKEN_PUNCTUATION:
-      self.unexpected_token(token, 'key')
-    elif token.token_type == lexer.TOKEN_DONE:
-      new_state = self.parser.STATE_DONE
-    elif token.token_type == lexer.TOKEN_STRING:
-      self.parser.key = token.value
-      new_state = self.parser.STATE_EXPECTING_DELIMITER
-    self.change_state(new_state, token)
-    return None
-    
-class _state_done(_state):
-  def __init__(self, parser):
-    super().__init__(parser)
-
-  def handle_token(self, token):
-    self.log_d('handle_token(%s)' % (str(token)))
-    if token.token_type != lexer.TOKEN_DONE:
-      self.unexpected_token(token, 'done')
-    self.change_state(self.parser.STATE_DONE, token)
-    return None
-  
-class _state_expecting_delimiter(_state):
-  def __init__(self, parser):
-    super().__init__(parser)
-
-  def handle_token(self, token):
-    self.log_d('handle_token(%s)' % (str(token)))
-    new_state = None
-    key_value_result = None
-    if token.token_type == lexer.TOKEN_COMMENT:
-      key_value_result = key_value(self.parser.key, self.parser.empty_value)
-      new_state = self.parser.STATE_DONE
-    elif token.token_type == lexer.TOKEN_SPACE:
-      self.unexpected_token(token, 'delimiter')
-    elif token.token_type == lexer.TOKEN_PUNCTUATION:
-      if not self.parser._token_is_delimiter(token):
-        self.unexpected_token(token, 'delimiter:%s' % (self.parser.delimiter))
-      new_state = self.parser.STATE_VALUE
-    elif token.token_type == lexer.TOKEN_DONE:
-      self.unexpected_token(token, 'delimiter:%s' % (self.parser.delimiter))
-    elif token.token_type == lexer.TOKEN_STRING:
-      self.unexpected_token(token, 'delimiter:%s' % (self.parser.delimiter))
-    self.change_state(new_state, token)
-    return key_value_result
-
-class _state_value(_state):
-  def __init__(self, parser):
-    super().__init__(parser)
-
-  def handle_token(self, token):
-    self.log_d('handle_token(%s)' % (str(token)))
-    new_state = None
-    key_value_result = None
-    if token.token_type == lexer.TOKEN_COMMENT:
-      key_value_result = key_value(self.parser.key, self.parser._buffer_value())
-      new_state = self.parser.STATE_DONE
-    elif token.token_type == lexer.TOKEN_SPACE:
-      key_value_result = key_value(self.parser.key, self.parser._buffer_value())
-      self.parser._buffer = None
-      new_state = self.parser.STATE_EXPECTING_KEY
-    elif token.token_type == lexer.TOKEN_PUNCTUATION:
-      if self.parser._token_is_delimiter(token):
-        self.unexpected_token(token, 'value')
+      if token.name == 't_done':
+        new_state_name = 's_done'
+      elif token.name == 't_line_break':
+        new_state_name = 's_start'
+      elif token.name == 't_space':
+        new_state_name = 's_start'
+      elif token.name == 't_key':
+        new_state_name = 's_expecting_delimiter'
+        self.node_creator.create('n_key_value')
+        self.node_creator.create('n_key')
+        self.node_creator.set_token('n_key', token)
+        self.node_creator.add_child('n_key_value', 'n_key')
+      elif token.name == 't_comment':
+        new_state_name = 's_start'
       else:
-        if not self.parser._buffer:
-          self.parser._buffer_reset()
-        self.parser._buffer_write(token.value)
-      new_state = self.parser.STATE_VALUE
-    elif token.token_type == lexer.TOKEN_DONE:
-      value = self.parser._buffer_value()
-      key_value_result = key_value(self.parser.key, self.parser._buffer_value())
-      new_state = self.parser.STATE_DONE
-    elif token.token_type == lexer.TOKEN_STRING:
-#      key_value_result = key_value(self.parser.key, token.value)
-      if not self.parser._buffer:
-        self.parser._buffer_reset()
-      self.parser._buffer_write(token.value)
-      new_state = self.parser.STATE_VALUE
-    self.change_state(new_state, token)
-    return key_value_result
+        new_state_name = 's_done'
+        state_name = self.name
+        msg = f'In state "{state_name}" unexpected token: "{token}"'
+        raise self.lexer.e_unexpected_token(message = msg)
+      
+      return new_state_name
+  
+  class _state_s_expecting_delimiter(btl_parser_state_base):
+    def __init__(self, parser, log_tag):
+      name = 's_expecting_delimiter'
+      super().__init__(parser, name, log_tag)
+  
+    def handle_token(self, token, first_time):
+      token = check.check_btl_lexer_token(token)
+      check.check_bool(first_time)
+  
+      self.log_handle_token(token)
+      new_state_name = None
+      if token.name == 't_key_value_delimiter':
+        new_state_name = 's_expecting_value'
+      elif token.name == 't_space':
+        new_state_name = 's_expecting_delimiter'
+      else:
+        new_state_name = 's_done'
+        state_name = self.name
+        msg = f'In state "{state_name}" unexpected token: "{token}"'
+        raise self.lexer.e_unexpected_token(message = msg)
+      
+      return new_state_name
+  
+  class _state_s_expecting_value(btl_parser_state_base):
+    def __init__(self, parser, log_tag):
+      name = 's_expecting_value'
+      super().__init__(parser, name, log_tag)
+  
+    def handle_token(self, token, first_time):
+      token = check.check_btl_lexer_token(token)
+      check.check_bool(first_time)
+  
+      self.log_handle_token(token)
+      new_state_name = None
+      if token.name == 't_value':
+        new_state_name = 's_after_value'
+        self.node_creator.create('n_value')
+        self.node_creator.set_token('n_value', token)
+        self.node_creator.add_child('n_key_value', 'n_value')
+        self.node_creator.add_child('n_root', 'n_key_value')
+      elif token.name == 't_space':
+        new_state_name = 's_expecting_value'
+      else:
+        new_state_name = 's_done'
+        state_name = self.name
+        msg = f'In state "{state_name}" unexpected token: "{token}"'
+        raise self.lexer.e_unexpected_token(message = msg)
+      
+      return new_state_name
+  
+  class _state_s_after_value(btl_parser_state_base):
+    def __init__(self, parser, log_tag):
+      name = 's_after_value'
+      super().__init__(parser, name, log_tag)
+  
+    def handle_token(self, token, first_time):
+      token = check.check_btl_lexer_token(token)
+      check.check_bool(first_time)
+  
+      self.log_handle_token(token)
+      new_state_name = None
+      if token.name == 't_done':
+        new_state_name = 's_done'
+      elif token.name == 't_space':
+        new_state_name = 's_after_value'
+      elif token.name == 't_comment':
+        new_state_name = 's_after_value'
+      elif token.name == 't_line_break':
+        new_state_name = 's_start'
+      else:
+        new_state_name = 's_done'
+        state_name = self.name
+        msg = f'In state "{state_name}" unexpected token: "{token}"'
+        raise self.lexer.e_unexpected_token(message = msg)
+      
+      return new_state_name
+  
+  class _state_s_done(btl_parser_state_base):
+    def __init__(self, parser, log_tag):
+      name = 's_done'
+      super().__init__(parser, name, log_tag)
+  
+    def handle_token(self, token, first_time):
+      token = check.check_btl_lexer_token(token)
+      check.check_bool(first_time)
+  
+      self.log_handle_token(token)
+      new_state_name = None
+      
+      return new_state_name
+
+  def __init__(self, lexer):
+    check.check_btl_lexer(lexer)
+    states = {
+      's_start': self._state_s_start(self, lexer.log_tag),
+      's_expecting_delimiter': self._state_s_expecting_delimiter(self, lexer.log_tag),
+      's_expecting_value': self._state_s_expecting_value(self, lexer.log_tag),
+      's_after_value': self._state_s_after_value(self, lexer.log_tag),
+      's_done': self._state_s_done(self, lexer.log_tag),
+    }
+    super().__init__(lexer, self._DESC_TEXT, states)
+  _DESC_TEXT = """
+#BTL
+#
+parser
+  name: bc_ini_parser
+  description: A ini style config file parser
+  version: 1.0
+  start_state: s_start
+  end_state: s_done
+
+errors
+  e_unexpected_token: In state "{state_name}" unexpected token: "{token}"
+
+states
+
+  s_start
+    transitions
+      t_done: s_done
+      t_line_break: s_start
+      t_space: s_start
+      t_key: s_expecting_delimiter
+        node create n_key_value
+        node create n_key
+        node set_token n_key
+        node add_child n_key_value n_key
+      t_comment: s_start
+      default: s_done
+        error e_unexpected_token
+    one_time_commands
+      node create_root n_root
+    commands
+
+  s_expecting_delimiter
+    transitions
+      t_key_value_delimiter: s_expecting_value
+      t_space: s_expecting_delimiter
+      default: s_done
+        error e_unexpected_token
+
+  s_expecting_value
+    transitions
+      t_value: s_after_value
+        node create n_value
+        node set_token n_value
+        node add_child n_key_value n_value
+        node add_child n_root n_key_value
+      t_space: s_expecting_value
+      default: s_done
+        error e_unexpected_token
+
+  s_after_value
+    transitions
+      t_done: s_done
+      t_space: s_after_value
+      t_comment: s_after_value
+      t_line_break: s_start
+      default: s_done
+        error e_unexpected_token
+
+  s_done
+
+"""
+check.register_class(bc_ini_parser, include_seq = False)
