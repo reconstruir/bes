6845a14202888c3b393021a9f9e7b8346de76628
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-02-12 11:05:30 -0800

    remove unused property

--- lib/bes/btl/btl_parser_context.py
+++ lib/bes/btl/btl_parser_context.py
@@ -10,16 +10,11 @@ class btl_parser_context(object):
   def __init__(self, parser):
     check.check_btl_parser(parser)
     self._node_creator = btl_parser_node_creator()
-    self._root_node = None
     self._state = parser.start_state
 
   @property
   def node_creator(self):
     return self._node_creator
-
-  @property
-  def root_node(self):
-    return self._root_node
 
   @property
   def state(self):
