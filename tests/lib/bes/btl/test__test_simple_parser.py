9c8b204058a3d46d5fb30db6c710f427e5d6c807
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-02-12 10:41:02 -0800

    add a parsing test of mutiple sessions that proves theres no side effects between sessions

--- tests/lib/bes/btl/test__test_simple_parser.py
+++ tests/lib/bes/btl/test__test_simple_parser.py
@@ -44,6 +44,48 @@ n_root;
     n_key;t_key:color:p=1,6:i=14
     n_value;t_value:green:p=7,6:i=16
 ''', str(result.root_node) )
+
+  def test_parse_multiple_times(self):
+    l = _test_simple_lexer()
+    p = _test_simple_parser(l)
+    text = '''
+fruit=apple
+color=red
+
+fruit=kiwi
+color=green
+'''
+    result = p.parse(text)
+    self.assert_python_code_text_equal( '''
+n_root;
+  n_key_value;
+    n_key;t_key:fruit:p=1,2:i=1
+    n_value;t_value:apple:p=7,2:i=3
+  n_key_value;
+    n_key;t_key:color:p=1,3:i=5
+    n_value;t_value:red:p=7,3:i=7
+  n_key_value;
+    n_key;t_key:fruit:p=1,5:i=10
+    n_value;t_value:kiwi:p=7,5:i=12
+  n_key_value;
+    n_key;t_key:color:p=1,6:i=14
+    n_value;t_value:green:p=7,6:i=16
+''', str(result.root_node) )
+
+    text = '''
+fruit=melon
+color=green
+'''
+    result = p.parse(text)
+    self.assert_python_code_text_equal( '''
+n_root;
+  n_key_value;
+    n_key;t_key:fruit:p=1,2:i=1
+    n_value;t_value:melon:p=7,2:i=3
+  n_key_value;
+    n_key;t_key:color:p=1,3:i=5
+    n_value;t_value:green:p=7,3:i=7
+''', str(result.root_node) )
     
 if __name__ == '__main__':
   unit_test.main()
