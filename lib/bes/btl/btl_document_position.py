f1b25a726190fcecd35026228364a3355f0fd5a0
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-02-13 06:25:26 -0800

    add more movement methods and unit tests

--- lib/bes/btl/btl_document_position.py
+++ lib/bes/btl/btl_document_position.py
@@ -6,22 +6,22 @@ from ..common.json_util import json_util
 from ..common.tuple_util import tuple_util
 from ..system.check import check
 
-class btl_document_position(namedtuple('btl_document_position', 'line_number, column')):
+class btl_document_position(namedtuple('btl_document_position', 'line, column')):
 
-  def __new__(clazz, line_number, column):
-    check.check_int(line_number)
+  def __new__(clazz, line, column):
+    check.check_int(line)
     check.check_int(column)
     
-    return clazz.__bases__[0].__new__(clazz, line_number, column)
+    return clazz.__bases__[0].__new__(clazz, line, column)
 
   def __str__(self):
-    return f'{self.line_number},{self.column}'
+    return f'{self.line},{self.column}'
 
   def to_dict(self):
     return dict(self._asdict())
   
-  def moved(self, delta_line_number, delta_column):
-    return btl_document_position(self.line_number + delta_line_number,
+  def moved(self, delta_line, delta_column):
+    return btl_document_position(self.line + delta_line,
                                  self.column + delta_column)
 
   def clone(self, mutations = None):
@@ -41,12 +41,27 @@ class btl_document_position(namedtuple('btl_document_position', 'line_number, co
     return tuple_util.cast_seq_to_namedtuple(clazz, obj)
 
   def advanced(self, c):
-    delta_line_number = 0
+    delta_line = 0
     delta_column = 0
     if c in ( '\n', '\r\n' ):
-      delta_line_number = 1
+      delta_line = 1
     else:
       delta_column = len(c)
-    return self.moved(delta_line_number, delta_column)
+    return self.moved(delta_line, delta_column)
+
+  def moved_horizontal(self, delta_column):
+    check.check_int(delta_column)
+
+    return self.moved(0, delta_column)
+
+  def moved_vertical(self, delta_line):
+    check.check_int(delta_line)
+
+    return self.moved(delta_line, 0)
+
+  def moved_to_line(self, line):
+    check.check_int(line)
+
+    return btl_document_position(line, self.column)
   
 check.register_class(btl_document_position, cast_func = btl_document_position._check_cast_func)
--- tests/lib/bes/btl/test_document_position.py
+++ tests/lib/bes/btl/test_document_position.py
@@ -15,7 +15,7 @@ class test_btl_document_position(unit_test):
     
   def test_to_dict(self):
     self.assertEqual( {
-      'line_number': 10,
+      'line': 10,
       'column': 1,
     }, btl_document_position(10, 1).to_dict() )
 
@@ -28,5 +28,16 @@ class test_btl_document_position(unit_test):
     self.assertEqual( ( 11, 1 ), btl_document_position(10, 1).advanced('\n') )
     self.assertEqual( ( 11, 1 ), btl_document_position(10, 1).advanced('\r\n') )
 
+  def test_moved_horizontal(self):
+    self.assertEqual( ( 10, 2 ), btl_document_position(10, 1).moved_horizontal(1) )
+    self.assertEqual( ( 10, 1 ), btl_document_position(10, 2).moved_horizontal(-1) )
+
+  def test_moved_vertical(self):
+    self.assertEqual( ( 11, 1 ), btl_document_position(10, 1).moved_vertical(1) )
+    self.assertEqual( ( 9, 1 ), btl_document_position(10, 1).moved_vertical(-1) )
+
+  def test_moved_to_line(self):
+    self.assertEqual( ( 3, 1 ), btl_document_position(10, 1).moved_to_line(3) )
+    
 if __name__ == '__main__':
   unit_test.main()
