5fe2556ef504e8a9c1eeeeec228528269ea18839
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-02-13 05:28:38 -0800

    more unit tests

--- tests/lib/bes/btl/test_document_position.py
+++ tests/lib/bes/btl/test_document_position.py
@@ -7,8 +7,20 @@ from bes.btl.btl_document_position import btl_document_position
 
 class test_btl_document_position(unit_test):
 
-  def test_move(self):
+  def test___str__(self):
+    self.assertEqual( '10,1', str(btl_document_position(10, 1)) )
+
+  def test_moved(self):
     self.assertEqual( ( 11, 1 ), btl_document_position(10, 1).moved(1, 0) )
+    
+  def test_to_dict(self):
+    self.assertEqual( {
+      'line_number': 10,
+      'column': 1,
+    }, btl_document_position(10, 1).to_dict() )
+
+  def test_parse_str(self):
+    self.assertEqual( ( 10, 1 ), btl_document_position.parse_str('10,1') )
     
 if __name__ == '__main__':
   unit_test.main()
