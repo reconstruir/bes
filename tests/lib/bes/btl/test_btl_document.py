4e56a4055459287122f9901bc695d14c784e8f04
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-03-09 12:18:34 -0800

    add skeleton unit test for btl_document

---
+++ tests/lib/bes/btl/test_btl_document.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+
+from bes.btl.btl_lexer_token import btl_lexer_token
+from bes.btl.btl_document import btl_document
+
+from _test_simple_lexer import _test_simple_lexer
+from _test_simple_parser import _test_simple_parser
+
+class _test_document(btl_document):
+
+  @classmethod
+  #@abstractmethod
+  def lexer_class(clazz):
+    return _test_simple_lexer
+
+  @classmethod
+  #@abstractmethod
+  def parser_class(clazz):
+    return _test_simple_parser
+
+class test_btl_document(unit_test):
+
+  def test_insert_one_token(self):
+    doc = _test_document('''
+name=apple
+color=red
+''')
+
+    self.assert_python_code_text_equal( '''
+0: t_line_break:[NL]:p=1,1:h=h_line_break:i=0
+1: t_key:name:p=2,1:i=1
+2: t_key_value_delimiter:=:p=2,5:i=2
+3: t_value:apple:p=2,6:i=3
+4: t_line_break:[NL]:p=2,11:h=h_line_break:i=4
+5: t_key:color:p=3,1:i=5
+6: t_key_value_delimiter:=:p=3,6:i=6
+7: t_value:red:p=3,7:i=7
+8: t_line_break:[NL]:p=3,10:h=h_line_break:i=8
+9: t_done::h=h_done:i=9
+''', doc.tokens.to_debug_str() )
+
+# 9: t_line_break:[NL]:p=4,1:h=h_line_break:i=9
+    
+if __name__ == '__main__':
+  unit_test.main()
