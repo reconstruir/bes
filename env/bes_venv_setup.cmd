fe11a8d041066cc7165e3f137ab5a0f47dd76471
Ramiro Estrugo <git@fateware.com>
2024-03-31 14:28:43 -0700

    use python 3.11

--- env/bes_venv_setup.cmd
+++ env/bes_venv_setup.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-call %~dp0\..\bin\best.cmd pip_project install_requirements --root-dir "%~dp0\..\VE\bes" %~dp0\..\bes_requirements.txt %~dp0\..\bes_requirements_dev.txt
+call %~dp0\..\bin\best.cmd pip_project install_requirements  --python-version 3.11 --root-dir "%~dp0\..\VE\bes" %~dp0\..\bes_requirements.txt %~dp0\..\bes_requirements_dev.txt
 
 set PATH=%~dp0\..\bin;%PATH%
