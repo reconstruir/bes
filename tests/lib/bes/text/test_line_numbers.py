4be1a3e906466a46ed23dd3edaa2967c8f738096
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-04-01 00:11:56 -0700

    fix for windows line breaks

--- tests/lib/bes/text/test_line_numbers.py
+++ tests/lib/bes/text/test_line_numbers.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import unittest
+from bes.testing.unit_test import unit_test
 from bes.text.line_numbers import line_numbers
 
-class test_line_numbers(unittest.TestCase):
+class test_line_numbers(unit_test):
 
   def test_fit(self):
-    self.assertMultiLineEqual(
-      '''1|foo
+    self.assert_string_equal_fuzzy(
+      '''\
+1|foo
 2|bar
 3|
 ''',
       line_numbers.add_line_numbers('foo\nbar\n\n') )
   
 if __name__ == '__main__':
-  unittest.main()
+  unit_test.main()
