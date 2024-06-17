e0224b5f593f5cf34e4e9c73e6fe9c3074be0062
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-06-17 01:06:50 -0700

    changed my mind about putting the testing class in a separate module.

--- tests/lib/bes/bcli/test_bcli_options.py
+++ tests/lib/bes/bcli/test_bcli_options.py
@@ -11,8 +11,6 @@ from bes.bcli.bcli_options import bcli_options
 from bes.bcli.bcli_options_desc import bcli_options_desc
 from bes.bcli.bcli_simple_type_item_list import bcli_simple_type_item_list
 
-from _unit_test_kiwi_options_desc import _unit_test_kiwi_options_desc
-
 class test_bcli_options(unit_test):
 
   def test_has_options(self):
@@ -69,10 +67,38 @@ color str None
     desc = _test_kiwi_options_shape()
     options = bcli_options(desc)
     self.assertEqual( None, options.color )
+
+  class _unit_test_kiwi_options_desc(bcli_options_desc):
+
+    def __init__(self):
+      super().__init__()
+
+    #@abstractmethod
+    def name(self):
+      return '_kiwi_options_desc'
+  
+    #@abstractmethod
+    def types(self):
+      return bcli_simple_type_item_list([
+      ])
+
+    #@abstractmethod
+    def options_desc(self):
+      return '''
+kiwi int ${_var_foo}
+pear int ${_var_bar}
+  '''
+  
+    #@abstractmethod
+    def variables(self):
+      return {
+        '_var_foo': lambda: '42',
+        '_var_bar': lambda: '666',
+      }
     
   @classmethod
   def _make_test_options(clazz, **kwargs):
-    desc = _unit_test_kiwi_options_desc()
+    desc = clazz._unit_test_kiwi_options_desc()
     return bcli_options(desc, **kwargs)
     
 if __name__ == '__main__':
--- tests/lib/bes/bcli/test_bcli_options_desc.py
+++ tests/lib/bes/bcli/test_bcli_options_desc.py
@@ -10,12 +10,38 @@ from bes.bcli.bcli_options_desc import bcli_options_desc
 from bes.bcli.bcli_option_desc_item import bcli_option_desc_item
 from bes.bcli.bcli_simple_type_item_list import bcli_simple_type_item_list
 
-from _unit_test_kiwi_options_desc import _unit_test_kiwi_options_desc
-
 class test_bcli_options_desc(unit_test):
 
+  class _unit_test_kiwi_options_desc(bcli_options_desc):
+
+    def __init__(self):
+      super().__init__()
+
+    #@abstractmethod
+    def name(self):
+      return '_kiwi_options_desc'
+  
+    #@abstractmethod
+    def types(self):
+      return bcli_simple_type_item_list([
+      ])
+
+    #@abstractmethod
+    def options_desc(self):
+      return '''
+kiwi int ${_var_foo}
+pear int ${_var_bar}
+  '''
+  
+    #@abstractmethod
+    def variables(self):
+      return {
+        '_var_foo': lambda: '42',
+        '_var_bar': lambda: '666',
+      }
+  
   def test___init__(self):
-    desc = _unit_test_kiwi_options_desc()
+    desc = self._unit_test_kiwi_options_desc()
     self.assertEqual( '_kiwi_options_desc', desc.name() )
     self.assertEqual( [], desc.types() )
     self.assertEqual( True, desc.has_option('kiwi') )
