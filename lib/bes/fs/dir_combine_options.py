262ee35ad6d115703388b4c40d6a6a1e7b639c17
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-08-10 15:42:50 -0700

    convert to new options class

--- lib/bes/fs/dir_combine_options.py
+++ lib/bes/fs/dir_combine_options.py
@@ -1,48 +1,32 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+from bes.bcli.bcli_options import bcli_options
+from bes.bcli.bcli_options_desc import bcli_options_desc
+
 from ..cli.cli_options import cli_options
 from ..system.check import check
 
 from .files_cli_options import files_cli_options
+from .files_cli_options import _files_cli_options_desc
 from .dir_combine_defaults import dir_combine_defaults
 
-class dir_combine_options(files_cli_options):
+class _dir_combine_options_desc(_files_cli_options_desc):
 
-  def __init__(self, **kargs):
-    super(dir_combine_options, self).__init__(**kargs)
-
-  @classmethod
   #@abstractmethod
-  def default_values(clazz):
-    'Return a dict of defaults for these options.'
-    return clazz.super_default_values({
-      'dup_file_timestamp': dir_combine_defaults.DUP_FILE_TIMESTAMP,
-      'dup_file_count': dir_combine_defaults.DUP_FILE_COUNT,
-      'destination_dir': None,
-      'ignore_empty': dir_combine_defaults.IGNORE_EMPTY,
-      'flatten': dir_combine_defaults.FLATTEN,
-      'delete_empty_dirs': dir_combine_defaults.DELETE_EMPTY_DIRS,
-    })
+  def name(self):
+    return '_dir_combine_options_desc'
   
-  @classmethod
   #@abstractmethod
-  def value_type_hints(clazz):
-    return clazz.super_value_type_hints({
-      'dup_file_count': int,
-      'ignore_empty': bool,
-      'flatten': bool,
-      'delete_empty_dirs': bool,
-    })
+  def options_desc(self):
+    return self.combine_options_desc(super().options_desc(), f'''
+  destination_dir str
+     ignore_empty bool default={dir_combine_defaults.IGNORE_EMPTY}
+          flatten bool default={dir_combine_defaults.FLATTEN},
+delete_empty_dirs bool default={dir_combine_defaults.DELETE_EMPTY_DIRS}
+''')
+  
+class dir_combine_options(bcli_options):
+  def __init__(self, **kwargs):
+    super().__init__(_dir_combine_options_desc(), **kwargs)
 
-  #@abstractmethod
-  def check_value_types(self):
-    'Check the type of each option.'
-    super(dir_combine_options, self).check_value_types()
-    check.check_string(self.dup_file_timestamp)
-    check.check_int(self.dup_file_count)
-    check.check_string(self.destination_dir, allow_none = True)
-    check.check_bool(self.ignore_empty)
-    check.check_bool(self.flatten)
-    check.check_bool(self.delete_empty_dirs)
-
-check.register_class(dir_combine_options)
+dir_combine_options.register_check_class()
