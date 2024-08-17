de300795da9d1e3a83dc2079431242f3696ce282
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-08-17 10:25:54 -0700

    cleanup rogue code

--- lib/bes/bcli/bcli_type_checked_enum.py
+++ lib/bes/bcli/bcli_type_checked_enum.py
@@ -1,17 +1,8 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import ast
-
 from bes.system.check import check
 
 from .bcli_type_i import bcli_type_i
-
-class _bcli_type_checked_enum_meta(type):
-  def __new__(cls, name, bases, dct, **kwargs):
-    assert 'enum_class' in kwargs
-    result = super().__new__(cls, name, bases, dct)
-    result._enum_class = kwargs['enum_class']
-    return result
 
 class bcli_type_checked_enum(bcli_type_i):
 
