2f98f6e7c742e929f8629f6169feba4370992ad6
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-08-26 08:33:09 -0700

    update for the change in the read() api

--- tests/lib/bes/btl/test_btl_cli_args.py
+++ tests/lib/bes/btl/test_btl_cli_args.py
@@ -58,9 +58,9 @@ stateDiagram-v2
     ]
     rv = self.run_program(self._program, args)
     self.assertEqual(0, rv.exit_code)
-    #print(bf_file_ops.read(tmp, codec = 'utf-8'), flush = True)
+    #print(bf_file_ops.read(tmp, encoding = 'utf-8'), flush = True)
     #return
-    self.assertEqual( True, bf_file_ops.read(tmp, codec = 'utf-8').startswith('<svg id="mermaid-svg" width="100%" xmlns="http://www.w3.org/2000/svg" class="statediagram"') )
+    self.assertEqual( True, bf_file_ops.read(tmp, encoding = 'utf-8').startswith('<svg id="mermaid-svg" width="100%" xmlns="http://www.w3.org/2000/svg" class="statediagram"') )
     
   def test_parser_make_mmd(self):
     tmp = self.make_temp_file(suffix = '.mmd')
@@ -73,7 +73,7 @@ stateDiagram-v2
     rv = self.run_program(self._program, args)
     self.assertEqual(0, rv.exit_code)
 
-    #print(bf_file_ops.read(tmp, codec = 'utf-8'), flush = True)
+    #print(bf_file_ops.read(tmp, encoding = 'utf-8'), flush = True)
     #return
     
     self.assert_text_file_equal_fuzzy( '''
@@ -121,9 +121,9 @@ stateDiagram-v2
     ]
     rv = self.run_program(self._program, args)
     self.assertEqual(0, rv.exit_code)
-    #print(bf_file_ops.read(tmp, codec = 'utf-8'), flush = True)
+    #print(bf_file_ops.read(tmp, encoding = 'utf-8'), flush = True)
     #return
-    self.assertEqual( True, bf_file_ops.read(tmp, codec = 'utf-8').startswith('<svg id="mermaid-svg" width="100%" xmlns="http://www.w3.org/2000/svg" class="statediagram"') )
+    self.assertEqual( True, bf_file_ops.read(tmp, encoding = 'utf-8').startswith('<svg id="mermaid-svg" width="100%" xmlns="http://www.w3.org/2000/svg" class="statediagram"') )
     
 if __name__ == '__main__':
   program_unit_test.main()
