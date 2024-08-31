95f99feb40f7d4cc7a8c1c7877fa87ab52c63922
Ramiro Estrugo <github_reconstruir@fateware.com>
2024-08-31 12:57:32 -0700

    broken code that proves this will never work for posterity

--- tests/lib/bes/files/test_bf_checksum_db.py
+++ tests/lib/bes/files/test_bf_checksum_db.py
@@ -6,6 +6,7 @@ from os import path
 from bes.testing.unit_test import unit_test
 
 from bes.files.bf_checksum_db import bf_checksum_db
+from bes.files.bf_date import bf_date
 
 class test_bf_checksum_db(unit_test):
 
@@ -15,10 +16,19 @@ class test_bf_checksum_db(unit_test):
 
   def test_get_checksum(self):
     db = self._make_tmp_db()
-    tmp1 = self.make_temp_file(content = 'kiwi')
-    tmp2 = self.make_temp_file(content = 'orange')
-    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp1) )
-    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp1) )
+    tmp = self.make_temp_file(content = 'kiwi')
+    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp) )
+    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp) )
 
+  def test_get_checksum_with_collision(self):
+    db = self._make_tmp_db()
+    tmp1 = self.make_temp_file(content = 'kiwi')
+    tmp2 = self.make_temp_file(content = 'kiwx')
+    bf_date.set_modification_date(tmp2, bf_date.get_modification_date(tmp1))
+    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp1) )
+    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp2) )
+#    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp2) )
+#    self.assertEqual( '1a5afeda973d776e31d1d7266f184468f84d99bed311d88d3dcb67015934f9f9', db.get_checksum(tmp) )
+    
 if __name__ == '__main__':
   unit_test.main()
