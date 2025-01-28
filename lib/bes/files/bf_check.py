5949ba8168bdf180abaec14135b84eb56281cac5
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-01-28 08:25:54 -0800

    get rid of the exception argument which was useless and replace exceptions with properly defined errors.  add unit test for FileNotFound case

--- lib/bes/files/bf_check.py
+++ lib/bes/files/bf_check.py
@@ -6,116 +6,110 @@ from os import path
 from ..system.check import check
 from ..system.env_var import os_env_var
 
-from .bf_symlink import bf_symlink
+from .bf_broken_symlink_error import bf_broken_symlink_error
+from .bf_not_dir_error import bf_not_dir_error
+from .bf_not_file_error import bf_not_file_error
 from .bf_permission_error import bf_permission_error
+from .bf_symlink import bf_symlink
 
 class bf_check(object):
 
   #_DISABLED = os_env_var('BES_BF_CHECK_DISABLE').is_set
   
   @classmethod
-  def check_file(clazz, filename, exception_class = None, allow_none = False):
+  def check_file(clazz, filename, allow_none = False):
     check.check_string(filename, allow_none = allow_none)
-    check.check_class(exception_class, allow_none = True)
 
     #if clazz._DISABLED:
     #  return path.normpath(path.abspath(filename))
     
-    exception_class = exception_class or IOError
     if filename:
       filename = path.expanduser(filename)
-    filename = clazz._check_symlink(filename, exception_class, allow_none)
+    filename = clazz._check_symlink(filename, allow_none)
     
     if allow_none and filename == None:
       return None
     if not path.exists(filename):
-      raise exception_class(f'File not found: {filename}')
+      raise FileNotFoundError(f'File not found: {filename}')
     if not path.isfile(filename):
-      raise exception_class(f'Not a file: {filename}')
+      raise bf_not_file_error(f'Not a file: {filename}')
     return path.normpath(path.abspath(filename))
 
   @classmethod
-  def check_dir(clazz, dirname, exception_class = None, allow_none = False):
+  def check_dir(clazz, dirname, allow_none = False):
     check.check_string(dirname, allow_none = allow_none)
-    check.check_class(exception_class, allow_none = True)
 
     #if clazz._DISABLED:
     #  return path.normpath(path.abspath(dirname))
     
-    exception_class = exception_class or IOError
     if dirname:
       dirname = path.expanduser(dirname)
-    dirname = clazz._check_symlink(dirname, exception_class, allow_none)
+    dirname = clazz._check_symlink(dirname, allow_none)
 
     if allow_none and dirname == None:
       return None
     if not path.exists(dirname):
-      raise exception_class(f'Directory not found: {dirname}')
+      raise FileNotFoundError(f'Directory not found: {dirname}')
     if not path.isdir(dirname):
-      raise exception_class(f'Not a directory: {dirname}')
+      raise bf_not_dir_error(f'Not a directory: {dirname}')
     return path.normpath(path.abspath(dirname))
 
   @classmethod
-  def check_file_seq(clazz, files, exception_class = None):
+  def check_file_seq(clazz, files):
     check.check_string_seq(files)
-    check.check_class(exception_class, allow_none = True)
 
     result = []
     for filename in files:
-      result.append(clazz.check_file(filename, exception_class = exception_class))
+      result.append(clazz.check_file(filename))
     return result
   
   @classmethod
-  def check_dir_seq(clazz, dirs, exception_class = None, ignore_files = False):
+  def check_dir_seq(clazz, dirs, ignore_files = False):
     check.check_string_seq(dirs)
-    check.check_class(exception_class, allow_none = True)
 
     result = []
     for d in dirs:
       should_ignore = ignore_files and path.isfile(d)
       if not should_ignore:
-        result.append(clazz.check_dir(d, exception_class = exception_class))
+        result.append(clazz.check_dir(d))
     return result
 
   @classmethod
-  def check_file_or_dir(clazz, ford, exception_class = None, allow_none = False):
+  def check_file_or_dir(clazz, ford, allow_none = False):
     check.check_string(ford, allow_none = allow_none)
-    check.check_class(exception_class, allow_none = True)
 
     #if clazz._DISABLED:
     #  return path.normpath(path.abspath(ford))
     
-    exception_class = exception_class or IOError
     if ford:
       ford = path.expanduser(ford)
-    ford = clazz._check_symlink(ford, exception_class, allow_none)
+    ford = clazz._check_symlink(ford, allow_none)
 
     if allow_none and ford == None:
       return None
     if not path.exists(ford):
-      raise exception_class(f'File not found: {ford}')
+      raise FileNotFoundError(f'File not found: {ford}')
     if not (path.isfile(ford) or path.isdir(ford)):
-      raise exception_class(f'Not a file or directory: {ford}')
+      raise bf_not_file_error(f'Not a file or directory: {ford}')
     return path.normpath(path.abspath(ford))
 
   @classmethod
-  def check_file_or_dir_seq(clazz, fords, exception_class = None):
+  def check_file_or_dir_seq(clazz, fords):
     check.check_string_seq(fords)
-    check.check_class(exception_class, allow_none = True)
 
     result = []
     for f in fords:
-      result.append(clazz.check_file_or_dir(f, exception_class = exception_class))
+      result.append(clazz.check_file_or_dir(f))
     return result
   
   @classmethod
-  def _check_symlink(clazz, filename, exception_class, allow_none):
+  def _check_symlink(clazz, filename, allow_none):
     if allow_none and filename == None:
       return None
     if not path.islink(filename):
       return filename
     if bf_symlink.is_broken(filename):
-      raise exception_class(f'Broken symlink: {filename}')
+      raise bf_broken_symlink_error(f'Broken symlink: {filename}')
     return bf_symlink.resolve(filename)
 
   @classmethod
