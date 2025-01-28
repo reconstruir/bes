9c56d6e4ceea05dd4737f2b30e09aeeb20cfdc55
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-01-28 08:26:35 -0800

    add a bcli declaration for this enum so it can be used in clis

--- lib/bes/data_output/data_output_style.py
+++ lib/bes/data_output/data_output_style.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+from bes.bcli.bcli_type_checked_enum import bcli_type_checked_enum
 from bes.enum_util.checked_enum import checked_enum
 
 class data_output_style(checked_enum):
@@ -11,3 +12,6 @@ class data_output_style(checked_enum):
   TABLE = 'table'
 
 data_output_style.register_check_class()
+
+class data_output_style_cli(bcli_type_checked_enum):
+  __enum_class__ = data_output_style
