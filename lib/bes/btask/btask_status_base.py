4172a632fe7b3777b60af6acfab9305204e1894c
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-03-29 22:00:23 -0700

    subclass from the right superclass

--- lib/bes/btask/btask_status_base.py
+++ lib/bes/btask/btask_status_base.py
@@ -7,7 +7,7 @@ from ..system.check import check
 from ..data_classes.bdata_class_base import bdata_class_base
 
 @dataclasses.dataclass
-class btask_status_base(object):
+class btask_status_base(bdata_class_base):
   task_id: int
     
 check.register_class(btask_status_base, name = 'btask_status', include_seq = False)
