8f6028f96c24c59d4acfe1246cfceb52bef25f55
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-06-08 17:46:53 -0700

    remove computer setup

--- lib/bes/computer_setup/tasks/macos/__init__.py
+++
@@ -1,8 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-# We need to explicitly import each module otherwise PyInstaller will not
-# include them in binaries frozen by pyinstaller.
-from .cst_change_computer_name import cst_change_computer_name
-from .cst_disable_screen_saver import cst_disable_screen_saver
-from .cst_install_command_line_tools import cst_install_command_line_tools
-
--- lib/bes/computer_setup/tasks/unix/__init__.py
+++
@@ -1,6 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-# We need to explicitly import each module otherwise PyInstaller will not
-# include them in the egoist binary.
-from .cst_change_shell_to_bash import cst_change_shell_to_bash
-from .cst_install_brew import cst_install_brew
