a76a487970b879347ea242a2c5cd720e9df634e8
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-06-11 21:00:36 -0700

    remove vmware - moved to bat

--- lib/bes/bat_vmware/bat_vmware_command_interpreters/__init__.py
+++
@@ -1,7 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-from .ci_unix_shell_bash import ci_unix_shell_bash
-from .ci_unix_shell_dash import ci_unix_shell_dash
-from .ci_unix_shell_ksh import ci_unix_shell_ksh
-from .ci_windows_cmd import ci_windows_cmd
-from .ci_windows_powershell import ci_windows_powershell
