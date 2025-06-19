5b511ea6941b0ffc4d5244f24d565fb9361bc07e
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-06-18 17:03:57 -0700

    retire

--- lib/bes/bat_docker_image_maker/dim_ecr.py
+++
@@ -1,6 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-class dim_ecr(object):
-
-  REPO_NAME_PREFIX = 'ego-cicd/images'
-  REPO_ADDRESS = '407733091588.dkr.ecr.us-west-2.amazonaws.com'
