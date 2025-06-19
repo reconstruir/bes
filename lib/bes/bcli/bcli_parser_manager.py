79526c630ac8e0198b2c5a965ee91e382a159584
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-06-18 18:08:46 -0700

    use tree

--- lib/bes/bcli/bcli_parser_manager.py
+++ lib/bes/bcli/bcli_parser_manager.py
@@ -1,27 +1,27 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from ..common.node import node
 from ..system.check import check
 from ..system.log import logger
+
+from .bcli_parser_tree import bcli_parser_tree
 
 class bcli_parser_manager(object):
 
   _log = logger('bcli')
 
   def __init__(self):
-    self._parser_classes = node('root')
+    self._parser_classes = bcli_parser_tree()
     
   def register_parser(self, parser_path, parser_class):
     check.check_string(parser_path)
     check.check_class(parser_class)
 
-    n = self._parser_classes.ensure_path(parser_path)
-    n.data = parser_class
+    n = self._parser_classes.set(parser_path, parser_class)
+#    n.data = parser_class
 
   def find_parser(self, parser_path):
     check.check_string(parser_path)
 
-    n = self._parser_classes.find_child_by_path_data(parser_path)
-    print(f'n={n}')
+    return self._parser_classes.get(parser_path)
     
 check.register_class(bcli_parser_manager, include_seq = False)
