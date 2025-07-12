ada8e2430b4df7c78457b36d4c02ba7d241ae600
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-07-11 18:11:47 -0700

    wip

---
+++ lib/bes/files/resolve/bf_file_resolver_mode.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.bcli.bcli_type_checked_enum import bcli_type_checked_enum
+from bes.enum_util.checked_enum import checked_enum
+
+class bf_file_resolver_mode(checked_enum):
+  INCREMENTAL = 'incremental'
+  PROGRESSIVE = 'progressive'
+  
+bf_file_resolver_mode.register_check_class()
+
+class bf_file_resolver_mode_bcli(bcli_type_checked_enum):
+  __enum_class__ = bf_file_resolver_mode
