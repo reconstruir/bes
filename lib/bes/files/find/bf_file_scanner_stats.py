7e7c220b4e0e9298ccd73428cfeac42f710dff70
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-09-03 05:12:36 -0700

    use a dataclass instead of namedtuple for more simplicity

--- lib/bes/files/find/bf_file_scanner_stats.py
+++ lib/bes/files/find/bf_file_scanner_stats.py
@@ -1,29 +1,27 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+import dataclasses
+import typing
 
-from collections import namedtuple
+from datetime import datetime
 
 from bes.system.check import check
+from bes.data_output.data_output import data_output
+from bes.files.bf_file_ops import bf_file_ops
 from bes.common.json_util import json_util
 
-class bf_file_scanner_stats(namedtuple('bf_file_scanner_stats', 'num_checked, num_files_checked, num_dirs_checked, start_time, end_time, depth')):
+from bes.data_classes.bdata_class_base import bdata_class_base
 
-  def __new__(clazz, num_checked, num_files_checked, num_dirs_checked, start_time, end_time, depth):
-    check.check_int(num_checked)
-    check.check_int(num_files_checked)
-    check.check_datetime(start_time)
-    check.check_datetime(end_time)
-    check.check_int(depth)
-    
-    return clazz.__bases__[0].__new__(clazz, num_checked, num_files_checked, num_dirs_checked, start_time, end_time, depth)
+@dataclasses.dataclass
+class bf_file_scanner_stats(bdata_class_base):
+  num_checked: int
+  num_files_checked: int
+  num_dirs_checked: int
+  start_time: datetime
+  end_time: datetime
+  depth: int
 
-  def to_dict(self):
-    return dict(self._asdict())
-  
-  def to_json(self):
-    return json_util.to_json(self.to_dict(), indent = 2, sort_keys = True)
-  
   @property
   def duration(self):
     return self.end_time - self.start_time
-
+  
 check.register_class(bf_file_scanner_stats, include_seq = False)
