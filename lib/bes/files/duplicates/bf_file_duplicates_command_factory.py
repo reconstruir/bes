21705c9cbc44000832ddd61c67d16f2fd3829e8f
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-09-12 05:45:33 -0700

    add old code for refernece

--- lib/bes/files/duplicates/bf_file_duplicates_command_factory.py
+++ lib/bes/files/duplicates/bf_file_duplicates_command_factory.py
@@ -46,6 +46,32 @@ class bf_file_duplicates_command_factory(bcli_command_factory_i):
     p.add_argument('where', action = 'store', default = [], nargs = '+',
                    help = 'A mix of files and dirs where to resolve files.')
     
+    '''
+    from .bf_file_duplicates_defaults import bf_file_duplicates_defaults
+    p = subparser.add_parser('dups', help = 'Find dups in files or directories.')
+    self.__file_duplicates_cli_add_add_common_args(p)
+    p.add_argument('files', action = 'store', default = [], nargs = '+',
+                   help = 'One or more files or dirs to find dups in [ None ]')
+    p.add_argument('--delete', action = 'store_true', default = False,
+                   help = 'Delete the duplicates [ False ]')
+    p.add_argument('--empty', action = 'store_true',
+                   default = bf_file_duplicates_defaults.INCLUDE_EMPTY_FILES,
+                   dest = 'include_empty_files',
+                   help = 'Include empty files [ False ]')
+    p.add_argument('--keep', action = 'store_true', default = False,
+                   dest = 'keep_empty_dirs',
+                   help = 'Keep empty directories after deleting dups [ False ]')
+    p.add_argument('--small-checksum-size', action = 'store',
+                   default = bf_file_duplicates_defaults.SMALL_CHECKSUM_SIZE,
+                   help = f'Small checksum [ {bf_file_duplicates_defaults.SMALL_CHECKSUM_SIZE} ]')
+    p.add_argument('--prefer', action = 'append', dest = 'prefer_prefixes', default = [],
+                   help = 'Prefer the files starting at the given prefix.')
+    p.add_argument('--ignore', action = 'append', dest = 'ignore_files', default = [],
+                   help = 'Ignore file.')
+    p.add_argument('--delete-empty-dirs', action = 'store_true',
+                   default = bf_file_duplicates_defaults.DELETE_EMPTY_DIRS,
+                   help = 'Delete empty directories after partitioning [ False ]')
+'''    
   #@abstractmethod
   def add_arguments(self, parser):
     parser.add_argument('-v', '--verbose', action = 'store_true',
--- lib/bes/files/duplicates/bf_file_duplicates_command_handler.py
+++ lib/bes/files/duplicates/bf_file_duplicates_command_handler.py
@@ -31,3 +31,49 @@ class bf_file_duplicates_command_handler(bcli_command_handler):
     for entry in entries:
       print(entry.filename)
     return 0
+  
+  '''
+  def dups(self, files, delete, keep_empty_dirs, blurber = None):
+    files = file_check.check_file_or_dir_seq(files)
+    check.check_bool(delete)
+    check.check_bool(keep_empty_dirs)
+
+    dups = file_duplicates.find_duplicates(files, options = self.options)
+    dup_filenames = []
+    for item in dups.items:
+      if not self.options.quiet:
+        print(f'{item.filename}:')
+      for dup in item.duplicates:
+        if not self.options.quiet:
+          print(f'  {dup}')
+        dup_filenames.append(dup)
+
+    if delete:
+      if self.options.dry_run:
+        for f in dup_filenames:
+          if blurber:
+            blurber.blurb(f'DRY_RUN: delete {f}')
+      else:
+        file_util.remove(dup_filenames)
+        if self.options.verbose:
+          for f in dup_filenames:
+            if blurber:
+              blurber.blurb_verbose(f'DELETED file: {f}')
+        if not keep_empty_dirs:
+          fmap = dups.resolved_files.filename_abs_map()
+          possible_empty_dir_roots = []
+          for f in dup_filenames:
+            item = fmap[f]
+            possible_empty_dir_roots.append(item.root_dir)
+          possible_empty_dir_roots = algorithm.unique(possible_empty_dir_roots)
+          deleted_dirs = []
+          for d in possible_empty_dir_roots:
+            next_deleted_dirs = file_find.remove_empty_dirs(d)
+            #print(f'next_deleted_dirs={next_deleted_dirs}')
+            deleted_dirs.extend(next_deleted_dirs)
+          if self.options.verbose:
+            for d in deleted_dirs:
+              if blurber:
+                blurber.blurb_verbose(f'DELETED empty dir: {d}')
+    return 0
+'''  
