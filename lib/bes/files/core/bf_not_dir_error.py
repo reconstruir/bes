98d25a51009c9ee0e0f0891d55e8f3abb71023e6
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-09-20 14:01:00 -0700

    delete premature move

--- lib/bes/files/core/bf_not_dir_error.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-class bf_not_dir_error(Exception):
-  def __init__(self, message = None):
-    super().__init__()
-    self.message = message
-
-  def __str__(self):
-    return self.message or ''
