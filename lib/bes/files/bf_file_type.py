e0c3a0828fae05470eedc04c157e0ed0afffeeaf
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-09-24 22:54:37 -0700

    add want methods

--- lib/bes/files/bf_file_type.py
+++ lib/bes/files/bf_file_type.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+import dataclasses
+
 from bes.enum_util.checked_int_flag_enum import checked_int_flag_enum
 from bes.bcli.bcli_type_checked_enum import bcli_type_checked_enum
 from bes.system.check import check
@@ -17,6 +19,26 @@ class bf_file_type(checked_int_flag_enum):
 
   def mask_matches(self, mask):
     return (self & mask) != 0
+  
+  @property
+  def want_file(self):
+    return self.mask_matches(self.FILE)
+
+  @property
+  def want_dir(self):
+    return self.mask_matches(self.DIR)
+
+  @property
+  def want_link(self):
+    return self.mask_matches(self.LINK)
+
+  @property
+  def want_device(self):
+    return self.mask_matches(self.DEVICE)
+
+  @property
+  def want_socket(self):
+    return self.mask_matches(self.SOCKET)
   
 bf_file_type.register_check_class()
 
