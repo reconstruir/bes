002df8c6fe10654940fcbc5c5ff6fef761a05f34
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-09-28 15:00:05 -0700

    improve the unit test coverage to have both duplicate not duplicate checksums

--- tests/lib/bes/files/duplicates/test_bf_file_dups_entry_list.py
+++ tests/lib/bes/files/duplicates/test_bf_file_dups_entry_list.py
@@ -30,7 +30,6 @@ class _bf_entry_list_tester(object):
 
   @property
   def entries(self):
-    print(f'self._find_result.entries={type(self._find_result.entries)}', flush = True)
     return self._find_result.entries
 
   def make_entry(self, fragment):
@@ -40,6 +39,16 @@ class _bf_entry_list_tester(object):
   def _make_temp_content(clazz, content):
     return temp_content.write_items_to_temp_dir(content, delete = not test_bf_file_dups_entry_list.DEBUG)
 
+  def checksum_map_as_json(self):
+    cm = self.entries.checksum_map(bf_hasher_hashlib(), 'sha256')
+    json_text = json_util.to_json(cm, indent = 2, sort_keys = True)
+    return json_text.replace(self._tmp_dir, '${tmp_dir}')
+
+  def short_checksum_map_as_json(self):
+    cm = self.entries.short_checksum_map(bf_hasher_hashlib(), 'sha256')
+    json_text = json_util.to_json(cm, indent = 2, sort_keys = True)
+    return json_text.replace(self._tmp_dir, '${tmp_dir}')
+  
 class test_bf_file_dups_entry_list(unit_test):
   
   def _make_test_entry(self, *args, **kargs):
@@ -65,44 +74,156 @@ class test_bf_file_dups_entry_list(unit_test):
     return bf_entry(filename, root_dir = root_dir)
     
   def test_checksum_map(self):
-    kiwi_tmp = self.make_temp_file(content = 'kiwi.fruit' * 200000)
     brie_tmp = self.make_temp_file(content = 'brie.cheese' * 200000)
+    cheddar_tmp = self.make_temp_file(content = 'cheddar.cheese' * 200000)
+    kiwi_tmp = self.make_temp_file(content = 'kiwi.fruit' * 200000)
+    lemon_tmp = self.make_temp_file(content = 'lemon.cheese' * 200000)
     t = _bf_entry_list_tester([
+      f'file foo/fruit/lemon.fruit "file:{lemon_tmp}"',
       f'file foo/fruit/kiwi.fruit "file:{kiwi_tmp}"',
       f'file foo/cheese/brie.cheese "file:{brie_tmp}"',
       f'file bar/fruit/kiwi.fruit "file:{kiwi_tmp}"',
       f'file bar/cheese/brie.cheese "file:{brie_tmp}"',
+      f'file bar/cheese/cheddar.cheese "file:{cheddar_tmp}"',
     ], where = [ 'foo', 'bar' ])
-    self.assertEqual( {
-      '98d4cb8bfd2a0adc645e1b56d9a1353afabee286b8dc55406fb31077d686d355': [
-        t.make_entry('bar/cheese/brie.cheese'),
-        t.make_entry('foo/cheese/brie.cheese'),
-      ],
-      'cedf6390305749237858b7f7061a131e5b2407c826f1f2a1afb4667c7e5fbc15': [
-        t.make_entry('bar/fruit/kiwi.fruit'),
-        t.make_entry('foo/fruit/kiwi.fruit'),
-      ],
-    }, t.entries.checksum_map(bf_hasher_hashlib(), 'sha256') )
+    self.assert_json_equal( '''
+{
+  "3d8c33d567ed80a3aec1f02c29d649d85839617ed2430efd374ae0f1ff231f91": {
+    "_values": [
+      {
+        "_filename": "cheese/cheddar.cheese",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/cheese/cheddar.cheese",
+        "filename": "${tmp_dir}/bar/cheese/cheddar.cheese",
+        "relative_filename": "cheese/cheddar.cheese"
+      }
+    ]
+  },
+  "5a7a0d61de3ad93b4d29664496781e792128a097c1f233234b43210254d7699e": {
+    "_values": [
+      {
+        "_filename": "fruit/lemon.fruit",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/fruit/lemon.fruit",
+        "filename": "${tmp_dir}/foo/fruit/lemon.fruit",
+        "relative_filename": "fruit/lemon.fruit"
+      }
+    ]
+  },
+  "98d4cb8bfd2a0adc645e1b56d9a1353afabee286b8dc55406fb31077d686d355": {
+    "_values": [
+      {
+        "_filename": "cheese/brie.cheese",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/cheese/brie.cheese",
+        "filename": "${tmp_dir}/bar/cheese/brie.cheese",
+        "relative_filename": "cheese/brie.cheese"
+      },
+      {
+        "_filename": "cheese/brie.cheese",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/cheese/brie.cheese",
+        "filename": "${tmp_dir}/foo/cheese/brie.cheese",
+        "relative_filename": "cheese/brie.cheese"
+      }
+    ]
+  },
+  "cedf6390305749237858b7f7061a131e5b2407c826f1f2a1afb4667c7e5fbc15": {
+    "_values": [
+      {
+        "_filename": "fruit/kiwi.fruit",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/fruit/kiwi.fruit",
+        "filename": "${tmp_dir}/bar/fruit/kiwi.fruit",
+        "relative_filename": "fruit/kiwi.fruit"
+      },
+      {
+        "_filename": "fruit/kiwi.fruit",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/fruit/kiwi.fruit",
+        "filename": "${tmp_dir}/foo/fruit/kiwi.fruit",
+        "relative_filename": "fruit/kiwi.fruit"
+      }
+    ]
+  }
+}
+''', t.checksum_map_as_json() )
 
   def test_short_checksum_map(self):
-    kiwi_tmp = self.make_temp_file(content = 'kiwi.fruit' * 200000)
     brie_tmp = self.make_temp_file(content = 'brie.cheese' * 200000)
+    cheddar_tmp = self.make_temp_file(content = 'cheddar.cheese' * 200000)
+    kiwi_tmp = self.make_temp_file(content = 'kiwi.fruit' * 200000)
+    lemon_tmp = self.make_temp_file(content = 'lemon.cheese' * 200000)
     t = _bf_entry_list_tester([
+      f'file foo/fruit/lemon.fruit "file:{lemon_tmp}"',
       f'file foo/fruit/kiwi.fruit "file:{kiwi_tmp}"',
       f'file foo/cheese/brie.cheese "file:{brie_tmp}"',
       f'file bar/fruit/kiwi.fruit "file:{kiwi_tmp}"',
       f'file bar/cheese/brie.cheese "file:{brie_tmp}"',
+      f'file bar/cheese/cheddar.cheese "file:{cheddar_tmp}"',
     ], where = [ 'foo', 'bar' ])
-    self.assertEqual( {
-      'd40fcfedf10603b4814ca7e13daf571c0b82b8240ac219df5fce0f97e5ee6287': [
-        t.make_entry('bar/cheese/brie.cheese'),
-        t.make_entry('foo/cheese/brie.cheese'),
-      ],
-      'b78b51fc10a1b3947d3de469f582c6dbb976642b2d0fd739665f2240df24cbbf': [
-        t.make_entry('bar/fruit/kiwi.fruit'),
-        t.make_entry('foo/fruit/kiwi.fruit'),
-      ],
-    }, t.entries.short_checksum_map(bf_hasher_hashlib(), 'sha256') )
+    self.assert_json_equal( '''
+{
+  "b78b51fc10a1b3947d3de469f582c6dbb976642b2d0fd739665f2240df24cbbf": {
+    "_values": [
+      {
+        "_filename": "fruit/kiwi.fruit",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/fruit/kiwi.fruit",
+        "filename": "${tmp_dir}/bar/fruit/kiwi.fruit",
+        "relative_filename": "fruit/kiwi.fruit"
+      },
+      {
+        "_filename": "fruit/kiwi.fruit",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/fruit/kiwi.fruit",
+        "filename": "${tmp_dir}/foo/fruit/kiwi.fruit",
+        "relative_filename": "fruit/kiwi.fruit"
+      }
+    ]
+  },
+  "bef3fb54553181092d38d9520059740b9d516bbdb6869469b997d5547f552297": {
+    "_values": [
+      {
+        "_filename": "fruit/lemon.fruit",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/fruit/lemon.fruit",
+        "filename": "${tmp_dir}/foo/fruit/lemon.fruit",
+        "relative_filename": "fruit/lemon.fruit"
+      }
+    ]
+  },
+  "cad7aab0deebe28c02c0af3561db03a23fc7d7189e33add2b42f1604837260b0": {
+    "_values": [
+      {
+        "_filename": "cheese/cheddar.cheese",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/cheese/cheddar.cheese",
+        "filename": "${tmp_dir}/bar/cheese/cheddar.cheese",
+        "relative_filename": "cheese/cheddar.cheese"
+      }
+    ]
+  },
+  "d40fcfedf10603b4814ca7e13daf571c0b82b8240ac219df5fce0f97e5ee6287": {
+    "_values": [
+      {
+        "_filename": "cheese/brie.cheese",
+        "_root_dir": "${tmp_dir}/bar",
+        "absolute_filename": "${tmp_dir}/bar/cheese/brie.cheese",
+        "filename": "${tmp_dir}/bar/cheese/brie.cheese",
+        "relative_filename": "cheese/brie.cheese"
+      },
+      {
+        "_filename": "cheese/brie.cheese",
+        "_root_dir": "${tmp_dir}/foo",
+        "absolute_filename": "${tmp_dir}/foo/cheese/brie.cheese",
+        "filename": "${tmp_dir}/foo/cheese/brie.cheese",
+        "relative_filename": "cheese/brie.cheese"
+      }
+    ]
+  }
+}
+''', t.short_checksum_map_as_json() )
 
   def test_basename_map(self):
     t = _bf_entry_list_tester([
