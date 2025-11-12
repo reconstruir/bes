f22a1f93cd26aee798bda653d224edeac8b66508
Ramiro Estrugo <github_reconstruir@fateware.com>
2025-11-11 23:48:54 -0800

    retire the old mime classes and just use the new ones

--- lib/bes/fs/file_mime.py
+++ lib/bes/fs/file_mime.py
@@ -1,75 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import fnmatch
+from bes.files.mime.bf_mime import bf_mime
 
-from collections import OrderedDict
+class file_mime(bf_mime):
 
-from bes.text.text_detect import text_detect
-from bes.system.host import host
-
-from .file_mime_type_detector import file_mime_type_detector
-
-class file_mime(object):
-
-  TEXT = 'text'
-
-  BINARY_TYPES = set([
-    'application/octet-stream',
-    'application/x-dosexec',
-    'application/x-executable',
-    'application/x-mach-binary', # This is new in macos sierra
-    'application/x-msdownload',
-    'application/x-pie-executable',
-    'application/x-sharedlib', # -pie vs -no-pie issue in gcc 7.3
-    'application/java', # needed because java and nextstep used "0xcafebabe" for binary magic
-  ])
-
-  @classmethod
-  def mime_type(clazz, filename):
-    return file_mime_type_detector.detect_mime_type(filename)
-    
-  @classmethod
-  def is_text(clazz, filename):
-    return clazz.mime_type_is_text(filename) or text_detect.file_is_text(filename)
-
-  @classmethod
-  def mime_type_is_text(clazz, filename):
-    return clazz.mime_type(filename).startswith(clazz.TEXT)
-
-  @classmethod
-  def is_binary(clazz, filename):
-    return clazz.mime_type(filename) in clazz.BINARY_TYPES
-
-  _GZIP_MIME_TYPES = [ 'application/x-gzip', 'application/gzip', 'application/x-tar' ]
-  _ZIP_MIME_TYPES = [ 'application/zip', 'application/x-zip-compressed' ]
-  
-  @classmethod
-  def is_gzip(clazz, filename):
-    return clazz.mime_type(filename) in clazz._GZIP_MIME_TYPES
-
-  @classmethod
-  def is_zip(clazz, filename):
-    return clazz.mime_type(filename) in clazz._ZIP_MIME_TYPES
-
-  _MEDIA_TYPE_PATTERNS = OrderedDict( [
-    ( 'video', 'video/*' ),
-    ( 'image', 'image/*' ),
-    ( 'audio', 'audio/*' ),
-    ( 'document', 'application/pdf' ),
-  ])
-
-  MEDIA_TYPES = frozenset(_MEDIA_TYPE_PATTERNS.keys())
-  
-  @classmethod
-  def media_type_for_file(clazz, filename):
-    mime_type = clazz.mime_type(filename)
-    return clazz.media_type_for_mime_type(mime_type)
-
-  @classmethod
-  def media_type_for_mime_type(clazz, mime_type):
-    if mime_type == None:
-      return None
-    for media_type, pattern in clazz._MEDIA_TYPE_PATTERNS.items():
-      if fnmatch.fnmatch(mime_type, pattern):
-        return media_type
-    return None
+  pass
--- lib/bes/fs/file_mime_type_detector.py
+++ lib/bes/fs/file_mime_type_detector.py
@@ -1,45 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from ..system.check import check
-from bes.fs.file_check import file_check
+from bes.files.mime.bf_mime_type_detector import bf_mime_type_detector
 
-from ._detail._file_mime_type_detector_base import _file_mime_type_detector_base
-from ._detail._file_mime_type_detector_cheesy  import _file_mime_type_detector_cheesy
-from ._detail._file_mime_type_detector_file_exe  import _file_mime_type_detector_file_exe
-from ._detail._file_mime_type_detector_magic  import _file_mime_type_detector_magic
-from ._detail._file_mime_type_detector_mimetypes  import _file_mime_type_detector_mimetypes
-from ._detail._file_mime_type_detector_puremagic  import _file_mime_type_detector_puremagic
+class file_mime_type_detector(bf_mime_type_detector):
 
-class file_mime_type_detector(_file_mime_type_detector_base):
-
-  @classmethod
-  #@abstractmethod
-  def is_supported(clazz):
-    'Return True if this class is supported on the current platform.'
-    return True
-
-  _ALL_DETECTORS = [
-    _file_mime_type_detector_magic,
-    _file_mime_type_detector_cheesy,
-    _file_mime_type_detector_puremagic,
-    _file_mime_type_detector_file_exe,
-    _file_mime_type_detector_mimetypes,
-  ]
-  _POSSIBLE_DETECTORS = [ p for p in _ALL_DETECTORS if p.is_supported() ]
-  _FILE_EXE_SUPPORTED = _file_mime_type_detector_file_exe in _POSSIBLE_DETECTORS
-  
-  @classmethod
-  #@abstractmethod
-  def detect_mime_type(clazz, filename):
-    'Detect the mime type for file.'
-    filename = file_check.check_file(filename)
-
-    mime_type = None
-    for next_detector in clazz._POSSIBLE_DETECTORS:
-      mime_type = next_detector.detect_mime_type(filename)
-      if mime_type:
-        break
-    if mime_type in ( 'application/octet-stream', ):
-      if clazz._FILE_EXE_SUPPORTED:
-        mime_type = _file_mime_type_detector_file_exe.detect_mime_type(filename)
-    return mime_type
+  pass
