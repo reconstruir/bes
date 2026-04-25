4c550c07e12dc6588b197895e275daca262ee791
Ramiro Estrugo <github_reconstruir@fateware.com>
2026-04-24 20:36:34 -0700

    add

---
+++ fixers/fix_bes_fs_file_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+_s=~/proj/bes/bin/best.sh
+
+${_s} refactor rename 'from bes.fs.file_check import file_check' 'from bes.files.bf_check import bf_check' $(ag bes.fs.file_check -l)
+
+${_s} refactor rename 'file_check.check_file' 'bf_check.check_file' $(ag file_check.check_file -l)
+
+${_s} refactor rename 'file_check.check_dir' 'bf_check.check_dir' $(ag file_check.check_dir -l)
+
