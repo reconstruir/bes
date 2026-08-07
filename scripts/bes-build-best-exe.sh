87d42c6d64013e17a23da80ce327278363b2f920
Ramiro Estrugo <github_reconstruir@fateware.com>
2026-08-07 15:57:50 -0700

    get rid of the superflous env/python.version

--- scripts/bes-build-best-exe.sh
+++ scripts/bes-build-best-exe.sh
@@ -12,7 +12,7 @@ function main()
   local _bat_sh="${_root_dir}/../bat/bin/bat.sh"
   local _bes_script="${_root_dir}/bin/bes_app.py"
   local _best_output_exe="${_root_dir}/best.exe"
-  local _python_version=$(cat "${_root_dir}/env/python.version")
+  local _python_version=$(cat "${_root_dir}/.python-version")
 
   ${_bat_sh} pyinstaller build \
            --build-dir _BES_TEST_BUILD \
